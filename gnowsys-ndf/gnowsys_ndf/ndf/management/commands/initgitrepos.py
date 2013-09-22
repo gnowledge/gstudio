912c51def0e33598bcdb944756d468ed2bc8e262
krishna31 <kpkrishnapal@gmail.com>
2013-09-23 00:46:26 +0530

    Modified accordingly to implement HistoryManager class's functionality.

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/initgitrepos.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/initgitrepos.py
@@ -2,7 +2,6 @@
 import os
 
 ''' imports from installed packages '''
-<<<<<<< HEAD
 from django.core.management.base import BaseCommand, CommandError
 
 #from git import Repo
@@ -11,18 +10,6 @@ from django.core.management.base import BaseCommand, CommandError
 from gnowsys_ndf.settings import VERSIONING_COLLECTIONS
 from gnowsys_ndf.ndf.models import HistoryManager
 
-=======
-# from django_mongokit import document
-from django.core.management.base import BaseCommand, CommandError
-
-from git import Repo
-
-
-''' imports from application folders/files '''
-from gnowsys_ndf import settings
-from gnowsys_ndf.ndf import models
-
->>>>>>> 50041433bee53a620ba79bdc145a23e7beea01df
 ####################################################################################################################
 
 class Command(BaseCommand):
@@ -32,22 +19,4 @@ class Command(BaseCommand):
            "\tgit.repo.base.Repo.init(path=<path>, mkdir=True)"
 
     def handle(self, *args, **options):
-<<<<<<< HEAD
     	HistoryManager().create_repos(*VERSIONING_COLLECTIONS)
-=======
-    	git_repo_path = settings.GIT_REPO_PATH
-    	
-    	dir_exists = os.path.isdir(git_repo_path)
-    	
-    	if not dir_exists:
-    		os.system("mkdir -p " + git_repo_path)
-    		print(" Following path for git repository created successfully :- \n  " + git_repo_path + "\n")
-
-    	for model in settings.VERSIONING_COLLECTIONS:
-    		model_repo = Repo.init( os.path.join( git_repo_path, model ), True )
-    		print(" " + str(model_repo) + " -- git repository created")
-    	
-    	print("\n")
-
-
->>>>>>> 50041433bee53a620ba79bdc145a23e7beea01df
