56ffb46ea2935a164a356ffefb26cedb2664ec46
Avadoot Nachankar <avadoot298@gmail.com>
2013-09-23 00:24:54 +0530

    Modified accordingly to implement HistoryManager class's functionality.

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/initgitrepos.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/initgitrepos.py
@@ -2,6 +2,16 @@
 import os
 
 ''' imports from installed packages '''
+<<<<<<< HEAD
+from django.core.management.base import BaseCommand, CommandError
+
+#from git import Repo
+
+''' imports from application folders/files '''
+from gnowsys_ndf.settings import VERSIONING_COLLECTIONS
+from gnowsys_ndf.ndf.models import HistoryManager
+
+=======
 # from django_mongokit import document
 from django.core.management.base import BaseCommand, CommandError
 
@@ -12,12 +22,19 @@ from git import Repo
 from gnowsys_ndf import settings
 from gnowsys_ndf.ndf import models
 
+>>>>>>> 50041433bee53a620ba79bdc145a23e7beea01df
 ####################################################################################################################
 
 class Command(BaseCommand):
-    help = 'Creates required git repositories for each collection'
+    help = "Creates/Initiates git repositories for each collection.\n\n" \
+           " By calling create_repos() function of HistoryManager class:\n" \
+           "\tHistroyManager().create_repos(self, *args)\n which in turn calls:\n" \
+           "\tgit.repo.base.Repo.init(path=<path>, mkdir=True)"
 
     def handle(self, *args, **options):
+<<<<<<< HEAD
+    	HistoryManager().create_repos(*VERSIONING_COLLECTIONS)
+=======
     	git_repo_path = settings.GIT_REPO_PATH
     	
     	dir_exists = os.path.isdir(git_repo_path)
@@ -33,3 +50,4 @@ class Command(BaseCommand):
     	print("\n")
 
 
+>>>>>>> 50041433bee53a620ba79bdc145a23e7beea01df
