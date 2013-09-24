4c7f457394c3e2fcf6592416a4bb4d537d54d1d9
sunnychaudhari <yuvaraj.chaudhari1gmail.com>
2013-09-24 14:58:17 +0530

    Import statement modified

--- gnowsys-ndf/gnowsys_ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
+
 
 urlpatterns = patterns('',
     (r'^$', include('gnowsys_ndf.ndf.urls')),
     (r'^ndf/', include('gnowsys_ndf.ndf.urls')),
     (r'^benchmarker/', include('gnowsys_ndf.benchmarker.urls')),
-    (r'^admin/', include(admin.site.urls)),
 )
