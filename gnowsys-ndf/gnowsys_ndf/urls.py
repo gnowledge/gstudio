9f11e6bf15587cf7ed49d08244447d9b94e93796
sunnychaudhari <yuvaraj.chaudhari1gmail.com>
2013-09-25 10:55:07 +0530

    Main branch url added

--- gnowsys-ndf/gnowsys_ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', include('gnowsys_ndf.ndf.urls')),
     (r'^ndf/', include('gnowsys_ndf.ndf.urls')),
     (r'^benchmarker/', include('gnowsys_ndf.benchmarker.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
