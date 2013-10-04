243dc8da24ea336ed6a6b056bfd5877e1678cdee
krishna31 <kpkrishnapal@gmail.com>
2013-10-04 17:43:57 +0530

    Modifications regarding replacing git to RCS variables.

--- gnowsys-ndf/gnowsys_ndf/settings.py
+++ gnowsys-ndf/gnowsys_ndf/settings.py
@@ -52,22 +52,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Absolute filesystem path to the project's base directory, 
-# i.e. having settings.py file
-# Example: "/home/tissavadoot/Desktop/Tissproject/TP_MK/gstudio/gnowsys-ndf/gnowsys_ndf/"
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
-
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
-
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -75,11 +67,9 @@ MEDIA_URL = ''
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = '/static'
 
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -87,19 +77,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-
-
-# List of collections (classes) whose documents' history has to be maintained.
-VERSIONING_COLLECTIONS = ['Authors', 'AttributeTypes', 'RelationTypes', 'GSystemTypes', 'GSystems']
-
-# Hash level - number of sub-directories that will be created before saving the json-file
-GIT_REPO_DIR_HASH_LEVEL = 3
-
-# Absolute filesystem path to the directory that will hold all git repositories;
-# belonging to each model(class) for maintaining history of each and every documents.
-# Example: "/home/media/media.lawrence.com/static/ndf/git-repo/"
-GIT_REPO_DIR = os.path.join( PROJECT_ROOT, "ndf/static/git-repo" )
-
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -181,7 +158,32 @@ LOGGING = {
     }
 }
 
+# Absolute filesystem path to the project's base directory, 
+# i.e. having settings.py file
+# Example: "/home/username/Desktop/gstudio/gnowsys-ndf/gnowsys_ndf/"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+""" ----- Revision Control System (RCS) configuration -----
+
+It operates only on single files; and hence used in this project 
+to keep track of history of each document belonging to different 
+collections (models).
+
+"""
+
+# Indicates list of collection-names whose documents' history has to be 
+# maintained.
+VERSIONING_COLLECTIONS = ['Authors', 'AttributeTypes', 'RelationTypes', 
+                          'GSystemTypes', 'GSystems']
+
+# Indicates the "hash-level-number", for level of sub-directories that 
+# will be created before saving the json-file
+RCS_REPO_DIR_HASH_LEVEL = 3
+
+# Absolute filesystem path to the directory that will hold all rcs-files 
+# (history-files corresponding to each json-files created for every document
+#  belonging to a collection).
+RCS_REPO_DIR = os.path.join(PROJECT_ROOT, "ndf/static/rcs-repo")
 
 
