b87f4608bf84b2bacc0916c5bf3b9667e66b8a2d
Kedar Aitawdekar <kedar2a@gmail.com>
2013-11-07 13:41:16 +0530

    Django-registartion App module files.

---
+++ gnowsys-ndf/gnowsys_ndf/registration/backends/default/urls.py
@@ -0,0 +1,53 @@
+"""
+URLconf for registration and activation, using django-registration's
+default backend.
+
+If the default behavior of these views is acceptable to you, simply
+use a line like this in your root URLconf to set up the default URLs
+for registration::
+
+    (r'^accounts/', include('registration.backends.default.urls')),
+
+This will also automatically set up the views in
+``django.contrib.auth`` at sensible default locations.
+
+If you'd like to customize registration behavior, feel free to set up
+your own URL patterns for these views instead.
+
+"""
+
+
+from django.conf.urls import patterns
+from django.conf.urls import include
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+from django.conf.urls.defaults import *
+from django.contrib.auth.views import password_reset
+
+from gnowsys_ndf.registration.backends.default.views import ActivationView
+from gnowsys_ndf.registration.backends.default.views import RegistrationView
+
+
+urlpatterns = patterns('',
+                       url(r'^activate/complete/$',
+                           TemplateView.as_view(template_name='ndf/activation_complete.html'),
+                           name='registration_activation_complete'),
+                       # Activation keys get matched by \w+ instead of the more specific
+                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+                       # that way it can return a sensible "invalid key" message instead of a
+                       # confusing 404.
+                       url(r'^activate/(?P<activation_key>\w+)/$',
+                           ActivationView.as_view(),
+                           name='registration_activate'),
+                       url(r'^register/$',
+                           RegistrationView.as_view(),
+                           name='registration_register'),
+                       url(r'^register/complete/$',
+                           TemplateView.as_view(template_name='ndf/registration_complete.html'),
+                           name='registration_complete'),
+                       url(r'^register/closed/$',
+                           TemplateView.as_view(template_name='ndf/registration_closed.html'),
+                           name='registration_disallowed'),
+                       (r'', include('gnowsys_ndf.registration.auth_urls')),
+                       )
