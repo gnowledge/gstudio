f6f2fe83c056e837d089b1eccb066131323a2d52
Kedar Aitawdekar <kedar2a@gmail.com>
2013-11-08 21:08:31 +0530

    Implemented django-registration app and made some changes in urls

--- gnowsys-ndf/gnowsys_ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from django.contrib.auth import views as auth_views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', include('gnowsys_ndf.ndf.urls')),
     (r'^ndf/', include('gnowsys_ndf.ndf.urls')),
     (r'^benchmarker/', include('gnowsys_ndf.benchmarker.urls')),
+
+	url(r'^accounts/password/reset/$', 
+	auth_views.password_reset,
+	{'template_name': 'registration/password_reset_form.html',
+	'email_template_name': 'registration/password_reset_email.html',
+	'subject_template_name':'registration/password_reset_email_subject.txt'},
+	name='auth_password_reset'),
+    
+    (r'^accounts/', include('registration.backends.default.urls')),
+
     (r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('gnowsys_ndf.registration.backends.default.urls')),
 )
