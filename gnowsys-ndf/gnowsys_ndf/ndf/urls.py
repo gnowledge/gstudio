1ecbb14d5c28daf43a898ae53e8f77bf0667f87d
krishna31 <kpkrishnapal@gmail.com>
2013-11-11 16:04:25 +0530

    Basic implementation of gridfs to upload file and retrive file on browser

--- gnowsys-ndf/gnowsys_ndf/ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls.py
@@ -10,15 +10,10 @@ urlpatterns = patterns('',
                        url(r'^create_wiki/', views.create_wiki, name='create_wiki'),
                        url(r'^wikipage/', views.wikipage, name='wikipage'),
                        url(r'^delete/(?P<_id>[\w-]+)$', views.delete_node, name='delete_node'),
-<<<<<<< HEAD
+
 		       url(r'^uploadDoc/$', TemplateView.as_view(template_name='ndf/UploadDoc.html')),#Direct ot html template
 		       url(r'^submitDoc/', views.submitDoc, name='submitDoc'),	
 		       url(r'^submit/', views.submitDoc, name='submitDoc'),
 		       url(r'^documentList/', views.GetDoc, name='documentList'),
 		       url(r'^readDoc/(?P<_id>[\w-]+)$', views.readDoc, name='read_file'),	
-=======
-                       url(r'^uploadDoc/$', TemplateView.as_view(template_name='ndf/UploadDoc.html')),#Direct ot html template
-                       url(r'^submitDoc/', views.submitDoc, name='submitDoc'),
-                       url(r'^submit/', views.submitDoc, name='submitDoc')	
->>>>>>> 4da6a7769d212d3b1b2af4e1ee8593b5d88c1543
                        )
