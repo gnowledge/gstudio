1255cffbb249f1479de57ff31b279b45c87fb00c
krishna31 <kpkrishnapal@gmail.com>
2013-11-12 16:22:09 +0530

    Restructured.

--- gnowsys-ndf/gnowsys_ndf/ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls.py
@@ -1,10 +1,34 @@
-
 from django.conf.urls import patterns, url
+
 from django.views.generic import TemplateView
 
+from gnowsys_ndf.ndf.views import *
+
+urlpatterns = patterns('gnowsys_ndf.ndf.views.home',
+                       url(r'^$', 'homepage', name='homepage'),
+)
+
+urlpatterns += patterns('gnowsys_ndf.ndf.views.wikipage',
+                        url(r'^wikipage/', 'wikipage', name='wikipage'),
+                        url(r'^create_wiki/', 'create_wiki', name='create_wiki'),
+                        url(r'^resources/page/(?P<page_id>[\w-]+)$', 'display_page', name='display_page'),
+                        url(r'^delete/(?P<_id>[\w-]+)$', 'delete_node', name='delete_node'),
+)
+
+urlpatterns += patterns('gnowsys_ndf.ndf.views.doc',
+                        url(r'^uploadDoc/$', TemplateView.as_view(template_name='ndf/UploadDoc.html')), #Direct ot html template
+                        url(r'^submitDoc/', 'submitDoc', name='submitDoc'),
+                        url(r'^submit/', 'submitDoc', name='submitDoc'),
+                        url(r'^documentList/', 'GetDoc', name='documentList'),
+                        url(r'^readDoc/(?P<_id>[\w-]+)$', 'readDoc', name='read_file'),
+)
+                       
+urlpatterns += patterns('gnowsys_ndf.ndf.views.ajax-views',
+                        url(r'^ajax/edit_content/', 'edit_content', name='edit_content'),
+)
+
+"""
 import views
-
-
 urlpatterns = patterns('',
                        url(r'^$', views.homepage, name='homepage'),
                        url(r'^create_wiki/', views.create_wiki, name='create_wiki'),
@@ -17,3 +41,4 @@ urlpatterns = patterns('',
 		       url(r'^documentList/', views.GetDoc, name='documentList'),
 		       url(r'^readDoc/(?P<_id>[\w-]+)$', views.readDoc, name='read_file'),	
                        )
+"""
--- gnowsys-ndf/gnowsys_ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/urls.py
@@ -10,14 +10,17 @@ urlpatterns = patterns('',
     (r'^ndf/', include('gnowsys_ndf.ndf.urls')),
     (r'^benchmarker/', include('gnowsys_ndf.benchmarker.urls')),
 
-	url(r'^accounts/password/reset/$', 
-	auth_views.password_reset,
-	{'template_name': 'registration/password_reset_form.html',
-	'email_template_name': 'registration/password_reset_email.html',
-	'subject_template_name':'registration/password_reset_email_subject.txt'},
-	name='auth_password_reset'),
+    (r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/password/reset/$', 
+        auth_views.password_reset,
+	{
+            'template_name': 'registration/password_reset_form.html',
+            'email_template_name': 'registration/password_reset_email.html',
+            'subject_template_name':'registration/password_reset_email_subject.txt'
+        },
+	name='auth_password_reset'
+    ),
     
     (r'^accounts/', include('registration.backends.default.urls')),
-
-    (r'^admin/', include(admin.site.urls)),
 )
