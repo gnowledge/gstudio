63a2b26ac19b4c6171b7df6f4adaad92d811e277
krishna31 <kpkrishnapal@gmail.com>
2013-11-12 14:54:57 +0530

    use django-passwords app to check the strength of a password of user

--- gnowsys-ndf/gnowsys_ndf/ndf/forms.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/forms.py
@@ -1,11 +1,16 @@
 import datetime
 
 from django import forms
-
 from django_mongokit.forms import DocumentForm
-from models import Node
+from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
 
+from models import Node
 from models import GSystemType
+
+from registration.forms import RegistrationForm
+from passwords.fields import PasswordField
+
+
 
 
 class NodeForm(DocumentForm):
@@ -21,3 +26,11 @@ class NodeForm(DocumentForm):
         fields = ['name', 'member_of', 'tags']
         
 
+class UserRegistrationForm(RegistrationForm):
+    password1 = PasswordField(label="Password")
+
+class UserChangeform(PasswordChangeForm):
+    new_password1 = PasswordField(label="New password") 
+
+class UserResetform(SetPasswordForm):
+    new_password1 = PasswordField(label="New password") 
