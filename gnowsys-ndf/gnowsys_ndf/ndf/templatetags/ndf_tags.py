00b0ea67e9de2d6947b8b7ee921a765a3570d833
krishna31 <kpkrishnapal@gmail.com>
2013-11-18 11:19:06 +0530

    Created an inclusion tag named 'get_gapps_toolbar()', which returns a template named 'gapps_toolbar.html'.

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -5,7 +5,28 @@ from django.template import Library
 register=Library()
 db=get_database()
 
-"""Get the common template for editing and displaying content 
+##################################################################################################
+
+"""
+/**
+ * Get GApps toolbar
+ */
+"""
+@register.inclusion_tag('ndf/gapps_toolbar.html')
+def get_gapps_toolbar():
+  gst_collection = db[GSystemType.collection_name]
+  gst_cur = gst_collection.GSystemType.find()
+
+  gapps = {}
+  for app in gst_cur:
+    gapps[app._id] = app.name.lower()
+
+  return {'template': 'ndf/gapps_toolbar.html', 'gapps': gapps}
+
+"""
+/**
+ * Get common template for editing and displaying content
+ */
 """
 @register.inclusion_tag('ndf/edit_content.html', takes_context=True)
 def edit_content(context):
@@ -15,7 +36,11 @@ def edit_content(context):
   doc_obj = context['node']
   return {'template': 'ndf/edit_content.html', 'user': user, 'node': doc_obj}
 
-""" Get Existing Groups """
+"""
+/**
+ * Get Existing Groups
+ */
+"""
 @register.assignment_tag
 def get_existing_groups():
   group = []
