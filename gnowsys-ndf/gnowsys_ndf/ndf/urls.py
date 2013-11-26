a3c85aa49745a15358372fcf002ba564da873211
krishna <krishna@glab.(none)>
2013-11-26 12:57:34 +0530

    implemented Image dashboard and image upload

--- gnowsys-ndf/gnowsys_ndf/ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls.py
@@ -22,11 +22,18 @@ urlpatterns += patterns('gnowsys_ndf.ndf.views.group',
 urlpatterns += patterns('gnowsys_ndf.ndf.views.file',
                         url(r'^gapp/file/(?P<file_id>[\w-]+)$', 'file', name='file'),
                         #url(r'^uploadDoc/$', TemplateView.as_view(template_name='ndf/UploadDoc.html')), #Direct ot html template
+                        # url(r'^uploadDoc/(?P<pageurl>\w+)/$', 'uploadDoc', name='uploadDoc'), #Direct ot html template
                         url(r'^uploadDoc/$', 'uploadDoc', name='uploadDoc'), #Direct ot html template
                         url(r'^submitDoc/', 'submitDoc', name='submitDoc'),
                         url(r'^submit/', 'submitDoc', name='submitDoc'),
                         url(r'^documentList/', 'GetDoc', name='documentList'),
                         url(r'^readDoc/(?P<_id>[\w-]+)$', 'readDoc', name='read_file'),
+)
+urlpatterns += patterns('gnowsys_ndf.ndf.views.imageDashboard',
+                        url(r'^gapp/image/(?P<image_id>[\w-]+)$', 'imageDashboard', name='image'),
+                        #url(r'^images/', 'imageDashboard', name='imageDashboard'),
+                        url(r'^imageThumbnail/(?P<_id>[\w-]+)$', 'getImageThumbnail', name='getImageThumbnail'),
+                        url(r'^fullImage/(?P<_id>[\w-]+)$', 'getFullImage', name='getFullImage'),
 )
                        
 urlpatterns += patterns('gnowsys_ndf.ndf.views.ajax-views',
