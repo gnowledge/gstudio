505247eeb9e596b4a70d7564a7440b17ef3c1b00
krishna31 <kpkrishnapal@gmail.com>
2013-11-29 13:17:08 +0530

    Implemented functionality for prior-node field.

--- gnowsys-ndf/gnowsys_ndf/ndf/views/home.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/home.py
@@ -15,10 +15,25 @@ except ImportError:  # old pymongo
 
 
 ''' -- imports from application folders/files -- '''
+from gnowsys_ndf.settings import GAPPS
 from gnowsys_ndf.ndf.models import GSystemType
 
-###########################################################################
+#######################################################################################################################################
+
+db = get_database()
+gst_collection = db[GSystemType.collection_name]
+
+#######################################################################################################################################
+#                                                                                           V I E W S   D E F I N E D   F O R   H O M E
+#######################################################################################################################################
 
 
 def homepage(request):
-    return render_to_response("ndf/base.html", context_instance=RequestContext(request))
+    gst_collection = db[GSystemType.collection_name]
+    gst_cur = gst_collection.GSystemType.find()
+    
+    gapps = {}
+    for app in gst_cur:
+        gapps[app._id] = app.name.lower()
+
+    return render_to_response("ndf/base.html", {'gapps': gapps}, context_instance=RequestContext(request))
