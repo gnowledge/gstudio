5718f29c1ddc94b5cf774d8cc3f401c0b973aef1
krishna31 <kpkrishnapal@gmail.com>
2014-01-02 11:28:00 +0530

    Added feture 1)created page for video,image 2)edit feture added for video,image 3)now on words download will be downloaded as filename with extension not as id

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/video.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/video.py
@@ -8,8 +8,10 @@ from gnowsys_ndf.ndf.views import *
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.videoDashboard',
                         url(r'^(?P<video_id>[\w-]+)$', 'videoDashboard', name='video'),
-                        #url(r'^videos/', 'videoDashboard', name='videoDashboard'),                                                          
+                        #url(r'^videos/', 'videoDashboard', name='videoDashboard'),              
                         url(r'^videoThumbnail/(?P<_id>[\w-]+)$', 'getvideoThumbnail', name='getvideoThumbnail'),
                         url(r'^fullvideo/(?P<_id>[\w-]+)$', 'getFullvideo', name='getFullvideo'),
                         url(r'^video_search/$', 'video_search', name='video_search'),
+                        url(r'^video_detail/(?P<_id>[\w-]+)$', 'video_detail', name='video_detail'),
+                        url(r'^video_edit/(?P<_id>[\w-]+)$', 'video_edit', name='video_edit')
 )
