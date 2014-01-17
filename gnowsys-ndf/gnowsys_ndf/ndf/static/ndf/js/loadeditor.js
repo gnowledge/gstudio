bba7b367feaac70f2f8ec398a9c7f585c2378bef
krishna31 <kpkrishnapal@gmail.com>
2014-01-17 15:37:47 +0530

    changes for create_forum

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/loadeditor.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/loadeditor.js
@@ -1,9 +1,21 @@
-jQuery(document).ready(function($) {
-    $(window).load(function() {
-        $("button[rel]").overlay({mask: '#000', effect: 'apple'});
-    });
-
-$(document).on('click',"#loadeditor",function() {
-        $("button[rel]").overlay({mask: '#000', effect: 'apple'});
+function saveforum(){
+var name=$("#forum_name").val();
+if (name=="" )
+{
+alert("Please enter forum name");
+}
+else
+{
+$("#forum").trigger('click');
+}
+}
+$(document).ready(function(){
+$('#orgitdown').orgitdown(mySettings);
+$("#orgitdown").change(function(){
+var content=$('#orgitdown').val()
+$('#editor_content').val(content);
+});
+$("#savecontent").click(function(){
+$('a.close-reveal-modal').trigger('click');
 });
 });
