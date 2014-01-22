4bc0bc47fe9b893e22a8d23e9394cb1a9b0e6223
Avadoot Nachankar <avadoot298@gmail.com>
2014-01-23 01:02:50 +0530

    Views added to create-and-edit quiz-item as well as quiz-category.

--- gnowsys-ndf/gnowsys_ndf/ndf/views/quiz.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/quiz.py
@@ -4,13 +4,14 @@ import json
 from difflib import HtmlDiff
 
 ''' -- imports from installed packages -- '''
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
 
 from django_mongokit import get_database
 
@@ -28,7 +29,6 @@ from gnowsys_ndf.ndf.models import QUIZ_TYPE_CHOICES
 from gnowsys_ndf.ndf.models import HistoryManager
 from gnowsys_ndf.ndf.rcslib import RCS
 from gnowsys_ndf.ndf.org2any import org2html
-from gnowsys_ndf.ndf.views.methods import get_drawers
 from gnowsys_ndf.ndf.views.methods import get_node_common_fields
 
 
@@ -77,99 +77,118 @@ def quiz(request, group_name, app_id):
                                   context_instance = RequestContext(request)
         )        
 
-def create_quiz_item(request, group_name):
-    """Creates a new quiz-item.
+
+@login_required
+def create_edit_quiz_item(request, group_name, node_id=None):
+    """Displays/Modifies details about the given quiz-item.
     """
-    if request.user.is_authenticated():
-        gst_quiz_item = collection.Node.one({'_type': u'GSystemType', 'name': u'QuizItem'})
+
+    gst_quiz_item = collection.Node.one({'_type': u'GSystemType', 'name': u'QuizItem'})
+
+    context_variables = { 'title': gst_quiz_item.name,
+                          'quiz_type_choices': QUIZ_TYPE_CHOICES,
+                          'group_name': group_name
+                      }
+
+    if node_id:
+        quiz_item_node = collection.Node.one({'_type': u'GSystem', '_id': ObjectId(node_id)})
+    else:
         quiz_item_node = collection.GSystem()
 
-        if request.method == "POST":
-            #get_node_common_fields(request, page_node, group_name, gst_page)
-            #page_node.save()
-            # print "\n Executed POST method part... \n"
+    if request.method == "POST":
+        usrid = int(request.user.id)
 
-            usrid = int(request.user.id)
+        if not quiz_item_node.has_key('_id'):
             quiz_item_node.created_by = usrid
-            # print " quiz_item_node.created_by: ", quiz_item_node.created_by
-
             quiz_item_node.member_of.append(gst_quiz_item.name)
-            # print " quiz_item_node.member_of: ", quiz_item_node.member_of
-
             quiz_item_node.gsystem_type.append(gst_quiz_item._id)
-            # print " quiz_item_node.gsystem_type: ", quiz_item_node.gsystem_type
 
-            if usrid not in quiz_item_node.modified_by:
-                quiz_item_node.modified_by.append(usrid)
-                # print " quiz_item_node.modified_by: ", quiz_item_node.modified_by
+        if usrid not in quiz_item_node.modified_by:
+            quiz_item_node.modified_by.append(usrid)
 
-            if group_name not in quiz_item_node.group_set:
-                quiz_item_node.group_set.append(group_name)
-                # print " quiz_item_node.group_set: ", quiz_item_node.group_set
+        if group_name not in quiz_item_node.group_set:
+            quiz_item_node.group_set.append(group_name)
 
-            quiz_type = request.POST.get('quiz_type')
-            # print " Quiz type: ", quiz_type, "\n"
-            quiz_item_node['quiz_type'] = unicode(quiz_type)
-            # print " quiz_item_node['quiz_type']: ", quiz_item_node['quiz_type']
+        quiz_type = request.POST.get('quiz_type_val')
+        quiz_item_node['quiz_type'] = unicode(quiz_type)
 
-            question = request.POST.get('question')
-            # print " Question: ", question, "\n" 
-            quiz_item_node.content_org = unicode(question)
-            print "\n quiz_item_node.content_org: ", quiz_item_node.content_org
+        question = request.POST.get('question')
+        quiz_item_node.content_org = unicode(question)
             
-            name = "quiz-item-" + (question.split()[3] if len(question.split()) > 4 else question.split()[0])   # Extracting the third word of the question if present, otherwise first word 
+        name = "quiz-item-" + (question.split()[3] if len(question.split()) > 4 else question.split()[0])   # Extracting the third word of the question if present, otherwise first word 
 
-            quiz_item_node.name = name if type(name) == unicode else unicode(name) 
-            # print " quiz_item_node.name: ", quiz_item_node.name
+        quiz_item_node.name = name if type(name) == unicode else unicode(name) 
 
-            # Required to link temporary files with the current user who is modifying this document
-            usrname = request.user.username
-            filename = slugify(name) + "-" + usrname + "-"
-            quiz_item_node.content = org2html(question, file_prefix=filename)
-            print "\n quiz_item_node.content: ", quiz_item_node.content
+        # Required to link temporary files with the current user who is modifying this document
+        usrname = request.user.username
+        filename = slugify(name) + "-" + usrname + "-"
+        quiz_item_node.content = org2html(question, file_prefix=filename)
 
-            print "\n inedx : ", 
+        # If "quiz_type" is either 'Single-Choice' or 'Multiple-Choice', then only extract options
+        options = []
+        if quiz_type != QUIZ_TYPE_CHOICES[0]:
+            no_of_options = int(request.POST.get('no_of_options'))
+            quiz_item_node['options'] = []
 
-            # If "quiz_type" is either 'Single-Choice' or 'Multiple-Choice', then only extract options
-            options = []
-            if quiz_type != QUIZ_TYPE_CHOICES[0]:
-                no_of_options = int(request.POST.get('no_of_options'))
-                quiz_item_node['options'] = []
+            i = 1
+            while i <= no_of_options:
+                options.append(request.POST.get("option" if i == 1 else "option_"+str(i)))
+                i = i + 1
 
-                i = 1
-                while i <= no_of_options:
-                    options.append(request.POST.get("option" if i == 1 else "option_"+str(i)))
-                    i = i + 1
-                # print " Options: ", options, "\n"
+            quiz_item_node['options'] = options
 
-                quiz_item_node['options'] = options
-                # print " quiz_item_node['options'] : ", quiz_item_node['options']
-            
-            qt_initial = quiz_type[:quiz_type.find("-")].lower()
-            quiz_item_node['correct_answer'] = []
-            if quiz_type == QUIZ_TYPE_CHOICES[2]:
-                correct_answer = request.POST.getlist('correct_answer_' + qt_initial)
-                quiz_item_node['correct_answer'] = correct_answer
-            else:
-                correct_answer = request.POST.get('correct_answer_' + qt_initial)
-                quiz_item_node['correct_answer'].append(correct_answer)
-            # print " Correct answer: ", correct_answer, "\n"
-            # print "\n type: ", type(correct_answer), "\n"
-            # print " quiz_item_node['correct_answer'] : ", quiz_item_node['correct_answer']
-
-            tags = request.POST.get('tags')
-            quiz_item_node.tags = [unicode(t.strip()) for t in tags.split(",") if t != ""]
-            # print " quiz_item_node.tags: ", quiz_item_node.tags
-
-            # quiz_item_node.save()
-
-            return HttpResponseRedirect(reverse('quiz', kwargs={'group_name': group_name, 'app_id': gst_quiz._id}))
-
+        # Extracting correct-answer, depending upon 'Multiple-Choice' / 'Single-Choice' 
+        qt_initial = quiz_type[:quiz_type.find("-")].lower()
+        quiz_item_node['correct_answer'] = []
+        if quiz_type == QUIZ_TYPE_CHOICES[2]:
+            correct_answer = request.POST.getlist('correct_answer_' + qt_initial)
+            quiz_item_node['correct_answer'] = correct_answer
         else:
-            return render_to_response("ndf/quiz_item_create.html",
-                                      { 'title': gst_quiz_item.name,
-                                        'quiz_type_choices': QUIZ_TYPE_CHOICES,
-                                        'group_name': group_name
-                                      },
-                                      context_instance=RequestContext(request)
-            )
+            correct_answer = request.POST.get('correct_answer_' + qt_initial)
+            quiz_item_node['correct_answer'].append(correct_answer)
+
+        tags = request.POST.get('tags')
+        quiz_item_node.tags = [unicode(t.strip()) for t in tags.split(",") if t != ""]
+        
+        quiz_item_node.save()
+        
+        return HttpResponseRedirect(reverse('quiz', kwargs={'group_name': group_name, 'app_id': gst_quiz._id}))
+        
+    else:
+        if node_id:
+            context_variables['node'] = quiz_item_node
+            
+        return render_to_response("ndf/quiz_item_create_edit.html",
+                                  context_variables,
+                                  context_instance=RequestContext(request)
+                              )
+
+@login_required
+def create_edit_quiz(request, group_name, node_id=None):
+    """Creates/Edits quiz category.
+    """
+    context_variables = { 'title': gst_quiz.name,
+                          'group_name': group_name
+                      }
+
+    if node_id:
+        quiz_node = collection.Node.one({'_type': u'GSystem', '_id': ObjectId(node_id)})
+    else:
+        quiz_node = collection.GSystem()
+
+    if request.method == "POST":
+        get_node_common_fields(request, quiz_node, group_name, gst_quiz)
+        quiz_node.save()
+        
+        return HttpResponseRedirect(reverse('quiz_details', kwargs={'group_name': group_name, 'app_id': quiz_node._id}))
+
+    else:
+        if node_id:
+            context_variables['node'] = quiz_node
+            
+        return render_to_response("ndf/quiz_create_edit.html",
+                                  context_variables,
+                                  context_instance=RequestContext(request)
+        )
+        
+
