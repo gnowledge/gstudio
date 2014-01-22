0699b20bcb6fb9791e975e0321808da1b6a79513
krishna31 <kpkrishnapal@gmail.com>
2014-01-23 01:02:21 +0530

    Urls added to create-and-edit quiz-item as well as quiz-category.

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/quiz.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/quiz.py
@@ -4,9 +4,10 @@ from gnowsys_ndf.ndf.views import quiz
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.quiz',
                        url(r'^(?P<app_id>[\w-]+)/list$', 'quiz', name='quiz'),
-                       url(r'^create/', 'create_quiz_item', name='quiz_item_create'),
-                       # url(r'^create/', 'create_page', name='page_create'),
-                       # url(r'^(?P<app_id>[\w-]+)/details$', 'page', name='page_details'),
-                       # url(r'^(?P<node_id>[\w-]+)/edit$', 'edit_page', name='page_edit'),
+                       url(r'^create-question/', 'create_edit_quiz_item', name='quiz_item_create_edit'),
+                       url(r'^create/', 'create_edit_quiz', name='quiz_create_edit'),
+                       url(r'^(?P<app_id>[\w-]+)/details$', 'quiz', name='quiz_details'),
+                       url(r'^(?P<node_id>[\w-]+)/edit-question$', 'create_edit_quiz_item', name='quiz_item_create_edit'),
+                       url(r'^(?P<node_id>[\w-]+)/edit$', 'create_edit_quiz', name='quiz_create_edit'),
                        # url(r'^(?P<node_id>[\w-]+)/version/(?P<version_no>\d+\.\d+)$', 'version_node', name='node_version'),
 )
