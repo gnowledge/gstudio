80f66ae102f6484203c48e4b97c7b39b080a33e1
krishna31 <kpkrishnapal@gmail.com>
2014-01-24 01:31:30 +0530

    Modifications regarding implementation of common template for page creation/editing.

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/page.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/page.py
@@ -7,9 +7,9 @@ from gnowsys_ndf.ndf.views import *
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.page',
                        url(r'^(?P<app_id>[\w-]+)/list$', 'page', name='page'),
-                       url(r'^create/', 'create_page', name='page_create'),
+                       url(r'^create/', 'create_edit_page', name='page_create_edit'),
                        url(r'^(?P<app_id>[\w-]+)/details$', 'page', name='page_details'),
-                       url(r'^(?P<node_id>[\w-]+)/edit$', 'edit_page', name='page_edit'),
+                       url(r'^(?P<node_id>[\w-]+)/edit$', 'create_edit_page', name='page_create_edit'),
                        url(r'^(?P<node_id>[\w-]+)/version/(?P<version_no>\d+\.\d+)$', 'version_node', name='node_version'),
 )
 
