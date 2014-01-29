6e357e85acf7ffed74d97c57a4e92400496c515b
krishna <kpkrishnapal@gmail.com>
2014-01-29 16:23:31 +0530

    Added course in GAPP variable in settings.py file

--- gnowsys-ndf/gnowsys_ndf/settings.py
+++ gnowsys-ndf/gnowsys_ndf/settings.py
@@ -200,7 +200,7 @@ LOGIN_REDIRECT_URL = "/"
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-GAPPS = [u"Page", u"File", u"Group", u"Image", u"Video", u"Forum", u"Quiz"]
+GAPPS = [u"Page", u"File", u"Group", u"Image", u"Video", u"Forum", u"Quiz", u"Course"]
 
 EMACS_INIT_FILE_PATH = "~/.emacs"
 
