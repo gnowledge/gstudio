5af77cdfc8fd048ef4c3a24d4e14f6b475618fc3
mukesh <mukeshpandey15@hotmail.com>
2014-02-19 12:42:42 +0530

    creating the private sub documnet along with Moderated group

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/group.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/group.py
@@ -8,7 +8,9 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.group',
                        url(r'^$','group_dashboard', name='groupchange'),
                        url(r'^(?P<app_id>[\w-]+)$', 'group', name='group'),
                        url(r'^create_group/', 'create_group', name='create_group'),
-		       url(r'^(?P<group_id>[\w-]+)/edit_group/', 'edit_group', name='edit_group'),
+		                   url(r'^(?P<group_id>[\w-]+)/edit_group/', 'edit_group', name='edit_group'),
+		                   
+		                   
 )
 urlpatterns += patterns('gnowsys_ndf.ndf.views.ajax-views',
                         url(r'^check_group/', 'checkgroup', name='check_group'),
