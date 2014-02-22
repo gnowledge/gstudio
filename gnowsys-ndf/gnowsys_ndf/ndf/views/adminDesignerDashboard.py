34a7aeb97ac4d5b7af91a0cb063dad832bdca08d
dhiru <dhirusingh11@gmail.com>
2014-02-22 13:14:26 +0530

    modification in Relationtype classification of fields of designer form

--- gnowsys-ndf/gnowsys_ndf/ndf/views/adminDesignerDashboard.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/adminDesignerDashboard.py
@@ -86,12 +86,12 @@ def adminDesignerDashboardClassCreate(request,class_name):
         dependencylist = ['prior_node']
         options = ['featured','created_by','created_at','start_publication','tags','url','last_update','login_required']
     elif class_name == "GSystemType":
-        definitionlist = ['name','altnames','status','meta_type_set','attribute_type_set','relation_type_set','process_type_set']
+        definitionlist = ['name','altnames','status','meta_type_set','attribute_type_set','relation_type_set']
         contentlist = ['content','content_org']
         dependencylist = ['prior_node']
         options = ['featured','created_by','created_at','start_publication','tags','url','last_update','login_required']
     elif class_name == "RelationType":
-        definitionlist = ['name','altnames','subject_type','applicable_node_type','data_type','verbose_name','null','blank','help_text','max_digit','decimal_places','auto_now','auto_now_add','path','verify_exist','status']
+        definitionlist = ['name','inverse_name','altnames','subject_type','object_type','subject_cardinality','object_cardinality','subject_applicable_nodetype','object_applicable_nodetype','slug','is_symmetric','is_reflexive','is_transitive','status']
         contentlist = ['content','content_org']
         dependencylist = ['prior_node']
         options = ['featured','created_by','created_at','start_publication','tags','url','last_update','login_required']
