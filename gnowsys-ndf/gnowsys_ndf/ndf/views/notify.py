425c9ca09dd4d48d0d2ac51d85b94c8cda50f80b
Anuja G <anuja.gk@gmail.com>
2014-02-23 17:49:53 +0530

    notification bug solved

--- gnowsys-ndf/gnowsys_ndf/ndf/views/notify.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/notify.py
@@ -26,11 +26,12 @@ def set_notif_val(request,group_name,msg,activ,bx):
         obj=group_name
         site=sitename.name.__str__()
         objurl="http://test"
-        render = render_to_string("notification/label.html",{'sender':username,'activity':activ,'conjunction':'-','object':obj,'site':site,'link':objurl})
+        render = render_to_string("notification/label.html",{'sender':request.user.username,'activity':activ,'conjunction':'-','object':obj,'site':site,'link':objurl})
         notification.create_notice_type(render, msg, "notification")
         notification.send([bx], render, {"from_user": request.user})
         return True
-    except:
+    except Exception as e:
+        print "Error-",e
         return False
 
 # Send invitation to any user
