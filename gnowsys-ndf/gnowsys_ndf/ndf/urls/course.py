9e012309d21de3d57bd558de339805ea08df6768
Avadoot Nachankar <avadoot298@gmail.com>
2014-03-01 12:37:12 +0530

    Removed unecessary code from respective file(s).

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/course.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/course.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from gnowsys_ndf.ndf.views import *
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.course',
                         url(r'^(?P<course_id>[\w-]+)$', 'course', name='course'),
--- gnowsys-ndf/gnowsys_ndf/ndf/urls/user.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/user.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, url
 
-from django.views.generic import TemplateView
-
-from gnowsys_ndf.ndf.views import *
-
-
 urlpatterns = patterns('gnowsys_ndf.ndf.views.userDashboard',                       
                        url(r'^(?P<user>[^/]+)/userDashboard', 'dashboard', name='userDashboard')                      
                        
