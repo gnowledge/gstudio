0d823067706eee8eef786ad6315636efb932cebf
Avadoot Nachankar <avadoot298@gmail.com>
2014-03-06 13:16:51 +0530

    Bug resolved: Url-redirection regarding private groups.

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -331,16 +331,23 @@ def get_profile_pic(user):
 
 
 @register.assignment_tag
-def get_group_type(group_id,user):
+def get_group_type(group_id, user):
+
   try:
     col_Group = db[Group.collection_name]
+
     if group_id == '/home/':
       colg=col_Group.Group.one({'$and':[{'_type':u'Group'},{'name':u'home'}]})
     else:  
-      #gpid=str(group_id).split("/")
-      colg=col_Group.Group.one({'_id': ObjectId(group_id)})
+      gid = group_id.replace("/", "")
+      if ObjectId.is_valid(gid):
+        colg = col_Group.Group.one({'_type': 'Group', '_id': ObjectId(gid)})
+      else:
+        colg = None
+    
     #check if Group exist in the database
     if colg is not None:
+
       # Check is user is logged in
       if  user.id:
         # condition for group accesseble to logged user
@@ -348,14 +355,21 @@ def get_group_type(group_id,user):
           return "allowed"
         else:
           raise Http404	
+
       else:
-        #condition for groups,accesseble to not logged users
-        if colg.group_type=="PUBLIC":
+        #condition for groups, accessible to not logged users
+        print "\n colg.group_type: ", colg.group_type
+        if colg.group_type == "PUBLIC":
           return "allowed"
         else:
+          print "\n going inn..."
           raise Http404
     else:
-	return "pass"		
+	return "pass"
+
+  except Http404:
+    raise Http404
+    
   except Exception as e:
     print "Error in group_type_tag "+str(e)
     colg=col_Group.Group.one({'$and':[{'_type':u'Group'},{'name':u'home'}]})
