c6ee0ae583b4b62bb695eb3ebc738b9e1339cbe5
dhiru <dhirusingh11@gmail.com>
2014-03-10 19:13:30 +0530

    created ajax url for send data of relationtype and attributetype

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/ajax-urls.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/ajax-urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.ajax_views',
                        url(r'^make_module/', 'make_module_set', name='make_module'),                 
                        url(r'^get_module_json/', 'get_module_json', name='get_module_json'),
                        url(r'^get_graph_json/', 'graph_nodes', name='get_graph_json'),
-                       url(r'^get_data_for_drawer/', 'get_data_for_drawer', name='get_data_for_drawer')
-
-                       
+                       url(r'^get_data_for_drawer/', 'get_data_for_drawer', name='get_data_for_drawer'),
+                       url(r'^get_data_for_drawer_of_attributetype_set/', 'get_data_for_drawer_of_attributetype_set', name='get_data_for_drawer_of_attributetype_set'),
+                       url(r'^get_data_for_drawer_of_relationtype_set/', 'get_data_for_drawer_of_relationtype_set', name='get_data_for_drawer_of_relationtype_set')                       
 
 )
