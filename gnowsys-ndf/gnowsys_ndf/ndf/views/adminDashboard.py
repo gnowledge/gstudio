3eb329004b67736531cee9cb1d9a5ddbcbd49eb1
dhiru <dhirusingh11@gmail.com>
2014-03-12 12:30:32 +0530

    made edit of at,rt,coll_set,member_of,group_set in admin data module

--- gnowsys-ndf/gnowsys_ndf/ndf/views/adminDashboard.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/adminDashboard.py
@@ -51,11 +51,32 @@ def adminDashboardClass(request, class_name):
             obj = collection.Node.one({ '_id': members})
             if obj:
                 member.append(obj.name+" - "+str(members))
-		group_set = [collection.Node.find_one({"_id":eachgroup}).name for eachgroup in each.group_set ]
+
+        member = []
+        member_of_list = []
+        collection_list = []
+        attribute_type_set = []
+        relation_type_set = []
+        if class_name in ("GSystemType"):
+            for e in each.member_of:
+                member_of_list.append(collection.Node.one({'_id':e}).name+" - "+str(e))
+                
+            for members in each.member_of:
+                member.append(collection.Node.one({ '_id': members}).name+" - "+str(members))
+
+            for coll in each.collection_set:
+                collection_list.append(collection.Node.one({ '_id': coll}).name+" - "+str(coll))
+
+            for at_set in each.attribute_type_set:
+                attribute_type_set.append(at_set.name+" - "+str(at_set._id))
+            for rt_set in each.relation_type_set:
+                relation_type_set.append(rt_set.name+" - "+str(rt_set._id))
+
 	if class_name in ("GSystem","File"):
+      		group_set = [collection.Node.find_one({"_id":eachgroup}).name for eachgroup in each.group_set ]
 		objects_details.append({"Id":each._id,"Title":each.name,"Type":",".join(member),"Author":User.objects.get(id=each.created_by).username,"Group":",".join(group_set),"Creation":each.created_at})
 	else :
-		objects_details.append({"Id":each._id,"Title":each.name,"Type":",".join(member),"Author":User.objects.get(id=each.created_by).username,"Creation":each.created_at})
+		objects_details.append({"Id":each._id,"Title":each.name,"Type":",".join(member),"Author":User.objects.get(id=each.created_by).username,"Creation":each.created_at,'member_of':",".join(member_of_list), "collection_list":",".join(collection_list), "attribute_type_set":",".join(attribute_type_set), "relation_type_set":",".join(relation_type_set)})
     groups = []
     group = collection.Node.find({'_type':"Group"})
     for each in group:
