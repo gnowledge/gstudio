cb3b647eaa3403dac5184345d05dc7022fc471a6
dhiru <dhirusingh11@gmail.com>
2014-03-14 11:19:12 +0530

    implementation of custom apps_set instances view

--- gnowsys-ndf/gnowsys_ndf/ndf/views/custom_app_view.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/custom_app_view.py
@@ -12,17 +12,21 @@ from gnowsys_ndf.ndf.models import *
 db = get_database()
 collection = db['Nodes']
 
-def custom_app_view(request, group_id, app_name, app_id, app_set_id=None):
+def custom_app_view(request, group_id, app_name, app_id, app_set_id=None, app_set_instance_id=None):
     """
     custom view for custom GAPPS
     """
     app_collection_set = [] 
+    atlist = []
     app = collection.Node.find_one({"_id":ObjectId(app_id)})
     app_set = ""
     nodes = ""
     nodes_dict = ""
     app_menu = ""
     app_set_template = ""
+    app_set_instance_template = ""
+    app_set_instance_name = ""
+    app_set_name = ""
     title = ""
     for eachset in app.collection_set:
 	 app_set = collection.Node.find_one({"_id":eachset})
@@ -45,8 +49,27 @@ def custom_app_view(request, group_id, app_name, app_id, app_set_id=None):
     else :
         app_menu = "yes"
         title = app_name
+    if app_set_instance_id :
+        app_set_instance_template = "yes"
+        app_set_template = ""
+        systemtype_attributetype_set = []
+        systemtype_relationtype_set = []
+        system = collection.Node.find_one({"_id":ObjectId(app_set_instance_id)})
+        systemtype = collection.Node.find_one({"_id":ObjectId(app_set_id)})
+        for each in systemtype.attribute_type_set:
+            systemtype_attributetype_set.append({"type":each.name,"type_id":each._id,"value":each.data_type})
+        for eachatset in systemtype_attributetype_set :
+            for eachattribute in collection.Node.find({"_type":"GAttribute", "subject":system._id, "attribute_type":eachatset["type_id"]}):
+                atlist.append({"type":eachatset["type"],"type_id":eachatset["type_id"],"value":eachattribute.object_value})
+        # for each in systemtype.relation_type_set:
+        #     object_type = [ {"name":rtot.name, "id":ObjectId(rtot._id)} for rtot in collection.Node.find({"_type":collection.Node.find_one({"_id":each.object_type}).name}) ]
+        #     systemtype_relationtype_set.append({"rt_name":each.name,"type_id":str(each._id),"object_type":object_type})
+                              
+        app_set_name = systemtype.name
+        app_set_instance_name = system.name
+        title =  systemtype.name +"-" +system.name
     template = "ndf/custom_template_for_app.html"
-    variable = RequestContext(request, {'groupid':group_id, 'app_name':app_name, 'app_id':app_id, "app_collection_set":app_collection_set,"app_set_id":app_set_id,"nodes":nodes_dict, "app_menu":app_menu, "app_set_template":app_set_template,"title":title})
+    variable = RequestContext(request, {'groupid':group_id, 'app_name':app_name, 'app_id':app_id, "app_collection_set":app_collection_set,"app_set_id":app_set_id,"nodes":nodes_dict, "app_menu":app_menu, "app_set_template":app_set_template, "app_set_instance_template":app_set_instance_template, "app_set_name":app_set_name, "app_set_instance_name":app_set_instance_name, "title":title, "app_set_instance_atlist":atlist})
     return render_to_response(template, variable)
       
 
@@ -73,9 +96,9 @@ def custom_app_new_view(request, group_id, app_name, app_id, app_set_id=None):
         title = systemtype_name + " - new"
         for each in systemtype.attribute_type_set:
             systemtype_attributetype_set.append({"type":each.name,"type_id":str(each._id),"value":each.data_type})
-        for each in systemtype.relation_type_set:
-            object_type = [ {"name":rtot.name, "id":ObjectId(rtot._id)} for rtot in collection.Node.find({"_type":collection.Node.find_one({"_id":each.object_type}).name}) ]
-            systemtype_relationtype_set.append({"rt_name":each.name,"type_id":str(each._id),"object_type":object_type})
+        # for each in systemtype.relation_type_set:
+        #     object_type = [ {"name":rtot.name, "id":ObjectId(rtot._id)} for rtot in collection.Node.find({"_type":collection.Node.find_one({"_id":each.object_type}).name}) ]
+        #     systemtype_relationtype_set.append({"rt_name":each.name,"type_id":str(each._id),"object_type":object_type})
     
     request_at_dict = {}
     request_rt_dict = {}
@@ -88,6 +111,13 @@ def custom_app_new_view(request, group_id, app_name, app_id, app_set_id=None):
         newgsystem.member_of=[ObjectId(app_set_id)]
         newgsystem.created_by = request.user.id
         newgsystem.save()
+        for key,value in request_at_dict.items():
+            attributetype_key = collection.Node.find_one({"_id":ObjectId(key)})
+            newattribute = collection.GAttribute()
+            newattribute.subject = newgsystem._id
+            newattribute.attribute_type = attributetype_key._id
+            newattribute.object_value = value
+            newattribute.save()
         return HttpResponseRedirect(reverse('GAPPS_set', kwargs={'group_id': group_id, 'app_name': app_name, "app_id":app_id, "app_set_id":app_set_id}))
           
     template = "ndf/custom_template_for_app.html"
