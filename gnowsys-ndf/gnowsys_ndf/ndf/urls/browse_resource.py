eeeb7c690a0a98594f5821a12745ff5c9a81552a
Sunny Chaudhari <yuvaraj.chaudhari1@gmail.com>
2014-04-28 18:23:11 +0530

    Included url for details view in browse resource

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/browse_resource.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/browse_resource.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.browse_resource',
-                       url(r'^(?P<app_id>[\w-]+)$', 'resource_list', name='resource_list')
+                       url(r'^(?P<app_id>[\w-]+)$', 'resource_list', name='resource_list'),
+                       url(r'^details/(?P<_id>[\w-]+)$', 'file_detail', name='resource_detail')
 )
