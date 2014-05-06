872b67bfff932bd94b464914ad0287da812cb311
Sunny Chaudhari <yuvaraj.chaudhari1@gmail.com>
2014-05-06 18:06:14 +0530

    Functions included for maintaining theme hierarchy and editing theme & topics

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/browse_topic.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('gnowsys_ndf.ndf.views.browse_topic',
+                       url(r'^(?P<app_id>[\w-]+)$', 'themes', name='theme_page'),
+                       url(r'^(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)$', 'themes', name='theme_list'),
+                       url(r'^(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/', 'theme_topic_create_edit', name='theme_topic_create')
+
+)
