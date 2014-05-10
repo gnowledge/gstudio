2dfdcce2c241d7b186377913b0b1046d0f7daa3a
dhiru <dhirusingh11@gmail.com>
2014-05-10 18:07:53 +0530

    generate new urls

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/group.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/group.py
@@ -1,24 +1,24 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.group',
-                       url(r'^$','group_dashboard', name='groupchange'),
-                       url(r'^(?P<app_id>[\w-]+)$', 'group', name='group'),
-                       url(r'^create_group/', 'create_group', name='create_group'),
-                       url(r'^group_publish/(?P<node>[\w-]+)$','publish_group',name='publish_group'),
-		       url(r'^edit_group/', 'edit_group', name='edit_group'),
-		       url(r'^switch_group/(?P<node_id>[\w-]+)$', 'switch_group', name='switch_group'),
-		       url(r'^app_selection/(?P<node_id>[\w-]+)$', 'app_selection', name='app_selection'),
+                       url(r'^$','group', name='group'),
+                       url(r'^/(?P<app_id>[\w-]+)$', 'group', name='group'),
+                       url(r'^/create_group/', 'create_group', name='create_group'),
+                       url(r'^/group_publish/(?P<node>[\w-]+)$','publish_group',name='publish_group'),
+		       url(r'^/edit_group/', 'edit_group', name='edit_group'),
+		       url(r'^/switch_group/(?P<node_id>[\w-]+)$', 'switch_group', name='switch_group'),
+		       url(r'^/app_selection/(?P<node_id>[\w-]+)$', 'app_selection', name='app_selection'),
 
 
 )
 
 urlpatterns += patterns('gnowsys_ndf.ndf.views.ajax_views',
-                        url(r'^check_group/', 'checkgroup', name='check_group'),
+                        url(r'^/check_group/', 'checkgroup', name='check_group'),
 
 )
 urlpatterns += patterns('gnowsys_ndf.ndf.views.notify',
-                        url(r'^notify/join/$', 'notifyuser', name='notifyuser'),
-                        url(r'^notify/remove/$', 'notify_remove_user', name='notifyremuser'),
-                        url(r'^notify/send_invitation/$', 'send_invitation', name='sendinvitation'),
+                        url(r'^/notify/join/$', 'notifyuser', name='notifyuser'),
+                        url(r'^/notify/remove/$', 'notify_remove_user', name='notifyremuser'),
+                        url(r'^/notify/send_invitation/$', 'send_invitation', name='sendinvitation'),
 
 )
