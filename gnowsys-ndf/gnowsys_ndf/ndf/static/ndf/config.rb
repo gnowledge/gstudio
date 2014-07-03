8ae98efdc612b4d83a2e50bda383fa008ec3063d
Kedar <kedar2a@gmail.com>
2014-07-03 17:34:32 +0530

    Implemented static file manager-'Bower' & hence removed concern static files

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/config.rb
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
-add_import_path "components/foundation/scss"
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
