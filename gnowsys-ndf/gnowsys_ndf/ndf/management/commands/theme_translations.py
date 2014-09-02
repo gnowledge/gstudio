2c033f3c7f9cd5b1c7699bd6b71352d80780f3c1
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-09-02 17:56:13 +0530

    Translation script updated with some conditions applied

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/theme_translations.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/theme_translations.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from django_mongokit import get_database
-#from mongokit import IS
+from mongokit import IS
 
 try:
   from bson import ObjectId
@@ -35,13 +35,17 @@ for rownum in xrange(sheet.nrows):
     wr.writerow([unicode(val).encode('utf8') for val in sheet.row_values(rownum)])
 csv_file.close()
 
+# print "file createddddd"
+
 class Command(BaseCommand):
     help = "Creating translations of themes"
 
     def handle(self, *args, **options):
-       main()
+      print "---------------------------------------------------\n"
+      main()
 
 def main():
+  #print "main"
   columns = defaultdict(list)
   translation_dict={}
 
@@ -54,6 +58,7 @@ def main():
       for (i,v) in enumerate(row):
         columns[i].append(v)
     translation_dict=dict(zip(columns[0],columns[1]))
+    #print translation_dict,"dict"
     for k, v in translation_dict.items():
       theme_elements=collection.Node.find({'name':k})
       for each in list(theme_elements):
@@ -67,27 +72,44 @@ def main():
               app_obj = theme_item_GST              
             else:        
               app_obj = topic_GST
-        
+                  
           name=v.decode('utf-8')
-          theme_node = collection.GSystem()
-          theme_node.name = unicode(name)
-          theme_node.access_policy = u"PUBLIC"
-          theme_node.contributors.append(1)
-          theme_node.created_by = 1
-          theme_node.group_set.append(get_group._id)
-          theme_node.language = u"hi"
-          theme_node.member_of.append(app_obj._id)
-          theme_node.modified_by = 1
-          theme_node.status = u"DRAFT"
-          theme_node.save()
-          relation_type=collection.Node.one({'$and':[{'name':'translation_of'},{'_type':'RelationType'}]})
-          grelation=collection.GRelation()
-          grelation.relation_type=relation_type
-          grelation.subject=each._id
-          grelation.right_subject=theme_node._id
-          grelation.name=u""
-          grelation.save()
-          print theme_node._id,"Node created Sucessfully!!"
+          #print name,"name"
+          theme_node_rt = collection.Node.find({'$and':[{'_type':"GRelation"},{'relation_type.$id':get_translation_rt._id},{'subject':get_node._id}]})               
+          if theme_node_rt.count() > 0:
+            theme_node = collection.Node.one({'_id': ObjectId(theme_node_rt[0].right_subject) })  
+          else:
+            theme_node = None
+            theme_node_rt = None
+
+          if theme_node is None:        
+            theme_node = collection.GSystem()
+            theme_node.name = unicode(name)
+            theme_node.access_policy = u"PUBLIC"
+            theme_node.contributors.append(1)
+            theme_node.created_by = 1
+            theme_node.group_set.append(get_group._id)
+            theme_node.language = u"hi"
+            theme_node.member_of.append(app_obj._id)
+            theme_node.modified_by = 1
+            theme_node.status = u"DRAFT"
+            theme_node.save()
+            print "\nTranslated Node ",theme_node.name," created successfully\n"
+          else:            
+            print "\nTranslated node ",theme_node.name," already exists\n"
+
+          if theme_node_rt is None:
+            
+            relation_type=collection.Node.one({'$and':[{'name':'translation_of'},{'_type':'RelationType'}]})            
+            grelation=collection.GRelation()
+            grelation.relation_type=relation_type
+            grelation.subject=each._id
+            grelation.right_subject=theme_node._id
+            grelation.name=u""
+            grelation.save()
+            print "\nGRelation for node ",theme_node.name," created sucessfully!!"
+          else:            
+            print "\nGRelation for node ",theme_node.name," already exists\n"
           
     for k, v in translation_dict.items():
       theme_elements=collection.Node.find({'name':k})
@@ -104,7 +126,11 @@ def main():
                 if get_rt:
                   for each in list(get_rt):
                     trans_list.append(each.right_subject)
-               
-              theme_node.collection_set=trans_list
-              theme_node.save()               
-       
+              
+              print "\nProcessing the translated collection_set for node ",theme_node.name," ...\n"
+              if trans_list != theme_node.collection_set:
+                theme_node.collection_set=trans_list
+                theme_node.save()               
+                print "Translated collection_set for node ",theme_node.name," updated successfully\n"
+              else:
+                print "Translated collection_set for node ",theme_node.name," already exists"
