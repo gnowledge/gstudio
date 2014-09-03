d2d902df73670faf770039790333b4c26c2dcd0e
Anuja G <anuja.gk@gmail.com>
2014-09-03 13:09:27 +0530

    Check for invalid email in password reset added

--- gnowsys-ndf/gnowsys_ndf/urls.py
+++ gnowsys-ndf/gnowsys_ndf/urls.py
@@ -11,23 +11,5 @@ urlpatterns = patterns('',
     (r'^$', include('gnowsys_ndf.ndf.urls')),
     (r'^ndf/', include('gnowsys_ndf.ndf.urls')),
     (r'^benchmarker/', include('gnowsys_ndf.benchmarker.urls')),
-    #(r'^i18n/', include('django.conf.urls.i18n')),
     (r'^admin/', include(admin.site.urls)),
-   # (r'^online/', include('online_status.urls')),                     # for online_users
-
-    url(r'^accounts/password/change/done/', auth_views.password_change_done),         
-    url(r'^accounts/password/change/', auth_views.password_change, {'password_change_form': UserChangeform}),
-    url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'set_password_form':UserResetform}),
-    url(r'^accounts/password/reset/$', 
-        auth_views.password_reset,
-	{
-            'template_name': 'registration/password_reset_form.html',
-            'email_template_name': 'registration/password_reset_email.html',
-            'subject_template_name':'registration/password_reset_email_subject.txt'
-        },
-	name='auth_password_reset'
-    ),
-#    url(r'^accounts/register/$', RegistrationView.as_view(form_class=UserRegistrationForm)),
-#    url(r'^accounts/',include('registration_email.backends.default.urls')),
-#    (r'^accounts/', include('registration.backends.default.urls')),
 )
