180eb10603a99208ce5838d9e2a07f53f8cb61bb
admin <admin@krishna-K55VM.(none)>
2014-10-20 17:32:23 +0530

    image multiple upload /deselect /update done

--- gnowsys-ndf/gnowsys_ndf/ndf/views/task.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/task.py
@@ -189,6 +189,7 @@ def create_edit_task(request, group_name, task_id=None,task=None,count=0):
         tag=""
 	field_value=[]
 	a=(request.POST.get("files"))
+	b=(request.POST.get("files_name"))
 	
 	print 
 	
@@ -304,23 +305,21 @@ def create_edit_task(request, group_name, task_id=None,task=None,count=0):
 				change_list.append(each.encode('utf8')+' set to '+request.POST.get(each,"").encode('utf8')) # updated details
 				
 		elif each == 'Upload_Task' :
-			print "unexpected blast"
+			
 			attributetype_key = collection.Node.find_one({"_type":'AttributeType', 'name':'Upload_Task'})
         		attr = collection.Node.find_one({"_type":"GAttribute", "subject":task_node._id, "attribute_type.$id":attributetype_key._id})
         		if attr:
-        		  print "the value",field_value,a
-        		  change_list.append(str(a)+' changed from '+str(attr.object_value)+' to '+str(a))
+        		  change_list.append(' changed from '+str(attr.object_value)+' to '+str(b))      
         		  attr.object_value=a
         		  attr.save()
                         else :
 				newattribute = collection.GAttribute()
                 		newattribute.subject = task_node._id
                 		newattribute.attribute_type = attributetype_key
-                		print "the value of this",a
                 		newattribute.object_value = a
                 		newattribute.save()
-				change_list.append(each.encode('utf8')+' set to '+request.POST.get(each,"").encode('utf8')) # updated details
-        		
+				change_list.append(each.encode('utf8')+' set to '+b.encode('utf8')) # updated details
+        		        
 			
 	    userobj = User.objects.get(id=task_node.created_by)
 	    userlist.append(userobj.username)
@@ -359,6 +358,7 @@ def create_edit_task(request, group_name, task_id=None,task=None,count=0):
     	for each in at_list:
 		attributetype_key = collection.Node.find_one({"_type":'AttributeType', 'name':each})
         	attr = collection.Node.find_one({"_type":"GAttribute", "subject":task_node._id, "attribute_type.$id":attributetype_key._id})
+        	
         	if attr:
         	  if each == "Upload_Task":
         	        file_list=[]
@@ -376,10 +376,10 @@ def create_edit_task(request, group_name, task_id=None,task=None,count=0):
                                 if filedoc:
                                         for i in filedoc:
 		                                file_list.append(i.name)
-                        
-		        blank_dict[each] = json.dumps(file_list)                
-		  else:                      	
-                                blank_dict[each] = attr.object_value
+                        blank_dict[each] = json.dumps(file_list)
+		        blank_dict['select'] = json.dumps(new)                
+		  else:          
+		                blank_dict[each] = attr.object_value
         	
         		
 	if task_node.prior_node :
