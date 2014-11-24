0a941a3f4d76b7c0b1df09980fce9d0fc9c3b635
supriya <sp.sawant14@gmail.com>
2014-11-24 17:36:30 +0530

    get_possible_translation tag modified

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -1782,26 +1782,60 @@ def get_source_id(obj_id):
     return 'null'
 
  
+# def get_translation_relation(obj_id, translation_list = [], r_list = []):
+#         r_list.append(obj_id._id)
+#     	relation_set=obj_id.get_possible_relations(obj_id.member_of)
+# 	if relation_set.has_key('translation_of'):  
+# 		for k,v in relation_set['translation_of'].items():                      
+# 			if k == 'subject_or_right_subject_list':
+# 				for each in v:
+# 					dic={}
+# 					if (each['_id'] not in r_list):
+# 						r_list.append(each['_id'])
+# 						dic[each['_id']]=each['language']
+# 						translation_list.append(dic)
+# 						get_translation_relation(each,translation_list, r_list)
+# 	return translation_list
+
+
 def get_translation_relation(obj_id, translation_list = [], r_list = []):
-        r_list.append(obj_id._id)
-    	relation_set=obj_id.get_possible_relations(obj_id.member_of)
-	if relation_set.has_key('translation_of'):  
-		for k,v in relation_set['translation_of'].items():                      
-			if k == 'subject_or_right_subject_list':
-				for each in v:
-					dic={}
-					if (each['_id'] not in r_list):
-						r_list.append(each['_id'])
-						dic[each['_id']]=each['language']
-						translation_list.append(dic)
-						get_translation_relation(each,translation_list, r_list)
-	return translation_list
+   get_translation_rt=collection.Node.one({'$and':[{'_type':'RelationType'},{'name':u"translation_of"}]})
+   
+   if obj_id not in r_list:
+      print obj_id,"iiiiiiiiiiiiii"
+      r_list.append(obj_id)
+      print r_list
+      node_sub_rt = collection.Node.find({'$and':[{'_type':"GRelation"},{'relation_type.$id':get_translation_rt._id},{'subject':obj_id}]})
+      node_rightsub_rt = collection.Node.find({'$and':[{'_type':"GRelation"},{'relation_type.$id':get_translation_rt._id},{'right_subject':obj_id}]})
+
+      if node_sub_rt: 
+         for each in list(node_sub_rt):
+            right_subject=collection.Node.one({'_id':each.right_subject})
+            if right_subject._id not in r_list:
+               r_list.append(right_subject._id)
+      if node_rightsub_rt:
+         for each in list(node_sub_rt):
+            right_subject=collection.Node.one({'_id':each.subject})
+            if right_subject._id not in r_list:
+               r_list.append(right_subject._id)
+      print r_list,"tttt"
+      if r_list:
+         for each in r_list:
+         
+            dic={}
+            node=collection.Node.one({'_id':each})
+            dic[node._id]=node.language
+            translation_list.append(dic)
+            get_translation_relation(each,translation_list, r_list)
+   return translation_list
+
+
 
 @register.assignment_tag
 def get_possible_translations(obj_id):
         translation_list = []
 	r_list1 = []
-	return get_translation_relation(obj_id,r_list1,translation_list)
+        return get_translation_relation(obj_id._id,r_list1,translation_list)
 
 
 
