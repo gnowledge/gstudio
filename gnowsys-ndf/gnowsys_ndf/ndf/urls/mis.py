f8637437a0a09db2f5eab1936be57f864ea02b10
Avadoot Nachankar <avadoot298@gmail.com>
2014-12-05 01:34:55 +0530

    mis_enroll url modified - Trailing slash removed.

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/mis.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/mis.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.mis',
   url(r'^$', 'mis_detail', name='mis_list'),
-  url(r'^/(?P<app_id>[\w-]+)$', 'mis_detail', name='mis_list'),			# MIS app landing
-  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)$', 'mis_detail', name='mis_app_detail'),      # mis_app_detail
-  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/enroll/$', 'mis_enroll', name='mis_enroll'),     # Student enrollment in Courses link
-  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/(?P<app_set_instance_id>[\w-]+)$', 'mis_detail', name='mis_app_instance_detail'),     # mis_app_instance_detail
-  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/edit/(?P<app_set_instance_id>[\w-]+)/$', 'mis_create_edit', name='mis_app_instance_edit'),			# mis_app_instance_edit
-  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/new/$', 'mis_create_edit', name='mis_app_instance_create'),			# mis_app_instance_create
+  url(r'^/(?P<app_id>[\w-]+)$', 'mis_detail', name='mis_list'),  # MIS app landing
+  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)$', 'mis_detail', name='mis_app_detail'),  # mis_app_detail
+  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/enroll$', 'mis_enroll', name='mis_enroll'),  # Person enrollment in Courses link
+  # url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/enroll/$', 'mis_enroll', name='mis_enroll'),  # Person enrollment in Courses link
+  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/(?P<app_set_instance_id>[\w-]+)$', 'mis_detail', name='mis_app_instance_detail'),  # mis_app_instance_detail
+  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/edit/(?P<app_set_instance_id>[\w-]+)/$', 'mis_create_edit', name='mis_app_instance_edit'),          # mis_app_instance_edit
+  url(r'^/(?P<app_id>[\w-]+)/(?P<app_set_id>[\w-]+)/new/$', 'mis_create_edit', name='mis_app_instance_create'),  # mis_app_instance_create
 )
 
