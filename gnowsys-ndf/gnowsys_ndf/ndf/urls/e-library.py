10ea3b7ba15d8810c04c44706cf15a29697d9383
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-10 16:43:23 +0530

    added url for pagination in e-library

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/e-library.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/e-library.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.e-library',
 					   url(r'^$', 'resource_list'),
                        url(r'^/(?P<app_id>[\w-]+)$', 'resource_list', name='resource_list'),
                        url(r'^/details/(?P<_id>[\w-]+)$', 'file_detail', name='resource_detail'),
-                       # url(r'^/(?P<filetype>[\w-]+)/page-no=(?P<page_no>\d+)/$', 'elib_paged_file_objs', name='elib_paged_file_objs')
+                       url(r'^/(?P<filetype>[\w-]+)/page-no=(?P<page_no>\d+)/$', 'elib_paged_file_objs', name='elib_paged_file_objs')
 )
