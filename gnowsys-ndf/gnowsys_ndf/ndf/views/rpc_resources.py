edc97f7cee1dc0149bda3d20ada351187d038c02
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-30 19:44:03 +0530

    rpc method modified according to fetch the resource info ,converted all keys into strings

--- gnowsys-ndf/gnowsys_ndf/ndf/views/rpc_resources.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/rpc_resources.py
@@ -31,10 +31,12 @@ def resources_list(request):
 	Pandora_GST = collection.Node.one({'_type':'GSystemType','name':'Pandora_video'})
 
 	if File_GST and Pandora_GST and grp:
-		nodes = collection.Node.find({'member_of': {'$nin':[Pandora_GST._id],'$in':[File_GST._id]}, 
-									  'access_policy':'PUBLIC','group_set':ObjectId(grp._id) })
+		nodes = collection.Node.find(
+			{'member_of': {'$nin':[Pandora_GST._id],'$in':[File_GST._id]}, 'access_policy':'PUBLIC','group_set':ObjectId(grp._id) },
+			{'_id':0, 'name':1, 'attribute_set':1, 'created_by':1, 'relation_set':1, 'content_org':1, 'language':1, 'mime_type':1, 'start_publication':1, 'url':1}
+		)
 
-		resource_dict = {}
+		resource_list = []
 		relation_set = {}
 		t_list = []
 		# i = 0 
@@ -47,11 +49,18 @@ def resources_list(request):
 						t_list.append(obj.name)
 					relation_set.update({ e.keys()[0] : t_list })
 
-			resource_dict.update({ str(each): relation_set })
+			each['relation_set'] = relation_set
+
+			for k in each:
+				node_dict = {str(k): each[k]}
+				each.pop(k)
+				each.update(node_dict)
+
+			resource_list.append(each)
 
 			# i=i+1
-			# if i == 1:
+			# if i == 3:
 				# break
 
 	# print "\n",json.dumps(resource_dict),"\n"
-	return json.dumps(resource_dict)
+	return json.dumps(resource_list)
