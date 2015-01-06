3924812990746490497ba51f43e165f89e345aa1
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2015-01-06 20:14:05 +0530

    'get_contents()' method modified to give the filter results

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -1033,7 +1033,7 @@ def get_create_url(groupid):
 	
 
 @register.assignment_tag
-def get_contents(node_id):
+def get_contents(node_id, selected):
 
 	contents = {}
 	image_contents = []
@@ -1063,21 +1063,23 @@ def get_contents(node_id):
 
 		if rel_obj._type == "File":
 			gattr = collection.Node.one({'_type': 'AttributeType', 'name': u'educationaluse'})
-			list_gattr = collection.Node.find({'_type': "GAttribute", 'attribute_type.$id': gattr._id, "subject":rel_obj._id})
-			for attr in list_gattr:
-				left_obj = collection.Node.one({'_id': ObjectId(attr.subject) })
-				if attr.object_value == "Images":
-					image_contents.append((left_obj.name, left_obj._id))
-				elif attr.object_value == "Videos":
-					video_contents.append((left_obj.name, left_obj._id))
-				elif attr.object_value == "Audios":
-					audio_contents.append((left_obj.name, left_obj._id))
-				elif attr.object_value == "Interactives":
-					interactive_contents.append((left_obj.name, left_obj._id))
-				elif attr.object_value == "Documents":
-					document_contents.append((left_obj.name, left_obj._id))
+			list_gattr = collection.Node.find({'_type': "GAttribute", 'attribute_type.$id': gattr._id, "subject":rel_obj._id, 'object_value': selected })
 
-				
+			for attr in list_gattr:
+				left_obj = collection.Node.one({'_id': ObjectId(rel_obj._id) })
+
+				if selected == "Images":
+					image_contents.append( (str(left_obj.name), str(left_obj._id)) )
+				elif selected == "Videos":
+					video_contents.append( (str(left_obj.name), str(left_obj._id)) )
+				elif selected == "Audios":
+					audio_contents.append( (str(left_obj.name), str(left_obj._id)) )
+				elif selected == "Interactives":
+					interactive_contents.append( (str(left_obj.name), str(left_obj._id)) )
+				elif selected == "Documents":
+					document_contents.append( (str(left_obj.name), str(left_obj._id)) )
+
+							
 	if image_contents:
 		contents['Images'] = image_contents
 	
