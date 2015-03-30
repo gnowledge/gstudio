c5adfedd46f8c729f223fb34ce071d5e33c8c746
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2015-03-30 13:50:58 +0530

    Update settings.py
    Made Institutional Partner in partner mapping list

--- gnowsys-ndf/gnowsys_ndf/settings.py
+++ gnowsys-ndf/gnowsys_ndf/settings.py
@@ -552,7 +552,7 @@ GSTUDIO_NROER_MENU = [{"Repository":[]}, {"Partners":["States", "Institutions", 
 GSTUDIO_NROER_GAPPS = [ {"Curated Zone": "topics"}, {"eLibrary": "e-library"}, {"eBooks": ""}, {"eCourses": ""}, {"Events": ""} ]
 
 GSTUDIO_NROER_MENU_MAPPINGS = {
-            "States": "State Partner", "Institutions": "Institution Partner", "Individuals": "Individual Partner",
+            "States": "State Partner", "Institutions": "Institutional Partner", "Individuals": "Individual Partner",
             "Teachers": "Teachers", "Interest Groups": "Interest Groups", "Schools": "Schools"
             }
 
