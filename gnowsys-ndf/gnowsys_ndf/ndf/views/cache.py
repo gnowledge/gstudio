b940d458e4e4b5ebc54cbb75414245538f548ee2
Kedar Aitawdekar <kedar2a@gmail.com>
2015-05-15 19:18:52 +0530

    Added method invalidate_view_cache() to invalidate the cache

--- gnowsys-ndf/gnowsys_ndf/ndf/views/cache.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/cache.py
@@ -75,3 +75,38 @@ def cache_status(request):
             time=datetime.datetime.now(), # server time
             all_cached_items=all_items_list,
         ))
+
+
+def invalidate_view_cache(view_name, args=[], namespace=None, key_prefix=None):
+    """
+        This function allows you to invalidate any view-level cache. 
+        view_name: view function you wish to invalidate or it's named url pattern
+        args: any arguments passed to the view function
+        namepace: optioal, if an application namespace is needed
+        key prefix: for the @cache_page decorator for the function (if any)
+    """
+    from django.core.urlresolvers import reverse
+    from django.http import HttpRequest
+    from django.utils.cache import get_cache_key
+    from django.core.cache import cache
+    # create a fake request object
+    request = HttpRequest()
+    # Loookup the request path:
+    if namespace:
+        view_name = namespace + ":" + view_name
+    # request.path = reverse(view_name, args=args)
+    # get cache key, expire if the cached item exists:
+    key = get_cache_key(request, key_prefix=key_prefix)
+    if key:
+        if cache.get(key):
+            # print key
+            # Delete the cache entry.  
+            #
+            # Note that there is a possible race condition here, as another 
+            # process / thread may have refreshed the cache between
+            # the call to cache.get() above, and the cache.set(key, None) 
+            # below.  This may lead to unexpected performance problems under 
+            # severe load.
+            cache.set(key, None, 0)
+        return True
+    return False
