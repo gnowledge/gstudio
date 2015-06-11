c7023d330ec53679ddbf0f0cce36088ced156b8b
AkaZuko <sharma.ayush1995@gmail.com>
2015-06-11 14:48:49 +0530

    removing files which are not required

--- django_mailbox/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from gnowsys_ndf.ndf.views import mailbox
-
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-)
--- gnowsys-ndf/gnowsys_ndf/django_mailbox/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('gnowsys_ndf.ndf.views.mailbox',
-    url(r'^$', MailView.as_view(), name='mailbox'),
-)
