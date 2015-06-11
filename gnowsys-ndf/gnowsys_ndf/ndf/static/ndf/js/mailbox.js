01ee6b9ffeab9bfcd838f60bc2b374040520f46e
AkaZuko <sharma.ayush1995@gmail.com>
2015-06-11 10:38:37 +0530

    To fetch the old mails in Unread Taab

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/mailbox.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/mailbox.js
@@ -1,8 +1,13 @@
-function setMailBoxName(username, csrf_token, mailBoxName) {
+function setMailBoxName(username, csrf_token, mailBoxName,type) {
 	document.getElementById( 'mailBoxName' ).innerHTML = mailBoxName;
-	$.post( 'mailclient/mailresponse/', {'mailBoxName':mailBoxName, 'username': username, 'csrfmiddlewaretoken': csrf_token }, function(data){		
+	// tyep 1 : Unread Mails ::::::::::::::::::::  type 2 : Read Mails
+	$.post( 'mailclient/mailresponse/', {'mailBoxName':mailBoxName, 'username': username, 'csrfmiddlewaretoken': csrf_token, 'type': type }, function(data){		
 		var content = $(data).filter( '#mailContent' );
 		$( ".mailBoxContent" ).empty().append( content );
-	});	
+	});
 		//.filter() is used because it is a top level-tag and if not use .find()
 }
+
+/*function fetchReadMails(username, csrf_token, mailBoxName) {
+	$.post('mailclient/mailresponse')
+} */
