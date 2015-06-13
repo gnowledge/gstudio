93f2ecdfa5e984e25508db3fd88f5772a4908d5a
Kedar Aitawdekar <kedar2a@gmail.com>
2015-06-14 00:39:21 +0530

    Using dont_show_error variable for not to show msg

--- gnowsys-ndf/gnowsys_ndf/ndf/views/moderation.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/moderation.py
@@ -117,6 +117,7 @@ def all_under_moderation(request, group_id):
 		return render_to_response('ndf/all_under_moderation_status.html', {
 			"group_id": group_id, "groupid": group_id, "title": "All Under Moderation Resources",
 			"files": all_resources, "detail_urlname": "moderation_status", "filetype": "all", 
+			"dont_show_error": True
 			}, RequestContext(request))
 
 	else:
