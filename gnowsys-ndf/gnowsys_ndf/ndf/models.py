70e212335031611c7de92d515dcc134f590d0e89
makfire <mukeshpandey15@hotmail.com>
2015-07-05 18:13:31 +0530

    Snapshot feild implemented

--- gnowsys-ndf/gnowsys_ndf/ndf/models.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/models.py
@@ -226,7 +226,10 @@ class Node(DjangoDocument):
         'status': STATUS_CHOICES_TU,
         'rating':[{'score':int,
                   'user_id':int,
-                  'ip_address':basestring}]
+                  'ip_address':basestring}],
+	'snapshot':dict	
+        
+	
     }
     
     required_fields = ['name', '_type'] # 'group_set' to be included
@@ -379,7 +382,7 @@ class Node(DjangoDocument):
         return self.__unicode__()
 
     def save(self, *args, **kwargs):
-        if "is_changed" in kwargs:
+	if "is_changed" in kwargs:
             if not kwargs["is_changed"]:
                 #print "\n ", self.name, "(", self._id, ") -- Nothing has changed !\n\n"
                 return
@@ -508,7 +511,13 @@ class Node(DjangoDocument):
             except Exception as err:
                 print "\n DocumentError: This document (", self._id, ":", self.name, ") can't be updated!!!\n"
                 raise RuntimeError(err)
-
+	#gets the last version no.
+        rcsno = history_manager.get_current_version(self)
+	#update the snapshot feild
+	if kwargs.get('groupid'):
+		print "enterwa"
+		node_collection.collection.update({'_id':self._id}, {'$set': {'snapshot'+"."+kwargs['groupid']:rcsno }}, upsert=False, multi=True)
+		
     # User-Defined Functions
     def get_possible_attributes(self, gsystem_type_id_or_list):
         """Returns user-defined attribute(s) of given node which belongs to
