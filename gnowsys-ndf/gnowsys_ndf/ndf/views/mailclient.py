6a7233e07d064e67585a376231ee5721442395e5
olympian94 <abtiwari94@gmail.com>
2015-07-06 12:29:24 +0530

    minor changes

--- gnowsys-ndf/gnowsys_ndf/ndf/views/mailclient.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/mailclient.py
@@ -560,7 +560,6 @@ def get_mails_in_box(mailboxname, username, mail_type, displayFrom):
                 print len(all_mails)
                 # To manage the mails that comes as a part of the server-sync technique
                 for mail in all_mails:
-                    #pass username which is later used by save_file function
                     server_sync(mail)
 
                 # To read the mails from the directories
