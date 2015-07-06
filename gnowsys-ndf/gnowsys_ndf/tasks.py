129dbb46880d9971b580b3e8e3cf20463854dd13
olympian94 <abtiwari94@gmail.com>
2015-07-06 12:31:00 +0530

    sending and fetching scripts added

--- gnowsys-ndf/gnowsys_ndf/tasks.py
+++ gnowsys-ndf/gnowsys_ndf/tasks.py
@@ -4,7 +4,14 @@ import subprocess
 
 @app.task
 def run_syncdata_script():
-	script_name = 'send_syncdata'
-	command = "python manage.py " + script_name
-	subprocess.call([command],shell=True)
-	return script_name + ' executed'
+	syncdata_sending_script_name = 'send_syncdata'
+	command1 = "python manage.py " + syncdata_sending_script_name
+	subprocess.call([command1],shell=True)
+	print syncdata_sending_script_name + ' executed.'
+
+	syncdata_fetching_script_name = 'fetch_syncdata'
+	command2 = "python manage.py " + syncdata_fetching_script_name
+	subprocess.call([command2],shell=True)
+	print syncdata_fetching_script_name + ' executed.'
+
+	return 'Both scripts executed'
