68664dbc3ef52e9072c3c8030621083352932c74
olympian94 <abtiwari94@gmail.com>
2015-07-07 11:11:35 +0530

    source duration from settings.py

--- gnowsys-ndf/gnowsys_ndf/celeryconfig.py
+++ gnowsys-ndf/gnowsys_ndf/celeryconfig.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
-
+from gnowsys_ndf.local_settings import SYNCDATA_DURATION
 BROKER_URL = 'amqp://'
 
 CELERYBEAT_SCHEDULE = {
-    'do-every-60-seconds': {
+    'do-every-fixed-seconds': {
         'task': 'gnowsys_ndf.tasks.run_syncdata_script',
-        'schedule': timedelta(seconds=60),
+        'schedule': timedelta(seconds=SYNCDATA_DURATION),
         #'args': (16, 16)
     },
 }
