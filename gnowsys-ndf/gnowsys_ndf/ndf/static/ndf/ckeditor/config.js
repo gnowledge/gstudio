99a251a271c77843fa1bde87ca63f3f636ef9cc8
AkaZuko <sharma.ayush1995@gmail.com>
2015-07-10 14:44:18 +0530

    required plugin for the ckeditor

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/ckeditor/config.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/ckeditor/config.js
@@ -32,6 +32,9 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
+	
+	//extra plugins
+	config.extraPlugins = 'confighelper';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
