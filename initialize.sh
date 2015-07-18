95979d96eed758c535b11933a75d11c56b013463
gnowgi <nagarjun@gnowledge.org>
2015-07-19 00:26:18 +0530

    corrected cvs to csv, and inserted once again filldb

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "[run] get updated additional schema STs, ATs and RTs"
-cp -a /home/docker/code/gstudio/doc/schema_directory/ /home/docker/code/gstudio/gnowsys-ndf/gnowsys_ndf/ndf/management/commands/schema_files/
+cp /home/docker/code/gstudio/doc/schema_directory/* /home/docker/code/gstudio/gnowsys-ndf/gnowsys_ndf/ndf/management/commands/schema_files/
 
 echo "[run] start mongod"
 mongod &
@@ -21,7 +21,8 @@ if not User.objects.filter(username='admin').count():
 
 echo "[run] create or update gstudio schema in mongodb"
 python manage.py filldb
-python manage.py create_schema STs_run1.cvs
-python manage.py create_schema ATs.cvs
-python manage.py create_schema RTs.cvs
-python manage.py create_schema STs_run2.cvs
+python manage.py create_schema STs_run1.csv
+python manage.py create_schema ATs.csv
+python manage.py create_schema RTs.csv
+python manage.py create_schema STs_run2.csv
+python manage.py filldb
