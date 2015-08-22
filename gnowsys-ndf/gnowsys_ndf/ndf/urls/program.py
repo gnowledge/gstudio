2c5dcb2e483209a188d0e3290e0f280fcf7a9173
katkamrachana <katkam.rachana@gmail.com>
2015-08-22 13:34:32 +0530

    new view&url files for programevents

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/program.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('gnowsys_ndf.ndf.views.program',
+                        url(r'^[/]$', 'program_event_list', name='program_event_list'),
+						# url(r'^programs/$', 'program_event_list', name='program_event_list'),
+                       
+                       )
