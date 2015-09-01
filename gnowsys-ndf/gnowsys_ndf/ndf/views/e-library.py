d8d8503ac5a41d4d539a4be8f93b71b0ef546b42
Kedar Aitawdekar <kedar2a@gmail.com>
2015-09-01 15:24:16 +0530

    Query modified to get collection after filtering in e-library

--- gnowsys-ndf/gnowsys_ndf/ndf/views/e-library.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/e-library.py
@@ -271,10 +271,10 @@ def elib_paged_file_objs(request, group_id, filetype, page_no):
 		# print "query_dict : ", query_dict
 
 		files = node_collection.find({												
-										'member_of': ObjectId(GST_FILE._id), 
+										'member_of': {'$in': [GST_FILE._id, GST_PAGE._id]},
 										# '_type': 'File',
 										# 'fs_file_ids': {'$ne': []}, 
-										'group_set': ObjectId(group_id), 
+										'group_set': {'$all': [ObjectId(group_id)]},
 										'$and': query_dict,
 										'$or': [
 												{ 'access_policy': u"PUBLIC" },
