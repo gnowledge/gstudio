3f50b791a01c4ecdf9ece39d92bfc9f9b9949c90
makfire <mukeshpandey15@hotmail.com>
2015-09-03 19:48:55 +0530

    catalog retrival code written of system types

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/create_catalog.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/create_catalog.py
@@ -2,66 +2,147 @@ import subprocess
 from django.core.management.base import BaseCommand, CommandError
 from gnowsys_ndf.factory_type import *
 from gnowsys_ndf.ndf.models import *
-
+from bson.json_util import dumps,loads
+from bson import json_util
+from gnowsys_ndf.ndf.models import NodeJSONEncoder
+from gnowsys_ndf.ndf.rcslib import RCS
+import json
+rcs = RCS()
+hr = HistoryManager()
+		
 class Command(BaseCommand):
 
 	def handle(self,*args,**options):
 		
 		#print factory_attribute_types
+		final_list = []
+		
 		GSystemTypeList = [i['name'] for i in factory_gsystem_types]
 		RelationTypeList = [ i.keys()[0] for i in  factory_relation_types ]
 		AttributeTypeList = [ i.keys()[0] for i in  factory_attribute_types ]
-		get_factory_data(factory_data) 
-		get_gsystems(GSystemTypeList)
-		get_relationtypes(RelationTypeList)
-		get_attributetypes(AttributeTypeList)
-
+		metatypes = get_metatypes()	
+		GSystemTypeList = get_gsystems(GSystemTypeList)
+		RelationTypeList = get_relationtypes(RelationTypeList)
+		AttributeTypeList = get_attributetypes(AttributeTypeList)
+		datalist = get_factory_data(factory_data)
+		final_list  = datalist + AttributeTypeList +  RelationTypeList  + GSystemTypeList + metatypes
+		#send it for making the rcs of all the nodes
+		make_rcs_dir(final_list)
+		'''
+		make_catalog()		 
+		'''
+def  get_metatypes():
+	node = node_collection.find({"_type":"MetaType"})
+	print node.count()
+	metatypeslist = [i._id for i in node]
+	print "the metatype",metatypeslist
+	return metatypeslist		
 
 def  get_gsystems(GSystemTypeList):
-	var = '{"_type": "GSystemType","name":{"$in":%s}}' % GSystemTypeList
-	var = var.replace("'",'"')
+	var = {"_type": "GSystemType","name":{"$in":GSystemTypeList}}  
+	Systemtypes = node_collection.find(var,{"_id":1})
+	Systemtypelist = [i._id for i in Systemtypes]
+	'''
 	cmd = "mongoexport --db studio-dev --collection Nodes -q '" + '%s'  % var + "' --out Schema/GSystemType.json"
 	subprocess.Popen(cmd,stderr=subprocess.STDOUT,shell=True)
-
+	'''
+	return Systemtypelist	
 def get_relationtypes(RelationTypeList):
-	var = '{"_type": "RelationType","name":{"$in":%s}}' % RelationTypeList
-	var = var.replace("'",'"')
+	var = {"_type": "RelationType","name":{"$in":RelationTypeList}} 
+	'''var = var.replace("'",'"')'''
+	'''
 	cmd = "mongoexport --db studio-dev --collection Nodes -q '" + '%s'  % var + "' --out Schema/RelationType.json"
 	subprocess.Popen(cmd,stderr=subprocess.STDOUT,shell=True)
-
-
+	'''
+	relationtype = node_collection.find(var,{"_id":1})
+	relationtypelist = [i._id for i in relationtype ]
+	return relationtypelist
 def get_attributetypes(AttributeTypeList):
-	var = '{"_type": "AttributeType","name":{"$in":%s}}' % AttributeTypeList
+	var = {"_type": "AttributeType","name":{"$in":AttributeTypeList}} 
+	'''
 	var = var.replace("'",'"')	
 	cmd = "mongoexport --db studio-dev --collection Nodes -q '" + '%s'  % var + "' --out Schema/AttributeType.json"
 	subprocess.Popen(cmd,stderr=subprocess.STDOUT,shell=True)
-
+	'''
+	attributetype = node_collection.find(var,{"_id":1})
+	attributetypelist = [i._id for i in attributetype]
+	return attributetypelist
 def  get_factory_data(Factory_data):
-	final_list = []
+	data_list = []
 	for i in factory_data:
 		var = str(i)
 		var = var.replace("'",'"')
 		node = node_collection.find(i)
-		final_list.append(node[0]._id)
-		# take the rcs of the data
+		data_list.append(node[0]._id)
+	'''
 		cmd = "mongoexport --db studio-dev --collection Nodes -q '" + '%s'  % var + "' --out Schema/" + str(i["name"]) + "." +"json" + ""
 		subprocess.Popen(cmd,stderr=subprocess.STDOUT,shell=True)
+	'''
+	return data_list
+		# take the rcs of the data
+		
+def make_rcs_dir(final_list):
 	PROJECT_ROOT = os.path.abspath(os.path.dirname(os.pardir))
 	rcs_path = os.path.join(PROJECT_ROOT)
 	path_val = os.path.exists(rcs_path)
+	GSystemtypenodes = []
+	Relationtypenodes = []
+	Attributetypenodes = []
+	factorydatanode = []
+	metatype = []
 	if path_val == False:
 		os.makedirs(rcs_path)
 	for i in final_list:	
 		#get rcs files path and copy them to the current dir:
-		hr = HistoryManager()
 		if type(i)!= int:
-			try:
+			
 				a = node_collection.find_one({"_id":ObjectId(i)})
 				if a:
-					path = hr.get_file_path(a)
-					path = path + ",v"
-					print path,rcs_path
-					cp = "cp  -u " + path + " " +" --parents " + rcs_path + "/" 
-					subprocess.Popen(cp,stderr=subprocess.STDOUT,shell=True)
-			except:		
-				pass
+					rel_path = hr.get_file_path(a)
+					path = rel_path + ",v"
+					#cp = "cp  -u " + path + " " +" --parents " + rcs_path + "/" 
+					#subprocess.Popen(cp,stderr=subprocess.STDOUT,shell=True)
+				if a:
+					filter_list = ['GSystemType','RelationType','AttributeType','MetaType']
+					if a._type  in filter_list:
+						file_node = get_version_document(a,rel_path)
+						if a._type == 'GSystemType':
+							GSystemtypenodes.append(file_node)
+						elif a._type == 'RelationType':
+							Relationtypenodes.append(file_node)
+						elif a._type == 'AttributeType':
+							Attributetypenodes.append(file_node)
+						elif a._type == 'MetaType': 
+							print "asdf",a 
+							metatype.append(file_node)
+					elif a._type not in filter_list:
+						file_node = get_version_document(a,rel_path,'1.1')
+						factorydatanode.append(file_node)					
+			
+	#start making catalog 	
+	make_catalog(GSystemtypenodes,'GSystemType')
+	make_catalog(Relationtypenodes,'RelationType')
+	make_catalog(Attributetypenodes,'AttributeType')	
+	make_catalog(factorydatanode,'factorydata')
+	print metatype
+	make_catalog(metatype,'metatype')	
+	
+def make_catalog(file_node,data_type):
+	''' make folder called catalog '''
+	PROJECT_ROOT = os.path.abspath(os.path.dirname(os.pardir))
+	refcatpath = os.path.join(PROJECT_ROOT + '/GRef.cat.')
+	path_val = os.path.exists(refcatpath)
+	if path_val == False:		
+		os.makedirs(refcatpath)
+	with open('GRef.cat./' + data_type+ ".json", 'w') as outfile:
+		json.dump(file_node, outfile,ensure_ascii=True,indent=4,
+                                          sort_keys=False)
+	file_node = []
+def get_version_document(document,rel_path,version=""):
+	if version == "":
+		versionno = hr.get_current_version(document)
+	rcs.checkout((rel_path,version))
+	with open(rel_path, 'r') as version_file:
+		json_data = version_file.read()
+	rcs.checkin(rel_path)
+	return json.loads(json_data)
--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/restore_data.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/restore_data.py
@@ -5,7 +5,71 @@ from django.core import serializers
 from bson.json_util import dumps,loads,object_hook
 import json
 class Command(BaseCommand):
-    help = "Based on "
+    help = "setup the initial database"
+    def handle(self,*args,**options):
+		file_names = ['GSystemType.json','factorydata','metatype.json','AttributeType.json','RelationType.json']
+		for i in file_names:	
+			PROJECT_ROOT = os.path.abspath(os.path.dirname(os.pardir))
+			refcatpath = os.path.join(PROJECT_ROOT + '/GRef.cat./' + i)
+			path_val = os.path.exists(refcatpath)
+			if path_val:
+				with open(refcatpath, 'r') as json_file:
+					json_data = json_file.read()
+				json_documents_list = json.loads(json_data)
+				
+			# Process data in proper format
+			
+			
+			# process data based on the structure type			
+			parsed_json_document = []
+			for j,json_data in enumerate(json_documents_list):
+				converted_data = node_collection.from_json(json.dumps(json_data))
+				if i ==  'GSystemType.json':
+					node = node_collection.collection.GSystemType()
+				elif i == 'factorydata.json':
+					if json_data['_type'] == 'Group':
+						node = node_collection.collection.Group()
+					else:					
+						node = node_collection.collection.GSystem()
+				elif i == 'metatype.json':
+					node = node_collection.collection.MetaType()
+				elif i == 'RelationType.json': 
+					node = node_collection.collection.RelationType()
+				elif i == 'AttributeType.json':
+					node = node_collection.collection.AttributeType()
+				if converted_data['name'] not in ['NUSSD Course','QuizItem']:
+					for key, values in converted_data.items():
+						if values and type(values) == list:
+							oid_ObjectId_list = []
+							oid_list_str = values.__str__()
+							if '$oid' in oid_list_str:
+								for oid_dict in values:
+									oid_ObjectId = ObjectId(oid_dict['$oid'])
+									oid_ObjectId_list.append(oid_ObjectId)
+
+								node[key] = oid_ObjectId_list
+							
+						else:
+							if key != "_type":
+								node[key] = values
+					node.save()
+					
+						
+
+
+
+
+
+
+
+
+
+
+
+
+#temprorily blocked code
+'''
+
 
     def handle(self, *args, **options):
 
@@ -30,9 +94,9 @@ def get_json_file(filepath):
 
     history_manager = HistoryManager()
     rcs = RCS()
-    version_no = '1.15'
+    version_no = '1.13'
     path = '55b0da90f87f070bb518c33e.json'
-    rcs.checkout(filepath)
+    rcs.checkout((filepath,version_no))
     with open(path, 'r') as version_file:
             json_data = version_file.read()
     #print json_data
@@ -40,23 +104,27 @@ def get_json_file(filepath):
     json_data = json.dumps(json_dict)
     # Converts the json-formatted data into python-specific format
     doc_obj =  loads(json_data)
-    #doc_obj = node_collection.from_json(json_data)
+    doc_obj =  dumps(doc_obj)
+    doc_obj = node_collection.from_json(doc_obj)
+    print doc_obj
+    rcs.checkin((filepath,version_no))
     #parse the data for perfect json node creation
     return doc_obj  
-
+'''
     
 
 def check_data(data):
     ''' method to check if all the system types relations and pre dependencies
         are present in the system before pushing them to the current system 
     '''
-    print data
     temp_node = node_collection.collection.Group()
     temp_dict = {}
     ''' dictionary creation '''
+    '''
     for key, values in data.items():
         temp_dict[key] = values
 
     temp_node.update(temp_dict)
-    temp_node.save()	
+    '''
+    #temp_node.save()	
 	
