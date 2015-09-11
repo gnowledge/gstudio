59fa44e36e7d9cc41c4f08c4271f2b1364a5b0e0
katkamrachana <katkam.rachana@gmail.com>
2015-09-11 18:21:45 +0530

    thread update on file/image/video edit and on ceg creation, default gattr discussion_enable will be False

--- gnowsys-ndf/gnowsys_ndf/ndf/views/group.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/group.py
@@ -1189,7 +1189,10 @@ class CreateCourseEventGroup(CreateEventGroup):
             new_gsystem.content_org = node.content_org
             new_gsystem.content = node.content
             new_gsystem.save()
-            return_status = create_thread_for_node(request, group_obj._id, new_gsystem)
+            discussion_enable_at = node_collection.one({"_type": "AttributeType", "name": "discussion_enable"})
+            create_gattribute(new_gsystem._id, discussion_enable_at, False)
+            new_gsystem.reload()
+            # return_status = create_thread_for_node(request, group_obj._id, new_gsystem)
             return new_gsystem
 
         except Exception as e:
