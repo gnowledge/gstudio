2a041a2f3765397ab8aa8b4a1a4c14d32b117d64
makfire <mukeshpandey15@hotmail.com>
2015-10-11 03:10:57 +0530

    code to chain data stream done

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/fetch_syncdata.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/fetch_syncdata.py
@@ -12,6 +12,8 @@ from django_mailbox.models import Mailbox
 from gnowsys_ndf.settings import SYNCDATA_FETCHING_EMAIL_ID, SYNCDATA_FETCHING_EMAIL_ID_PASSWORD, SYNCDATA_FETCHING_IMAP_SERVER_ADDRESS
 from gnowsys_ndf.ndf.views.mailclient import server_sync
 import os
+import traceback
+import filecmp
 
 def connected_to_internet(url='http://www.google.com/', timeout=2):
 	try:
@@ -26,69 +28,71 @@ class Command(BaseCommand):
 	help = 'Function to fetch mails from the MailID specified in settings.py '
 
 	def handle(self, *args, **kwargs):
-		if connected_to_internet() is False:
-			return None
-		received_registries = []
-		received_data = []	
-		email = SYNCDATA_FETCHING_EMAIL_ID.replace('@','%40')
-		metabox = Mailbox()
-		metabox.name = 'Metabox'
-		metabox.uri = 'imap+ssl://' + email + ':' + SYNCDATA_FETCHING_EMAIL_ID_PASSWORD + '@' + SYNCDATA_FETCHING_IMAP_SERVER_ADDRESS + '?archive=METABOX_SYNCDATA'
+                if connected_to_internet() is False:
+                        return None
+                received_registries = []
+                received_data = []	
+                email = SYNCDATA_FETCHING_EMAIL_ID.replace('@','%40')
+                metabox = Mailbox()
+                metabox.name = 'Metabox'
+                metabox.uri = 'imap+ssl://' + email + ':' + SYNCDATA_FETCHING_EMAIL_ID_PASSWORD + '@' + SYNCDATA_FETCHING_IMAP_SERVER_ADDRESS + '?archive=METABOX_SYNCDATA'
 		
-		try:
-			metabox.get_connection()
-		except IMAP4.error as error_obj:
-			print "Syncdata emailid, password or imap server address improperly configured! Please check settings.py"
-			print str(error_obj)
-			return 'Syncdata emailid, password or imap server address improperly configured! Please check settings.py'
-		except Exception as error_obj:
-			print 'Some error occured while trying to establish connection with : %s for fetching syncdata', SYNCDATA_FETCHING_EMAIL_ID
-			print str(error_obj)
-			return str(error_obj)
+                try:
+                        metabox.get_connection()
+                except IMAP4.error as error_obj:
+                        print "Syncdata emailid, password or imap server address improperly configured! Please check settings.py"
+                        print str(error_obj)
+                        return 'Syncdata emailid, password or imap server address improperly configured! Please check settings.py'
+                except Exception as error_obj:
+                        print 'Some error occured while trying to establish connection with : %s for fetching syncdata', SYNCDATA_FETCHING_EMAIL_ID
+                        print str(error_obj)
+                        return str(error_obj)
 
-		metabox.save()
-		try:
-			if connected_to_internet:
-				syncdata_mails_list = metabox.get_new_mail()
-				if syncdata_mails_list:
-					for mail in syncdata_mails_list:
-						text,data,serverdir = process_mails(mail)
+                metabox.save()
+                try:
+                        if connected_to_internet:
+                                syncdata_mails_list = metabox.get_new_mail()
+                                if syncdata_mails_list:
+                                        for mail in syncdata_mails_list:
+                                                text,data,serverdir = process_mails(mail)
                                                 #received_registries += serverdir
                                                 if serverdir not in received_registries:         
                                                         received_registries.append(serverdir)                            
-						received_data += data	
-                                        result=check_and_chain_registry(received_registries)
-                                        if result == True:
-					   server_sync(received_data)
-                                           delete(received_registries)		
-				else:
-					print 'No new mails received'
-			metabox.delete()
-		except Exception as error:
-			#delete temporary mailbox from database
-			metabox.delete()
-			print "The following error occured while fetching syncdata mails: "
-			print str(error)
+                                                received_data += data	
+                                        result=chain_and_check_registry(received_registries)
+                                        print result
+                                        if result:
+                                                server_sync(result)
+                                                delete(result)		
+                                else:
+                                        print 'No new mails received'
+                        metabox.delete()
+                except Exception as error:
+                        #delete temporary mailbox from database
+                        metabox.delete()
+                        print "The following error occured while fetching syncdata mails: "
+                        print str(error)
+                        traceback.print_exc()
 
 def process_mails(mail):
     #place files in server related directory
-	all_attachments = mail.attachments.all()
-	all_attachments_path = []
-	json_file_path = ''
-	file_object_path = ''
+    all_attachments = mail.attachments.all()
+    all_attachments_path = []
+    json_file_path = ''
+    file_object_path = ''
 
 
-	''' Code reads all the attachmnet of the single mail '''    
-	''' Code to decrypt every attachment and create a list with the file paths of decrypted attachments'''
-	list_of_decrypted_attachments = []
-	for attachment in all_attachments:
-		filename = attachment.document.path
-		op_file_name = filename.split('_sig')[0]            
-		print 'output file name of decrypted attachment : \n %s' % op_file_name            
-		command = 'gpg --output ' + op_file_name + ' --decrypt ' + filename
-		std_out= subprocess.call([command],shell=True)
-		list_of_decrypted_attachments.append(op_file_name)
-    	return fetch_registry(list_of_decrypted_attachments,mail)    
+    ''' Code reads all the attachmnet of the single mail '''    
+    ''' Code to decrypt every attachment and create a list with the file paths of decrypted attachments'''
+    list_of_decrypted_attachments = []
+    for attachment in all_attachments:
+        filename = attachment.document.path
+        op_file_name = filename.split('_sig')[0]            
+        print 'output file name of decrypted attachment : \n %s' % op_file_name            
+        command = 'gpg --output ' + op_file_name + ' --decrypt ' + filename
+        std_out= subprocess.call([command],shell=True)
+        list_of_decrypted_attachments.append(op_file_name)
+    return fetch_registry(list_of_decrypted_attachments,mail)
 
 def fetch_registry(file_list,mail):
     registryfile = [i for i in file_list if i.find(".txt")!= -1]
@@ -98,7 +102,6 @@ def fetch_registry(file_list,mail):
     serverid = subject.split('_')[0]
     registryno = subject.split('_')[2]
     subject_split = subject.split('_')[-6:]
-    print subject_split
     folder_name = "_".join(subject_split)
     if os.path.exists(serverid) == False:
         os.mkdir(serverid)
@@ -116,47 +119,87 @@ def fetch_registry(file_list,mail):
     subprocess.Popen(cp,stderr=subprocess.STDOUT,shell=True)	
     return  registryfile,datafile,server_folder
 
-def check_and_chain_registry(file_list):
-    #os.popen("grep -r 'view' *").read()
-    #code considers that files are coming from single server only
-    filepath = ""
-    directories = []
-    missing_data_files = []
-    print file_list
+def chain_and_check_registry(file_list):
+    #file_list is list of servers from where files are recived
+    all_file_path = []
     for i in file_list:
-        for dir_,_i,files in os.walk(i):                
-            for filename in files:
-                if filename.find(".txt") != -1:
-                   filepath = os.path.join(dir_,filename)
-                   break
-    #this line should look into individual servers                   
-    directories += os.listdir(file_list[0])               
-    registryopen = open(filepath)
-    #file open
-    directories = [(i.split('_')[-1]) for i in directories]
-    print directories
-    for i in  registryopen:
-        line = i
-        objectid =  line[line.find("_id")+4:line[line.find("_id")+4:].find(",")+line.find("_id")+4]
-        objectid = objectid.strip('\'')
-        if objectid in directories:
-            print "do nothing match happend"
-        else:
-            print "didnot find the match"
-            missing_data_files.append(objectid)
-    registryopen.close()
-
-    if missing_data_files:
-        for i in missing_data_files:
-            request_for_packet(i)           
-        return False
-    else:
-        return True    
-
+        print "the list of files",i    
+        #Get all the dir in the server folder
+        dir_list = get_directories(i)
+        #chain and check the sequence of th files
+        file_list = chain_registries(dir_list,i)
+        all_file_path += file_list
+    return all_file_path    
+def get_directories(path):
+    scanned_file = []    
+    for dir_,_i,files in os.walk(path):
+        scanned_file.append(dir_)
+    return scanned_file    
+def chain_registries(dir_list,serverdir):
+    create_stream = []
+    streams = []
+    continue_stream = True
+    filepath_list = []
+    temp_list = []
+    readed = []
+    if not os.path.exists(serverdir + "registries/"):
+        os.mkdir(serverdir + "registries/")
+    #method would chain the received registries
+    #And store them to folder called registries
+    #chaining which is incomplete wont be having their registry
+    dir_list.remove(serverdir)
+    sort_list = sorted(dir_list)
+    for k,i in enumerate(sort_list):
+        text = ""
+        json = ""
+        for files in os.listdir(i):
+            if files.find(".txt") != -1:
+               text = files
+            if files.find(".json") != -1:
+                json = files        
+        filepath = os.path.join(i,text)
+        current_file = open(filepath).readlines()
+        if 'Start\n' in current_file or 'Start\r\n' in current_file or create_stream:
+            create_stream.append(current_file[1])
+            temp_list.append((os.path.join(i,json)))    
+            if readed and create_stream:
+                if readed[2] == current_file[1]:
+                    if  'End' in current_file:
+                        filepath_list += temp_list
+                        print "coming here"    
+                        with open(serverdir + "registries/"+str("%06d" % k)+".txt",'w') as outfile:
+                            for i in create_stream:
+                                outfile.write(i)
+                        create_stream = []
+                        current_file = []
+                        temp_list = []
+                else:
+                    #break the stream if chaining breaks
+                    create_stream = []
+                    current_file = []
+                    temp_list = []    
+        #condition when their exist only single file
+        if ('Start\n' in current_file or 'Start\r\n' in current_file )and 'End' in current_file:
+            with open(serverdir + "registries/"+str("%06d" % k)+".txt",'w') as outfile:
+                for line in create_stream:
+                    outfile.write(line)
+            create_stream = []
+            current_file = [] 
+            temp_list = []            
+            filepath_list.append((os.path.join(i,json)))
+        if not create_stream:
+             if 'Start\r\n' not in current_file:
+                current_file = []           
+        readed = current_file
+    return filepath_list            
+                            
 def delete(file_list):
     #delete files which all are synced
     for  i in file_list:
-        shutil.rmtree(i)
+        file_list = i.split("/")[0:-1]
+        path = "/".join(file_list)
+        print path
+        shutil.rmtree(path)
 
 def request_for_packet(packet_name):
     #print coming here
