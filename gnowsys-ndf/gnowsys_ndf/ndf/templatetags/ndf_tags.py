dff26ef91daa02ab1fd58eba4267da14ca1f220e
Kedar Aitawdekar <kedar2a@gmail.com>
2015-11-05 16:43:15 +0530

    Updated ndf_tag for getting filter parameteres

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -1320,6 +1320,7 @@ def get_all_resources(request,node_id):
         if node._id not in obj_set:
                 obj_set.append(node._id)
                 for item in result_set:
+                        # print "\n=====",item.keys()
                         obj_set.extend(item.keys())
         resources={'Images':[],'Documents':[],'Audios':[],'Videos':[],'Interactives':[], 'eBooks':[]}
         for each in obj_set:
@@ -2776,7 +2777,7 @@ def get_filters_data(gst_name):
 	static_mapping = {
                     "educationalsubject": GSTUDIO_RESOURCES_EDUCATIONAL_SUBJECT,
                     "language": GSTUDIO_RESOURCES_LANGUAGES,
-                    # "educationaluse": GSTUDIO_RESOURCES_EDUCATIONAL_USE,
+                    "educationaluse": GSTUDIO_RESOURCES_EDUCATIONAL_USE,
                     "interactivitytype": GSTUDIO_RESOURCES_INTERACTIVITY_TYPE,
                     # "educationalalignment": GSTUDIO_RESOURCES_EDUCATIONAL_ALIGNMENT,
                     "educationallevel": GSTUDIO_RESOURCES_EDUCATIONAL_LEVEL,
@@ -2790,16 +2791,25 @@ def get_filters_data(gst_name):
 
 	filter_dict = {}
 
-	gst = node_collection.one({'_type':"GSystemType", "name": unicode(gst_name)})
+	# gst = node_collection.one({'_type':"GSystemType", "name": unicode(gst_name)})
+	gst = node_collection.one({'_type':"GSystemType", "name": unicode('File')})
 	poss_attr = gst.get_possible_attributes(gst._id)
+	# print "============", gst.name
 
-	exception_list = ["educationaluse"]
+	filter_parameters = []
+	# filter_parameters = GSTUDIO_FILTERS.get('File', [])
+	filter_parameters = GSTUDIO_FILTERS.get(gst_name, [])
+	# print filter_parameters
+
+	exception_list = ["interactivitytype"]
 
 	for k, v in poss_attr.iteritems():
 
-		if (k in exception_list) or not static_mapping.has_key(k):
+		# if (k in exception_list) or not static_mapping.has_key(k):
+		if (k in exception_list) or (k not in filter_parameters):
 			continue
 
+		# print k
 		filter_dict[k] = {
 	    					"data_type": v["data_type"].__name__,
 	    					"altnames": v['altnames'],
