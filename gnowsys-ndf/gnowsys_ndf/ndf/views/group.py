b6fe8079f64f3cc6e08ec965cf9b3d4017ea7b1e
katkamrachana <katkam.rachana@gmail.com>
2015-11-08 01:15:07 +0530

    list subgroups in group dashboard

--- gnowsys-ndf/gnowsys_ndf/ndf/views/group.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/group.py
@@ -1770,7 +1770,7 @@ def group_dashboard(request, group_id=None):
     selected = request.GET.get('selected','')
     group_obj = get_group_name_id(group_id, get_obj=True)
     if group_obj and group_obj.post_node:
-        subgroups_cur = node_collection.find({'_id': {'$in': group_obj.post_node}, 'member_of': group_gst._id})
+        subgroups_cur = node_collection.find({'_id': {'$in': group_obj.post_node}, 'member_of': group_gst._id, 'edit_policy': {'$ne': "EDITABLE_MODERATED"}})
 
     if not group_obj:
       group_obj=node_collection.one({'$and':[{'_type':u'Group'},{'name':u'home'}]})
