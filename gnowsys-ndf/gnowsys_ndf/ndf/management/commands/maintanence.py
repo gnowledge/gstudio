cc4721524c831f2fa3fae09f26b5ab1467868054
makfire <satej5999@gmail.com>
2015-11-20 15:32:21 +0530

    rotated log shifted to /data folder

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/maintanence.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/maintanence.py
@@ -14,8 +14,20 @@ class Command(BaseCommand):
         print "Mongo Log Rotated."
 
         #Rotate Registry and Recived File
+        #code to create folders in /data Registry
+        if not os.path.exists('/data/Registry'):
+            os.mkdir('/data/Registry')
+
+        if not os.path.exists('/data/receiveddata'):
+            os.mkdir('/data/receiveddata')
+
+        #code written with docker in consideration
+        Registry_Log_path =  '/data/Registry'
+        Recived_data_path   =  '/data/receiveddata'
+
         root_path =  os.path.abspath(os.path.dirname(os.pardir))
         tym_scan =  os.path.join(root_path, 'Last_Scan.txt') 
+
         if os.path.exists(tym_scan):
             file_output = open(tym_scan)
             last_scan = file_output.readline()
@@ -33,23 +45,27 @@ class Command(BaseCommand):
         receivedfile =  os.path.join(root_path, 'receivedfile')
         recv_filename  = "receivedfile." + str(timestamp)
 
+        filename = os.path.join(Registry_Log_path,filename)
+        recv_filename = os.path.join(Recived_data_path,recv_filename)
+
+
         #time = time - datetime.timedelta(minutes=5)
         #Rotate Registry File
 
-        c = os.popen("cat %s|awk '$0  < \"%s\"' >  %s" % (str(registry_path),str(time),str(filename)))
-        c.close()
+        cmd = os.popen("cat %s|awk '$0  < \"%s\"' >  %s" % (str(registry_path),str(time),str(filename)))
+        cmd.close()
         
         d = os.popen("sed -i '1,/$TIME/d' %s" % str(registry_path) )
         d.close()
-        print "Registry.txt Rotated"
+        print "Registry.txt Rotated."
         #Rotate recived file information
 
-        c = os.popen("cat %s|awk '$0  < \"%s\"' >  %s" % (str(receivedfile),str(time),str(recv_filename)))
-        c.close()
+        cmd = os.popen("cat %s|awk '$0  < \"%s\"' >  %s" % (str(receivedfile),str(time),str(recv_filename)))
+        cmd.close()
         
         d = os.popen("sed -i '1,/$TIME/d' %s" % str(receivedfile) )
         d.close()
-        print "receivedfile Rotated"
+        print "Receivedfile Rotated."
 
 
         
