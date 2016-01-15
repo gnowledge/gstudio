f6e3aaf8ff63f38f234a350fbe89de447c868111
saurabh <saurabhbharswadkar7@gmail.com>
2016-01-15 11:29:34 +0530

    prompt msg changed

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckPlugins/addImage/plugin.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckPlugins/addImage/plugin.js
@@ -31,7 +31,11 @@ CKEDITOR.plugins.add('addImage',
                             // var locationURL = 'http://' + location.host;
                             var locationURL = window.location.origin
                             var completeURL = locationURL + imageURL
-                            var width = prompt("Please enter width",'100');
+                            var width = prompt("Please enter width in px",'100');
+                            if(width == null)
+                            {
+                                return false;
+                            }
                             CKEDITOR.instances[textAreaId].insertHtml('<img width="'+  width + '"src=' + completeURL +  '>' + '</img>');
                             $('#group_imgs_on_modal').foundation('reveal', 'close');
                             return false;
