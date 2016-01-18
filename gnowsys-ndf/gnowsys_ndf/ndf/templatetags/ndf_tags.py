3aea435eb0c63bc86ac8927d9a172d7b4eec9c3c
katkamrachana <katkam.rachana@gmail.com>
2016-01-18 15:44:15 +0530

    conflicts resolved

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -1293,37 +1293,36 @@ def get_event_type(node):
 @get_execution_time
 @register.assignment_tag
 def get_url(groupid):
-     
-    node = node_collection.one({'_id': ObjectId(groupid) }) 
-    
-    if node._type == 'GSystem':
+	node = node_collection.one({'_id': ObjectId(groupid) }) 
+
+	if node._type == 'GSystem':
 
 		type_name = node_collection.one({'_id': node.member_of[0]})
-                if type_name.name == 'Exam' or type_name.name == "Classroom Session":
-                   return ('event_app_instance_detail')
-                if type_name.name == 'Quiz':
-                   return 'quiz_details'
-                elif type_name.name == 'Page':
-                   return 'page_details' 
-                elif type_name.name == 'Theme' or type_name == 'theme_item':
-                   return 'theme_page'
-                elif type_name.name == 'Forum':
-	                 return 'show'
-                elif type_name.name == 'Task' or type_name.name == 'task_update_history':
-	                 return 'task_details'
-                else:
-	                  return 'None'    
-    elif node._type == 'Group' :
-                    return 'group'
-    elif node._type == 'File':
+		if type_name.name == 'Exam' or type_name.name == "Classroom Session":
+			return ('event_app_instance_detail')
+		if type_name.name == 'Quiz':
+			return 'quiz_details'
+		elif type_name.name == 'Page':
+			return 'page_details' 
+		elif type_name.name == 'Theme' or type_name == 'theme_item':
+			return 'theme_page'
+		elif type_name.name == 'Forum':
+			return 'show'
+		elif type_name.name == 'Task' or type_name.name == 'task_update_history':
+			return 'task_details'
+		else:
+			return 'None'    
+	elif node._type == 'Group':
+		return 'group'
+	elif node._type == 'File':
 		if (node.mime_type) == ("application/octet-stream"): 
 			return 'video_detail'       
 		elif 'image' in node.mime_type:
 			return 'file_detail'
 		else:
 			return 'file_detail'
-    else:
-			return 'group'
+	else:
+		return 'group'
 
 @get_execution_time
 @register.assignment_tag
@@ -3215,3 +3214,12 @@ def get_user_quiz_resp(node_obj, user_obj):
 		qip_sub = get_attribute_value(qip._id,'quizitempost_user_submitted_ans')
 		recent_ans = qip_sub[-1]
 		return recent_ans.values()[0]
+
+@get_execution_time
+@register.assignment_tag
+def get_node_views(request, node_obj):
+	# print "\n\n request", request.META.PATH_INFO
+	benchmark_collection = db[Benchmark.collection_name]
+	cursor = benchmark_collection.find({'name':node_obj.url,'calling_url':{'$regex':str(node_obj.url) + "/"+str(node_obj._id)}})
+	print "\n\n cursor.count() --- ",cursor.count()
+	return cursor.count()
