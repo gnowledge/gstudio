18b39a24f0f8ba5f0fc09e6ce82607ae19eafc7e
katkamrachana <katkam.rachana@gmail.com>
2016-02-01 15:49:11 +0530

    Adding notes from notebook tab

--- gnowsys-ndf/gnowsys_ndf/ndf/views/page.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/page.py
@@ -393,6 +393,9 @@ def create_edit_page(request, group_id, node_id=None):
 
         # To fill the metadata info while creating and editing page node
         metadata = request.POST.get("metadata_info", '')
+        if "CourseEventGroup" in group_obj.member_of_names_list and blog_type and GSTUDIO_SITE_NAME == "clix":
+            return HttpResponseRedirect(reverse('course_notebook', kwargs={'group_id': group_id}))
+
         if ce_id or res or program_res:
             url_name = "/" + group_name + "/" + str(page_node._id)
             if ce_id:
