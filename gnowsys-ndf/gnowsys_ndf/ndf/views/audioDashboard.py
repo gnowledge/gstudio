76d9857a7c5ac0587b2182c21b41636895463452
saurabh <saurabhbharswadkar7@gmail.com>
2016-03-15 16:06:29 +0530

    commented code deleted

--- gnowsys-ndf/gnowsys_ndf/ndf/views/audioDashboard.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/audioDashboard.py
@@ -27,18 +27,6 @@ def audioDashboard(request, group_id, audio_id=None):
     '''
     fetching audio files acording to group name
     '''
-    # ins_objectid  = ObjectId()
-    # if ins_objectid.is_valid(group_id) is False :
-    #     group_ins = node_collection.find_one({'_type': "Group", "name": group_id})
-    #     auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #     if group_ins:
-    #         group_id = str(group_ins._id)
-    #     else :
-    #         auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #         if auth :
-    #             group_id = str(auth._id)
-    # else :
-    #     pass
     try:
         group_id = ObjectId(group_id)
     except:
