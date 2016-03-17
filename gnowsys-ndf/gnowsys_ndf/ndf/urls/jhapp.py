db0caf20dab4d5e77223e10df9c24a801b654904
saurabh <saurabhbharswadkar7@gmail.com>
2016-03-17 15:57:25 +0530

    pagination template added in gallery and raw materials

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/jhapp.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('gnowsys_ndf.ndf.views.jhapp',
-			url(r'^[/]$', 'get_jhapp_apps', name='get_jhapp_apps'),
-                        
-)
