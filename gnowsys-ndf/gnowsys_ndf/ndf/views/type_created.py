46e7d2606ec9df5cb3bcecafd5e2ee2b37576d7a
stestrepo <sayali4294@yahoo.co.in>
2016-03-25 15:31:56 +0530

    code for get_possible ats and rts added

--- gnowsys-ndf/gnowsys_ndf/ndf/views/type_created.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/type_created.py
@@ -50,33 +50,32 @@ def default_template(request,group_id,node=None):
     except:
         group_name, group_id = get_group_name_id(group_id)
 
-    gst = node_collection.find({'_type':'GSystemType'})
+    gs = node_collection.find({'_type':'GSystem'})
 
     option_list=[]
     
-    for e in gst :
+    for e in gs :
         option_list.append(e.name)
         # opt_list.append(e)
 
-    class_name="GSystemType"
+    # class_name="GSystemType"
     # select = "Exam"
-    class_structure = eval(class_name).structure
-    print class_structure
+    # class_structure = eval(class_name).structure
+    # print class_structure
+    # gst = node_collection.find({'_type':'GSystemType'})
 
+# notes: find out wt is gs structure and how saving can be done apply save button to the form and call ndf tag to pass the url
 
-    gst = node_collection.find({'_type':'GSystemType'})
+    new_instance_type = None
+
     gs = node_collection.find({'_type':'GSystem'})
 
     gs_sys = "GSystem"
+    class_structure =  eval(gs_sys).structure
     gs_struc =  eval(gs_sys).structure
     print "gs_sys\n",gs_struc,"\n\n\n"
-
     
-
-# notes: fiind out wt is gs structure and how saving can be done apply save button to the form and call ndf tag to pass the url
-
-
-    # gst_node
+# For Display
 
     basic_list = ['Name : ', 'Alternate Name : '];
 
@@ -86,11 +85,75 @@ def default_template(request,group_id,node=None):
         # print st_name
         node_gs = node_collection.one({'$and':[{'_type': u'GSystemType'},{'name':st_name}]})
         # print node_gs
+
+        node_gs_id = node_gs._id
+        print node_gs_id
+
         ats = node_gs.attribute_type_set
         # print ats
         rts = node_gs.relation_type_set
+        # print rts
+
+
+
+        # user_id = 1
+
+        node = node_collection.find_one({'$and':[{'_type': u'GSystem'},{'name':st_name}]})
+        
+        gsys_node = node_collection.collection.GSystem()
+        
+        gsys_node.name = unicode(st_name)
+        print gsys_node.name        
+
+        # gsys_node.created_by = user_id
+        # gsys_node.modified_by = user_id
+        # if user_id not in gsys_node.contributors:
+          # gsys_node.contributors.append(user_id)
+
+        # gs_city = node_collection.one({'_type':'GSystemType','name':'City'})
+        # if gs_city:
+          # gs_type_id = gs_city._id
+
+        # if gs_type_id:
+          # gsys_node.member_of.append(gs_type_id)
+        
+        if node_gs_id:
+          gsys_node.member_of.append(node_gs_id)
+
+        # if user_id not in gsys_node.contributors:
+          # gsys_node.contributors.append(user_id)
+
+
+        # gsys_node.save()
+        print gsys_node.member_of
+
+        # gsys_node = node_collection.collection.GSystem()
+        # gsys_node.name = unicode(st_name)
+
+        pos_ats = gsys_node.get_possible_attributes(node_gs_id)
+
+        print pos_ats
+
+        pos_rts = gsys_node.get_possible_relations(node_gs_id)
+
+        print pos_rts
+
+        for key,value in pos_ats.iteritems():
+            print value['altnames'],value['_id']
+        for key,value in pos_rts.iteritems():
+            print value['altnames'],value['_id']
+
+
+
+
+
+
+
+        # pos_ats = node.get_possible_attributes(node_gs._id)
+        # print pos_ats , "??////////////////???/ \n\n\n\n"
+        
     else:
-        st_name = "Exam"
+        st_name = " "
         # print st_name
         node_gs = node_collection.one({'$and':[{'_type': u'GSystemType'},{'name':st_name}]})
         # print node_gs
@@ -101,11 +164,41 @@ def default_template(request,group_id,node=None):
 
 
 
-    print node,"\n\n\n\n\n\n\n"
+    print "\n\n\n\n\n\n\n"
 
 
 
-    # gst_exam = node_collection.one({'_type': 'GSystemType', 'name':'Exam'})
+
+    newdict = {}
+
+    # if node_id:
+    #     new_instance_type = node_collection.one({'_type': unicode(gs_sys), '_id': ObjectId(node_id)})
+    # else:
+
+    new_instance_type = eval("node_collection.collection"+"."+gs_sys)()
+
+
+
+    # if request.method=="POST":
+        # for key,value in class_structure.items():
+
+
+
+    # If GET request ---------------------------------------------------------------------------------------
+    # for key,value in class_structure.items():
+
+            # newdict[key] = [value, new_instance_type[key]]
+
+    # class_structure = newdict
+
+
+
+
+
+
+
+
+
 
 
 
@@ -124,6 +217,6 @@ def default_template(request,group_id,node=None):
 
 
     template = "ndf/basic_temp.html"
-    variable = RequestContext(request, {'group_id':group_id,'groupid':group_id ,'basic_list':basic_list, 'ats':ats , 'rts':rts , 'node_gs':node_gs  })
+    variable = RequestContext(request, {'group_id':group_id,'groupid':group_id ,'basic_list':basic_list, 'ats':ats , 'rts':rts , 'node_gs':node_gs ,'pos_ats':pos_ats , 'pos_rts':pos_rts  })
 
     return render_to_response(template,variable)
