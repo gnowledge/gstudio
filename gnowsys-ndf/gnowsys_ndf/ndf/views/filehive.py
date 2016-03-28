dad064f533f16df6c9e624b5b835a7c43e5c986f
Kedar Aitawdekar <kedar2a@gmail.com>
2016-03-28 19:45:44 +0530

    added method for read_file() in filehive

--- gnowsys-ndf/gnowsys_ndf/ndf/views/filehive.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/filehive.py
@@ -11,12 +11,10 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from gnowsys_ndf.settings import MEDIA_ROOT, GSTUDIO_SITE_DEFAULT_LANGUAGE
-from gnowsys_ndf.ndf.models import node_collection, filehive_collection
+from gnowsys_ndf.ndf.models import node_collection, filehive_collection, gfs
 from gnowsys_ndf.ndf.models import GSystem
 from gnowsys_ndf.ndf.views.methods import get_language_tuple
 
-
-fs = HashFS(MEDIA_ROOT, depth=4, width=1, algorithm='sha256')
 gst_file = node_collection.one({'_type': u'GSystemType', 'name': u'File'})
 gst_file_id = gst_file._id
 
@@ -51,7 +49,16 @@ def write_file(request, group_id):
 		if collection_set:
 			first_obj.collection_set = collection_set
 			first_obj.save()
-			
+
 		return render_to_response('ndf/filehive.html', {
 			'group_id': group_id, 'groupid': group_id,
 			}, context_instance=RequestContext(request))
+
+
+def read_file(request, group_id):
+
+	all_fgs = node_collection.find({'_type': 'GSystem', 'member_of': gst_file._id})
+	return render_to_response('ndf/filehive_listing.html', {
+		'group_id': group_id, 'groupid': group_id,
+		'all_filehives': all_fgs
+		}, context_instance=RequestContext(request))
