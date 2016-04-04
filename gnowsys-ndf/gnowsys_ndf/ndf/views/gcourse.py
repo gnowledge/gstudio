39e764e21f16f97b2cc60c9e8c1668049e8004f5
saurabh <saurabhbharswadkar7@gmail.com>
2016-04-04 18:58:15 +0530

    views for create note and collection in gallery

--- gnowsys-ndf/gnowsys_ndf/ndf/views/gcourse.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/gcourse.py
@@ -2128,9 +2128,26 @@ def course_gallery(request, group_id,node_id=None,page_no=1):
         gstaff_users.extend(group_obj.group_admin)
         gstaff_users.append(group_obj.created_by)
         query = {'group_set': group_id, 'relation_set.clone_of':{'$exists': False}, '_type': "File", 'created_by': {'$nin': gstaff_users}}
-        files_cur = node_collection.find(query,{'name': 1, '_id': 1, 'fs_file_ids': 1, 'member_of': 1, 'mime_type': 1}).sort('created_at', -1)
+        
+        # files_cur = node_collection.find(query,{'name': 1, '_id': 1, 'fs_file_ids': 1, 'member_of': 1, 'mime_type': 1}).sort('created_at', -1)
         # print "\n\n Total files: ", files_cur.count()
+        files_cur = node_collection.find({ '$or' : [ {'group_set': group_id, 'relation_set.clone_of':{'$exists': False}, '_type': "File", 'created_by': {'$nin': gstaff_users},
+                                    # 'tags': {'$regex': u"raw", '$options': "i"}
+                                    },
 
+                                    {'member_of': {'$in': [GST_FILE._id, GST_PAGE._id]},
+                                                    'group_set': {'$all': [ObjectId(group_id)]},
+                                                    '$or': [
+                                                        {'access_policy': u"PUBLIC"},
+                                                        {'$and': [
+                                                            {'access_policy': u"PRIVATE"},
+                                                            {'created_by': request.user.id}
+                                                        ]
+                                                     }
+                                                    ],
+                                                    'collection_set': {'$exists': "true", '$not': {'$size': 0} }
+                                                
+                                    }]},{'name': 1, '_id': 1, 'fs_file_ids': 1, 'member_of': 1, 'mime_type': 1}).sort("last_update", -1)
         context_variables.update({'files_cur': files_cur})
         gallery_page_info = paginator.Paginator(files_cur, page_no, GSTUDIO_NO_OF_OBJS_PP)
         context_variables.update({'gallery_page_info':gallery_page_info,'coll_cur':coll_cur})
