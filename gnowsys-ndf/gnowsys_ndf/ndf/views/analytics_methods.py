7dc71317a3d075e7973203b935a37c07097f7958
katkamrachana <katkam.rachana@gmail.com>
2016-04-05 14:46:46 +0530

    Analytics method: get_total_modules_count, get_completed_modules_count

--- gnowsys-ndf/gnowsys_ndf/ndf/views/analytics_methods.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/analytics_methods.py
@@ -16,6 +16,46 @@ class AnalyticsMethods(object):
 		self.user_id = user_id
 		self.username = username
 
+	def get_total_modules_count(self):
+		t0 = time.time()
+
+		# print "\n\n get_total_units_count === "
+		if not hasattr(self,'course_section_event_gst'):
+			self.course_section_event_gst = node_collection.one({'_type': "GSystemType", 'name': "CourseSectionEvent"})
+
+		all_modules_cur = node_collection.find({'member_of': self.course_section_event_gst._id, 'group_set': ObjectId(self.group_id)},{'_id': 1})
+		t1 = time.time()
+		time_diff = t1 - t0
+		print "\n get_total_units_count == ", time_diff
+
+		return all_modules_cur.count()
+
+	def get_completed_modules_count(self):
+		t0 = time.time()
+
+		# print "\n\n get_completed_units_count === "
+		if not hasattr(self,'course_section_event_gst'):
+			self.course_section_event_gst = node_collection.one({'_type': "GSystemType", 'name': "CourseUnitEvent"},{'_id': 1})
+
+		if hasattr(self,'user_completed_obj_ids'):
+			completed_modules_cur = node_collection.find({'member_of': self.course_section_event_gst._id,
+			 'group_set': ObjectId(self.group_id), '_id': {'$in': self.user_completed_obj_ids}},{'_id': 1})
+		else:
+			if not hasattr(self,'result_status'):
+				self.result_status = get_course_completetion_status(self.group_obj, self.user_id, True)
+			if "completed_ids_list" in self.result_status:
+				str_ids = json.loads(self.result_status['completed_ids_list'])
+				self.user_completed_obj_ids = map(ObjectId, str_ids)
+				completed_modules_cur = node_collection.find({'member_of': self.course_section_event_gst._id,
+				 'group_set': ObjectId(self.group_id), '_id': {'$in': self.user_completed_obj_ids}},{'_id': 1})
+
+		t1 = time.time()
+		time_diff = t1 - t0
+		print "\n get_completed_units_count == ", time_diff
+
+		return completed_modules_cur.count()
+
+
 	def get_total_units_count(self):
 		t0 = time.time()
 
