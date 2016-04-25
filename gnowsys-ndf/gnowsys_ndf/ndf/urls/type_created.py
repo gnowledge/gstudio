8f0491695674e49ded6e6cc88eaaedd8353d7596
stestrepo <sayali4294@yahoo.co.in>
2016-04-25 17:40:10 +0530

    URL for edit

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/type_created.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/type_created.py
@@ -3,4 +3,9 @@ from django.conf.urls import *
 urlpatterns = patterns('gnowsys_ndf.ndf.views.type_created',
                        url(r'^$', 'type_created', name='type_created'),
                        url(r'^/default/(?P<node>[^/]+)$', 'default_template', name='basic_temp'),
+                       # url(r'^/default/(?P<node>[/]+)$', 'default_template', name='basic_temp'),
+
+                       url(r'^/default/(?P<node>[^/]+)/edit/(?P<node_id>[^/]+)$', 'default_template', name='basic_temp_edit'),
+                       # url(r'^/default/(?P<node>[^/]+)/edit/(?P<node_id>[^/]+)$', 'default_template', name='basic_temp_edit'),
+
 )
