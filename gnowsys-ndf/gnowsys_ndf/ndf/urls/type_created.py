c08d990dbf303309fdbcfa888fe6aba8d7ed920f
stestrepo <sayali4294@yahoo.co.in>
2016-04-29 13:48:47 +0530

    urls to fetch the graph node

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/type_created.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/type_created.py
@@ -4,4 +4,8 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.type_created',
                        url(r'^$', 'type_created', name='type_created'),
                        url(r'^/default/(?P<node>[^/]+)$', 'default_template', name='basic_temp'),
                        url(r'^/default/(?P<node>[^/]+)/edit/(?P<edit_node>[^/]+)$', 'default_template', name='basic_temp_edit'),
+                       url(r'^/graph/(?P<node_id>[^/]+)$', 'adminRenderConceptGraph', name='adminRenderConceptGraph'),
+                       url(r'^/graph/(?P<node_id>[^/]+)$', 'adminRenderCollectionGraph', name='adminRenderCollectionGraph'),
+                       url(r'^/graph/(?P<node_id>[^/]+)$', 'adminRenderDependencyGraph', name='adminRenderDependencyGraph'),
+
 )
