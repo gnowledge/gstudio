131ef27013f5cdd0704bcd06dc08613c9e678b81
katkamrachana <katkam.rachana@gmail.com>
2016-05-11 20:08:04 +0530

    New implementation: Alert message to user for any new comments added by others

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/discussion.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/discussion.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.discussion',
 					    url(r'^/(?P<node_id>[^/]+)/create_discussion$', 'create_discussion', name='create_discussion'),    
 					    url(r'^/(?P<node_id>[^/]+)/discussion_reply$', 'discussion_reply', name='discussion_reply'),
 					    url(r'^/(?P<node_id>[^/]+)/discussion_delete_reply$', 'discussion_delete_reply', name='discussion_delete_reply'),    
+					    url(r'^get_thread_comments_count/(?P<thread_node_id>[\w-]+)$', 'get_thread_comments_count', name='get_thread_comments_count'),
+
                         )
