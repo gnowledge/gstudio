b89842cf703713f41904e43daea4a79a6edfdb68
Kedar Aitawdekar <kedar2a@gmail.com>
2016-05-11 17:07:57 +0530

    Added url for updating current selected buddies

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/buddy.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/buddy.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.buddy',
 			url(r'^[/]$', 'list_buddy', name='list_buddy'),
+			url(r'^/update_buddies/$', 'update_buddies', name='update_buddies'),
 		)
