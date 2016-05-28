b2c750911dc30666d713804d53b287f91e75ae56
Kedar Aitawdekar <kedar2a@gmail.com>
2016-05-28 15:13:10 +0530

    Preventing already active user on site to be added as a buddy

--- gnowsys-ndf/gnowsys_ndf/ndf/views/buddy.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/buddy.py
@@ -14,7 +14,7 @@ try:
 except ImportError:  # old pymongo
     from pymongo.objectid import ObjectId
 
-from gnowsys_ndf.ndf.models import Buddy
+from gnowsys_ndf.ndf.models import Buddy, Author, DjangoActiveUsersGroup
 from gnowsys_ndf.ndf.models import node_collection
 from gnowsys_ndf.ndf.views.methods import get_execution_time
 
@@ -29,6 +29,7 @@ from gnowsys_ndf.ndf.views.methods import get_execution_time
 # GST_AUDIO = node_collection.one({'member_of': gapp_mt._id, 'name': GAPPS[3]})
 from gnowsys_ndf.settings import GSTUDIO_INSTITUTE_ID
 
+@login_required
 def list_buddy(request, group_id):
 
     '''
@@ -39,11 +40,23 @@ def list_buddy(request, group_id):
     # except:
     #     group_name, group_id = get_group_name_id(group_id)
 
-    all_inst_users = User.objects.filter(username__iendswith=GSTUDIO_INSTITUTE_ID)
-    all_inst_authors = node_collection.find({ '_type': u'Author', 'name': {'$regex': GSTUDIO_INSTITUTE_ID + '$'} })
-    # print all_inst_authors.count()
     buddies_authid_name_dict= request.session.get('buddies_authid_name_dict', {})
+    # print "buddies_authid_name_dict : ", buddies_authid_name_dict
     buddies_authid_list     = request.session.get('buddies_authid_list', [])
+    # print "buddies_authid_list : ", buddies_authid_list
+
+    # filter_authors = [ObjectId(auth_oid)for auth_oid in buddies_authid_list]
+
+    all_inst_users = User.objects.filter(username__iendswith=GSTUDIO_INSTITUTE_ID)
+    all_inst_authors = node_collection.find({
+                                            '_type': u'Author',
+                                            # '_id': {'$nin': filter_authors},
+                                            'name': {
+                                                '$regex': GSTUDIO_INSTITUTE_ID + '$'
+                                                },
+                                            'created_by': {'$ne': request.user.id}
+                                            })
+    # print all_inst_authors.count()
 
 
     template = 'ndf/buddy_list.html'
@@ -62,10 +75,40 @@ def list_buddy(request, group_id):
 def update_buddies(request, group_id):
 
     selected_buddies_list = eval(request.POST.get('selected_buddies_list', '[]'))
+    # print "=== selected_buddies_list : ", selected_buddies_list
+    selected_buddies_userids_list = Author.get_user_id_list_from_author_oid_list(selected_buddies_list)
+    selected_buddies_userids_set = set(selected_buddies_userids_list)
 
-    updated_buddies_authid_name_dict = []
+    updated_buddies_authid_name_dict = {}
+    already_active_userid_name_dict = {}
 
     buddies_authid_list = request.session.get('buddies_authid_list', [])
+    existing_buddies_userid_list = Author.get_user_id_list_from_author_oid_list(buddies_authid_list)
+    # print "=== buddies_authid_list : ", buddies_authid_list
+    # print "=== existing_buddies_userid_list : ", existing_buddies_userid_list
+    aa = selected_buddies_userids_set.intersection(set(existing_buddies_userid_list))
+    # print "999999999999900000000", aa
+
+    if selected_buddies_list:
+
+        sitewide_active_userids_list = DjangoActiveUsersGroup.get_all_user_set_ids_list()
+        sitewide_active_userids_set  = set(sitewide_active_userids_list)
+
+        already_active_user_ids = list(selected_buddies_userids_set.intersection(sitewide_active_userids_set) - set(aa))
+
+        if already_active_user_ids:
+            auth_cur = node_collection.find({'_type': u'Author', 'created_by': {'$in': already_active_user_ids} }, {'_id': 0, 'name': 1, 'created_by': 1} )
+                        # { b['_id'].__str__(): b['name'] for b in updated_buddies_cur}
+            if auth_cur:
+                already_active_userid_name_dict = {a['created_by']: a['name'] for a in auth_cur}
+            # print "==== already_active_userid_name_dict : ", already_active_userid_name_dict
+
+        selected_buddies_list = list(selected_buddies_userids_set - sitewide_active_userids_set) + list(aa)
+        # print "== sitewide_active_userids_set: ", sitewide_active_userids_set
+        # print "selected_buddies_list : ", selected_buddies_list
+        # print "== selected_buddies_userids_set: ", selected_buddies_userids_set
+
+        selected_buddies_list = Author.get_author_oid_list_from_user_id_list(user_ids_list=selected_buddies_list, list_of_str_oids=True)
 
     if selected_buddies_list or buddies_authid_list:
 
@@ -74,6 +117,7 @@ def update_buddies(request, group_id):
         active_buddy_auth_list = Buddy.update_buddies(request.user.id, request.session.session_key, selected_buddies_list)
         # print "\n\nactive_buddy_auth_list : ", active_buddy_auth_list
 
+        # ab: active buddy
         updated_buddies_cur = node_collection.find({
                                                     '_id': {
                                                         '$in': [ObjectId(ab) for ab in active_buddy_auth_list]
@@ -88,4 +132,11 @@ def update_buddies(request, group_id):
         request.session['buddies_authid_list']      = active_buddy_auth_list
         request.session['buddies_authid_name_dict'] = updated_buddies_authid_name_dict
 
-    return HttpResponse(json.dumps(updated_buddies_authid_name_dict))
+
+    result_dict = {
+                'buddies': updated_buddies_authid_name_dict,
+                'already_active': already_active_userid_name_dict
+            }
+    # print "=== result_dict : ", result_dict
+
+    return HttpResponse(json.dumps(result_dict))
