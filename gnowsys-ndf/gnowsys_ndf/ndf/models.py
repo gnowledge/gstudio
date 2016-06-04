e0e177bbad2f9de0ea5efcfb77cab6b78f639127
Kedar Aitawdekar <kedar2a@gmail.com>
2016-06-05 03:55:25 +0530

    Added method to know logged in user's status

--- gnowsys-ndf/gnowsys_ndf/ndf/models.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/models.py
@@ -2451,28 +2451,31 @@ class ActiveUsers(object):
     """docstring for ActiveUsers"""
 
     @staticmethod
-    def get_all_logged_in_users(list_of_ids=False):
+    def get_active_id_session_keys():
         # Query all non-expired sessions
         # use timezone.now() instead of datetime.now() in latest versions of Django
         sessions = Session.objects.filter(expire_date__gte=timezone.now())
-        uid_list = []
-        session_key_list = []
-        uid_session_key_dict = {}
+        # uid_list = []
+        # uid_list_append = uid_list.append
+        # session_key_list = []
+        userid_session_key_dict = {}
 
         # Build a list of user ids from that query
         for session in sessions:
             data = session.get_decoded()
-            user_id = data.get('_auth_user_id', None)
-            if user_id:
-                uid_list.append(user_id)
-                session_key_list.append(session.session_key)
-                uid_session_key_dict[user_id] = session.session_key
+            user_id = data.get('_auth_user_id', 0)
+            # if user_id:
+            # uid_list_append(user_id)
+            # session_key_list.append(session.session_key)
+            userid_session_key_dict[user_id] = session.session_key
 
-        # Query all logged in users based on id list
-        if list_of_ids:
-            return User.objects.filter(id__in=uid_list).values_list('id', flat=True)
-        else:
-            return User.objects.filter(id__in=uid_list)
+        return userid_session_key_dict
+
+        # # Query all logged in users based on id list
+        # if list_of_ids:
+        #     return User.objects.filter(id__in=uid_list).values_list('id', flat=True)
+        # else:
+        #     return User.objects.filter(id__in=uid_list)
 
 
 class DjangoActiveUsersGroup(object):
@@ -2939,7 +2942,7 @@ class Buddy(DjangoDocument):
             return { 'in': None, 'out': None }
 
 
-    def get_active_buddy_authid_list(self):
+    def get_active_authid_list_from_single_buddy(self):
 
         active_buddy_auth_list = []
 
@@ -2978,7 +2981,7 @@ class Buddy(DjangoDocument):
         '''
         Removes/releses all existing-active buddies.
         '''
-        active_buddy_authid_list = self.get_active_buddy_authid_list()
+        active_buddy_authid_list = self.get_active_authid_list_from_single_buddy()
 
         for each_buddy_authid in active_buddy_authid_list:
             self.get_latest_in_out_dict(self.buddy_in_out[each_buddy_authid])['out'] = datetime.datetime.utcnow()
@@ -2994,7 +2997,7 @@ class Buddy(DjangoDocument):
             - buddy object will be saved using .save() method.
         '''
 
-        active_buddy_authid_list = self.get_active_buddy_authid_list()
+        active_buddy_authid_list = self.get_active_authid_list_from_single_buddy()
 
         self = self.remove_all_buddies()
         self.ends_at = datetime.datetime.utcnow()
@@ -3032,6 +3035,48 @@ class Buddy(DjangoDocument):
 
 
     @staticmethod
+    def get_active_buddies_user_ids_list():
+
+        active_users = ActiveUsers.get_active_id_session_keys()
+        active_users_session_keys = active_users.values()
+        active_user_ids = active_users.keys()
+
+        Buddy.close_incomplete_buddies(active_users)
+        active_buddies = buddy_collection.find({
+                                    'session_key': {'$in': active_users_session_keys},
+                                    'ends_at': None
+                                })
+
+        active_buddy_auth_list = []
+        active_buddy_userids_list = active_user_ids
+
+        for each_buddy in active_buddies:
+            active_buddy_auth_list += each_buddy.get_active_authid_list_from_single_buddy()
+            active_buddy_userids_list.append(each_buddy['loggedin_userid'])
+
+        active_buddy_userids_list += Author.get_user_id_list_from_author_oid_list(active_buddy_auth_list)
+        active_buddy_userids_list = list(set(active_buddy_userids_list))
+
+        return active_buddy_userids_list
+
+
+    @staticmethod
+    def close_incomplete_buddies(active_users=None):
+
+        if not active_users:
+            active_users = ActiveUsers.get_active_id_session_keys()
+
+        active_users_session_keys = active_users.values()
+        incomplete_buddies = buddy_collection.find({
+                                    'session_key': {'$nin': active_users_session_keys},
+                                    'ends_at': None
+                                })
+
+        for each_incomplete_buddy in incomplete_buddies:
+            each_incomplete_buddy.end_buddy_session()
+
+
+    @staticmethod
     def update_buddies(loggedin_userid, session_key, buddy_auth_ids_list=[]):
         buddy_obj = buddy_collection.one({
                     'session_key': str(session_key),
@@ -3043,7 +3088,7 @@ class Buddy(DjangoDocument):
             buddy_obj = buddy_collection.collection.Buddy()
             buddy_obj = buddy_obj.get_filled_buddy_obj(loggedin_userid, session_key)
 
-        current_active_buddy_auth_list = buddy_obj.get_active_buddy_authid_list()
+        current_active_buddy_auth_list = buddy_obj.get_active_authid_list_from_single_buddy()
 
         if set(current_active_buddy_auth_list) == set(buddy_auth_ids_list):
             return current_active_buddy_auth_list
@@ -3076,7 +3121,7 @@ class Buddy(DjangoDocument):
 
                     buddy_obj.remove_buddy(each_buddy)
 
-        active_buddy_auth_list = buddy_obj.get_active_buddy_authid_list()
+        active_buddy_auth_list = buddy_obj.get_active_authid_list_from_single_buddy()
 
         if current_active_buddy_auth_list != active_buddy_auth_list:
             buddy_obj.save()
