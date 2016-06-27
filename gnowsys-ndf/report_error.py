39ce2b21aea525f89d2cf09bb335cd42790e61dc
g66shivam <shivam066gupta@gmail.com>
2016-06-27 16:32:10 +0530

    support for email acknowledgement added

---
+++ gnowsys-ndf/report_error.py
@@ -0,0 +1,9 @@
+error_message = {"Page name required":1,
+				"Page Content required":2,
+				"User details required":3,
+				"already exists":4,
+				"User does not exist":5,
+				"User authenticated":6,
+				"User not a member of this group":7,
+				"Group does not exist":8}
+
---
+++ gnowsys-ndf/send_page.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email import message
+
+def send_page(to_user,page_name,page_content):
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = "Page successfully created"
+	msg['From'] = "ps.mio.bits@gmail.com"
+	msg['To'] = to_user 
+	msg['Update'] = "False"
+	html = """\
+	<html>
+	<head></head>
+	<body>
+		<H1>{pg_name}</H1>
+		<H2>{pg_content}</H2>
+		<p>Hi!<br>
+			<font color="blue"> How are ssssyou?</font><br>
+			Here is the <a href="http://www.python.org">link</a> you wanted.
+		</p>
+	</body>
+	</html>
+	""".format(pg_name=page_name,pg_content=page_content)
+
+	msg.attach(MIMEText(html, 'html'))
+	smtpObj=smtplib.SMTP('smtp.gmail.com', 587)
+	smtpObj.ehlo()
+	smtpObj.starttls()
+	smtpObj.login('ps.mio.bits@gmail.com','1Guesswhat')
+	smtpObj.sendmail('ps.mio.bits@gmail.com',to_user,msg.as_string())
+	smtpObj.quit()
