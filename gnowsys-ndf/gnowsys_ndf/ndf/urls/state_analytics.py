945a047fc423174afccc8b5f49f9040f026702ec
deepvyas <vyasdeep.dv@@gmail.com>
2016-07-06 12:29:31 +0530

    added org_belongs_to_state_rel for fetching

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/state_analytics.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/state_analytics.py
@@ -5,6 +5,7 @@ from gnowsys_ndf.ndf.views import state_analytics
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.state_analytics',
 		url(r'^[/]$' ,'map_view',name='map_view'),
-		url(r'^[/]update_cities/$','add_organization',name='add_city'),
+		url(r'^[/]update_organization/(?P<node_id>[\w-]+)$','add_organization',name='add_organization'),
+		url(r'^[/]update_organization/$','add_organization',name='add_organization'),
 		url(r'^[/]fetch_organization/$','fetch_organization',name='fetch_organization'),
 	)
