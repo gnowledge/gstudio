17859b41d0cc1b4666b492f0c61b35a94fde6363
deepvyas <vyasdeep.dv@@gmail.com>
2016-07-06 18:20:31 +0530

    Modify state_rel left

--- gnowsys-ndf/gnowsys_ndf/ndf/views/state_analytics.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/state_analytics.py
@@ -31,6 +31,7 @@ def map_view(request,group_id):
 	return render_to_response("ndf/map_india.html",{ 'group_id':group_id, 'groupid':group_id}
 		, context_instance=RequestContext(request))
 
+# add staff required
 @csrf_exempt
 def add_organization(request,group_id,node_id=None):
 	try:
@@ -40,8 +41,6 @@ def add_organization(request,group_id,node_id=None):
 
 	org_node = node_collection.find({'_type':'GSystemType','name':'Organization'});
 	if request.method=="POST":
-			print node_id
-
 			organization = json.loads(request.body)
 			# print organization
 			organization_node = None
@@ -49,22 +48,28 @@ def add_organization(request,group_id,node_id=None):
 				organization_node = node_collection.find({'_id':ObjectId(str(node_id))})[0]
 			if not organization_node:
 				organization_node = node_collection.collection.GSystem();
+				organization_node['name'] = organization['properties']['name']
 				organization_node['member_of'] = [org_node[0]._id]
+				organization_node['modified_by'] = request.user.id
 				organization_node['created_by'] = request.user.id
 				organization_node['tags'].append(u'state_analytics')
 				organization_node['group_set'].append(ObjectId(group_id))
+				organization_node['origin'] = organization['coordinates']
+				organization_node.save()
+				
+				state_id = organization['properties']['state']
+				if state_id:
+					state_node = node_collection.one({'attribute_set.state_code':state_id})
+					organization_belongs_to_state_rt = node_collection.one({'_type': 'RelationType', 'name': u'organization_belongs_to_state'})
+					create_grelation(organization_node._id, organization_belongs_to_state_rt  ,state_node._id)
 
-			organization_node['name'] = organization['properties']['name']
-			organization_node['modified_by'] = request.user.id
-			organization_node['origin'] = organization['coordinates']
-			# print organization_node
-			organization_node.save()
-			state_id = organization['properties']['state']
-			if state_id and not node_id:
-				state_node = node_collection.one({'attribute_set.state_code':state_id})
-				organization_belongs_to_state_rt = node_collection.one({'_type': 'RelationType', 'name': u'organization_belongs_to_state'})
-				create_grelation(organization_node._id, organization_belongs_to_state_rt  ,state_node._id)
-			return HttpResponse(json.dumps({'status':200,'message':'OK'}))
+			else:
+				organization_node['name'] = organization['properties']['name']
+				organization_node['modified_by'] = request.user.id
+				organization_node['origin'] = organization['coordinates']
+				organization_node.save()
+			# how to modify state?????===========================================
+			return HttpResponse(json.dumps({'status':200,'id':str(organization_node._id)}))
 	else:
 		return HttpResponse("Not POST")
 				
@@ -77,18 +82,23 @@ def fetch_organization(request,group_id=None):
 
 	organization_belongs_to_state_rel = triple_collection.find({'right_subject':{'$in':states_id}})
 	organization_ids = [doc.subject for doc in organization_belongs_to_state_rel]
-	organization_list = node_collection.find({'_id':{'$in':organization_ids}})
+	organization_list = node_collection.find({'_id':{'$in':organization_ids},'tags':{'$in': ['state_analytics']}})
 	result = [] 
 	temp = {
 		'id':'',
 		'properties':{},
-		'coordinates':'',
+		'coordinates':[],
 	}
 	for organization in organization_list:
-		state = node_collection.find({'_id':organization.relation_set[0]['organization_belongs_to_state'][0]})
-		temp['id'] = str(organization._id).encode('ascii','ignore')
-		temp['properties']['name'] = organization.name.encode('ascii','ignore')
-		temp['properties']['state'] = state[0].attribute_set[0]['state_code'].encode('ascii','ignore')
-		temp['coordinates'] = organization.origin
-		result.append(json.dumps(temp))
+		state = None
+		for relation in organization.relation_set:
+			if 'organization_belongs_to_state' in relation.keys():
+				state = node_collection.find({'_id':relation['organization_belongs_to_state'][0]})
+		if state:
+			temp['id'] = str(organization._id).encode('ascii','ignore')
+			temp['properties']['name'] = organization.name.encode('ascii','ignore')
+			temp['properties']['state'] = state[0].attribute_set[0]['state_code'].encode('ascii','ignore')
+			temp['coordinates'] = organization.origin
+			result.append(json.dumps(temp))
+
 	return HttpResponse(json.dumps(result))
