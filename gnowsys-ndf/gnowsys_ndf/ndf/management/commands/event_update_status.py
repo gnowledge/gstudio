7148ecba85b3f43e3f4fdc9db9330caf6da43cf5
root <root@gnowgi.net>
2016-07-07 16:33:22 +0530

    event_update_status command added in manage.py

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/event_update_status.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/event_update_status.py
@@ -1,66 +1,73 @@
 from gnowsys_ndf.ndf.models import *
 from gnowsys_ndf.ndf.views.methods import create_gattribute
+from django.core.management.base import BaseCommand, CommandError
 
 import datetime
 
-def event_upd():	
+class Command(BaseCommand):
+	help = "This command updates the status of all events"
 
-	# Update for meeting
-	all_meets = node_collection.find({'url':u'meeting'})
-	updater(all_meets)
+	def handle(self, *args, **options):
+		self.event_upd()
 
-	# Update for session
-	all_sessions = node_collection.find({'url':u'session'})
-	updater(all_sessions)
+	def event_upd(self):
 
-	# Update for inaugrations
-	all_inaug = node_collection.find({'url':u'inauguration'})
-	updater(all_inaug)
+		# Update for meeting
+		all_meets = node_collection.find({'url':u'meeting'})
+		self.updater(all_meets)
 
-	# Update for field visit
-	all_fv = node_collection.find({'url':u'field visit'})
-	updater(all_fv)
+		# Update for session
+		all_sessions = node_collection.find({'url':u'session'})
+		self.updater(all_sessions)
 
-	# Update for orientation
-	all_or = node_collection.find({'url':u'orientation'})
-	updater(all_or)
+		# Update for inaugrations
+		all_inaug = node_collection.find({'url':u'inauguration'})
+		self.updater(all_inaug)
 
-	# Update for master trainer program
-	all_mtp = node_collection.find({'url':u'master trainer program'})
-	updater(all_mtp)
+		# Update for field visit
+		all_fv = node_collection.find({'url':u'field visit'})
+		self.updater(all_fv)
 
-	# Update for master trainer program
-	all_mtp = node_collection.find({'url':u'master trainer program'})
-	updater(all_mtp)
+		# Update for orientation
+		all_or = node_collection.find({'url':u'orientation'})
+		self.updater(all_or)
 
-	# Update for training of teachers
-	all_tot = node_collection.find({'url':u'training of teachers'})
-	updater(all_tot)
+		# Update for master trainer program
+		all_mtp = node_collection.find({'url':u'master trainer program'})
+		self.updater(all_mtp)
 
-	# Update for course developers meeting
-	all_cdm = node_collection.find({'url':u'course developers meeting'})
-	updater(all_cdm)
+		# Update for master trainer program
+		all_mtp = node_collection.find({'url':u'master trainer program'})
+		self.updater(all_mtp)
 
-	# Update for lab session
-	all_ls = node_collection.find({'url':u'lab session'})
-	updater(all_ls)
+		# Update for training of teachers
+		all_tot = node_collection.find({'url':u'training of teachers'})
+		self.updater(all_tot)
+
+		# Update for course developers meeting
+		all_cdm = node_collection.find({'url':u'course developers meeting'})
+		self.updater(all_cdm)
+
+		# Update for lab session
+		all_ls = node_collection.find({'url':u'lab session'})
+		self.updater(all_ls)
 
 
 
-def updater(all_events):
+	def updater(self,all_events):
 
-	now = datetime.datetime.now()
-	status = node_collection.one({'_type' : 'AttributeType' , 'name': 'event_status'})
+		now = datetime.datetime.now()
+		status = node_collection.one({'_type' : 'AttributeType' , 'name': 'event_status'})
 
-	for event in all_events:
-		for i in event.attribute_set:
-			try:
-				end_time = i['end_time']
-				break
-			except:
-				pass
+		for event in all_events:
+			for i in event.attribute_set:
+				try:
+					end_time = i['end_time']
+					break
+				except:
+					pass
 
-		if now > end_time:
-			create_gattribute(event._id , status , unicode("Completed"))
+			if now > end_time:
+				create_gattribute(event._id , status , unicode("Completed"))
 			
 			
