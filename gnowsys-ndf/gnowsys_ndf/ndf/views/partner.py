6f75dbcab902ed46c3fdb551c8401b7fb33c258b
saurabh <saurabhbharswadkar7@gmail.com>
2016-07-07 18:22:53 +0530

    ajax call added in task edit view to update

--- gnowsys-ndf/gnowsys_ndf/ndf/views/partner.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/partner.py
@@ -217,7 +217,7 @@ def nroer_groups(request, group_id, groups_category):
 def partner_showcase(request, group_id):
 
     group_name, group_id = get_group_name_id(group_id)
-    resource_count = []
+    resource_meta_info = []
     
     all_source = node_collection.find({'attribute_set.source': {'$exists': True, '$ne': ''} }).distinct('attribute_set.source')
 
@@ -227,12 +227,12 @@ def partner_showcase(request, group_id):
     # tmp_source = node_collection.find({'attribute_set.source': "CCRT" })
     # print "\n\n tmp_source",tmp_source.count()
     
-    print "\n\n source_partners",source_partners.count()
+    # print "\n\n source_partners",source_partners.count()
     for each in source_partners:
       # print "sas"
       tmp_source = node_collection.find({'attribute_set.source': each.name })
-      resource_count.append(tmp_source.count()) 
-    print "\n\n tmp_source ",resource_count
+      resource_meta_info.append({"res_count":tmp_source.count(),"res_name":each.name}) 
+    # print "\n\n resource_meta_info ",resource_meta_info
        
     source_partners.rewind()
 
@@ -240,6 +240,6 @@ def partner_showcase(request, group_id):
                             {
                               'group_id': group_id, 'groupid': group_id,
                               'source_partners': source_partners,
-                              'tmp_source':resource_count
+                              'resource_meta_info':resource_meta_info
                             }, context_instance=RequestContext(request)
                           )
