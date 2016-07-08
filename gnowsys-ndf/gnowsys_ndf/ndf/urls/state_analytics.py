a578f0d0a29ac83104dcdf25424dfb616bb7e606
deepvyas <vyasdeep.dv@@gmail.com>
2016-07-08 12:00:12 +0530

    Delete organization added

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/state_analytics.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/state_analytics.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.state_analytics',
 		url(r'^[/]update_organization/(?P<node_id>[\w-]+)$','add_organization',name='add_organization'),
 		url(r'^[/]update_organization/$','add_organization',name='add_organization'),
 		url(r'^[/]fetch_organization/$','fetch_organization',name='fetch_organization'),
+		url(r'^[/]delete_organization/(?P<node_id>[\w-]+)$','delete_organization',name='delete_organization'),
 	)
--- gnowsys-ndf/gnowsys_ndf/ndf/views/state_analytics.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/state_analytics.py
@@ -25,7 +25,7 @@ from mongokit import paginator
 from django.views.decorators.csrf import csrf_exempt
 from gnowsys_ndf.ndf.models import *
 from pymongo import Connection
-from gnowsys_ndf.ndf.views.methods import create_grelation,get_group_name_id
+from gnowsys_ndf.ndf.views.methods import create_grelation,get_group_name_id, delete_node
 
 def map_view(request,group_id):
 	return render_to_response("ndf/map_india.html",{ 'group_id':group_id, 'groupid':group_id}
@@ -48,28 +48,22 @@ def add_organization(request,group_id,node_id=None):
 				organization_node = node_collection.find({'_id':ObjectId(str(node_id))})[0]
 			if not organization_node:
 				organization_node = node_collection.collection.GSystem();
-				organization_node['name'] = organization['properties']['name']
 				organization_node['member_of'] = [org_node[0]._id]
-				organization_node['modified_by'] = request.user.id
 				organization_node['created_by'] = request.user.id
 				organization_node['tags'].append(u'state_analytics')
 				organization_node['group_set'].append(ObjectId(group_id))
-				organization_node['origin'] = organization['coordinates']
-				organization_node.save()
-				
-				state_id = organization['properties']['state']
-				if state_id:
-					state_node = node_collection.one({'attribute_set.state_code':state_id})
-					organization_belongs_to_state_rt = node_collection.one({'_type': 'RelationType', 'name': u'organization_belongs_to_state'})
-					create_grelation(organization_node._id, organization_belongs_to_state_rt  ,state_node._id)
 
-			else:
-				organization_node['name'] = organization['properties']['name']
-				organization_node['modified_by'] = request.user.id
-				organization_node['origin'] = organization['coordinates']
-				organization_node.save()
-			# how to modify state?????===========================================
-			return HttpResponse(json.dumps({'status':200,'id':str(organization_node._id)}))
+			organization_node['name'] = organization['properties']['name']
+			organization_node['modified_by'] = request.user.id
+			organization_node['origin'] = organization['coordinates']
+			organization_node.save()
+			state_id = organization['properties']['state']
+			if state_id:
+				state_node = node_collection.one({'attribute_set.state_code':state_id})
+				organization_belongs_to_state_rt = node_collection.one({'_type': 'RelationType', 'name': u'organization_belongs_to_state'})
+				create_grelation(organization_node._id, organization_belongs_to_state_rt  ,state_node._id)
+
+			return HttpResponse(json.dumps({'status':1,'id':str(organization_node._id)}))
 	else:
 		return HttpResponse("Not POST")
 				
@@ -80,7 +74,7 @@ def fetch_organization(request,group_id=None):
 	states_nodes = node_collection.find({'member_of':{'$in':[state_gst[0]._id]}})
 	states_id = [doc._id for doc in states_nodes]
 
-	organization_belongs_to_state_rel = triple_collection.find({'right_subject':{'$in':states_id}})
+	organization_belongs_to_state_rel = triple_collection.find({'right_subject':{'$in':states_id},'status':{'$ne':u'DELETED'}})
 	organization_ids = [doc.subject for doc in organization_belongs_to_state_rel]
 	organization_list = node_collection.find({'_id':{'$in':organization_ids},'tags':{'$in': ['state_analytics']}})
 	result = [] 
@@ -92,7 +86,7 @@ def fetch_organization(request,group_id=None):
 	for organization in organization_list:
 		state = None
 		for relation in organization.relation_set:
-			if 'organization_belongs_to_state' in relation.keys():
+			if 'organization_belongs_to_state' in relation.keys() and relation['organization_belongs_to_state']:
 				state = node_collection.find({'_id':relation['organization_belongs_to_state'][0]})
 		if state:
 			temp['id'] = str(organization._id).encode('ascii','ignore')
@@ -102,3 +96,12 @@ def fetch_organization(request,group_id=None):
 			result.append(json.dumps(temp))
 
 	return HttpResponse(json.dumps(result))
+
+@csrf_exempt
+def delete_organization(request, group_id=None,node_id=None):
+	if not node_id:
+		return HttpResponse({'status':0,'message':'Node id not sent'})
+	else:
+		status,status_message = delete_node(ObjectId(node_id), deletion_type=0)
+		print status
+		return HttpResponse(json.dumps({'status':status,'message':''}))
