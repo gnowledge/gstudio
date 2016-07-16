e070457c8eadbaf6211b3a61145eb14519b76458
Mayank Agarwal <mayankgrwl97@gmail.com>
2016-07-16 22:59:42 +0530

    added docstrings to bbb_api functions

--- gnowsys-ndf/gnowsys_ndf/ndf/views/bbb_api.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/bbb_api.py
@@ -3,6 +3,7 @@ import hashlib, random
 from xml.dom import minidom 
 from xml.dom.minidom import Node 
 
+# ----------------------UTILITY FUNCTIONS--------------------------------------
 
 def bbb_wrap_load_file(url):
     timeout = 10
@@ -30,7 +31,25 @@ def assign2Dict(xml):
     except:
         return False
     
+
+# --------------------------GET URLs-------------------------------------------
+
+
 def joinURL(meetingID, username, PW, SALT, URL):
+    '''
+    This method returns the url to join the specified meeting.
+
+    Parameters:
+        meetingID -- the unique meeting identifier used to store the meeting in the bigbluebutton server
+        username -- the display name to be used when the user joins the meeting
+        PW -- the attendee or moderator password of the meeting
+        SALT -- the security salt of the bigbluebutton server
+        URL -- the url of the bigbluebutton server
+
+    Return:
+        The url to join the meeting
+    '''
+
     url_join = URL + "api/join?"
     
     parameters = {'meetingID' : meetingID,
@@ -43,6 +62,23 @@ def joinURL(meetingID, username, PW, SALT, URL):
 
 
 def createMeetingURL(name, meetingID, attendeePW, moderatorPW, welcome, logoutURL, SALT, URL):
+    '''
+    This method returns the url to join the specified meeting.
+
+    Parameters:
+        name -- a name fot the meeting
+        meetingID -- the unique meeting identifier used to store the meeting in the bigbluebutton server
+        attendeePW -- the attendee of the meeting
+        moderatorPW -- the moderator of the meeting
+        welcome -- the welcome message that gets displayed on the chat window
+        logoutURL -- the URL that the bbb client will go to after users logouut
+        SALT -- the security salt of the bigbluebutton server
+        URL -- the url of the bigbluebutton server
+
+    Return:
+        The url to join the meeting
+    '''
+
     url_create = URL + "api/create?"
     voiceBridge = 70000 + random.randint(0, 9999);
     parameters = {'name': name,
@@ -64,9 +100,27 @@ def createMeetingURL(name, meetingID, attendeePW, moderatorPW, welcome, logoutUR
     params = parameters + welcome_parameters
     return url_create  + params + '&checksum=' + hashlib.sha1("create" + params + SALT).hexdigest() 
 
+# -------------------------------------CREATE----------------------------------
 
-def createMeeting( username, meetingID, welcomeString, mPW, aPW, SALT, URL, logoutURL ):
+def createMeeting(username, meetingID, welcomeString, mPW, aPW, SALT, URL, logoutURL ):
+    '''
+    This method creates a meeting and return an array of the xml packet
 
+    Parameters:
+        username -- name of the meeting to be created
+        meetingID -- the unique meeting identifier used to store the meeting in the bigbluebutton server
+        welcomeString -- the welcome message to be displayed when a user logs in to the meeting
+        mPW -- the moderator password of the meeting
+        aPW -- the attendee password of the meeting
+        SALT -- the security salt of the bigbluebutton server
+        URL -- the url of the bigbluebutton server
+        logoutURL -- the url the user should be redirected to when they logout of bigbluebutton
+
+    Return:
+       - Null if unable to reach the bigbluebutton server
+       - False if an error occurs while parsing
+       - Dictionary containing the values of the xml packet
+    '''
     create_url = createMeetingURL(username, meetingID, aPW, mPW, welcomeString, logoutURL, SALT, URL )
     xml = bbb_wrap_load_file( create_url )
 
