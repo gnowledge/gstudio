2ed95e3d03823772ee078841fb5e74def6494407
Kedar Aitawdekar <kedar2a@gmail.com>
2016-09-02 15:26:12 +0530

    conflicts resolved

--- gnowsys-ndf/gnowsys_ndf/settings.py
+++ gnowsys-ndf/gnowsys_ndf/settings.py
@@ -5,16 +5,16 @@ import os
 import djcelery
 
 # imports from core django libraries
-from django.conf import global_settings
-from django.utils.translation import ugettext
+# from django.conf import global_settings
+# from django.utils.translation import ugettext
 # from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
 # imports from third-party app(s)
 
-from gnowsys_ndf.ndf.utils import (is_dir_exists, ensure_dir, get_current_dbs_path,
-    move_file_or_dirctory)
+# from gnowsys_ndf.ndf.utils import (is_dir_exists, ensure_dir, get_current_dbs_path,
+#     move_file_or_dirctory)
 
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ["127.0.0.1"]
 
 TEMPLATE_DEBUG = DEBUG
@@ -28,7 +28,7 @@ OTHER_COMMON_LANGUAGES = [
     ('ks','Kashmiri'), ('kok','Konkani'), ('kha','Khasi'),
     ('dra','Dravidian'), ('gon','Gondi'), ('bra','Braj'),
     ('mi','Malayalam'), ('mai','Maithili'), ('mag','Magahi'),
-    ('lus','Lushai'), ('bh','Bihari'), ('kru','Kurukh'),
+    ('lus','Lushai'), ('bho','Bhojpuri'), ('kru','Kurukh'),
     ('awa','Awadhi'), ('sa','Sanskrit'), ('sat','Santali'),
     ('him','Himachali'), ('sd','Sindhi'), ('as','Assamese'),
     ('ar', 'Arabic'), ('bn', 'Bengali'), ('ca', 'Catalan'),
@@ -430,6 +430,9 @@ MIDDLEWARE_CLASSES = (
     # gstudio custom middleware(s):
     # 'gnowsys_ndf.ndf.middleware.Buddy.BuddySession',
     # 'gnowsys_ndf.ndf.middleware.UserRestrictMiddleware.UserRestrictMiddleware',
+
+    # for profiling methods:
+    'gnowsys_ndf.ndf.middleware.ProfileMiddleware.ProfileMiddleware',
 )
 
 # AUTH_PROFILE_MODULE = 'gnowsys_ndf.ndf.models.UserProfile'
@@ -475,9 +478,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gnowsys_ndf.benchmarker',
+    # 'gnowsys_ndf.benchmarker',
     'registration',
-    'djangoratings',
+    # 'djangoratings',
     'notification',
     'pagination',
     'captcha',
@@ -488,7 +491,7 @@ INSTALLED_APPS = (
     # 'django.contrib.flatpages',   #textb
     # 'online_status',              #for online_users
     # 'endless_pagination',
-    'jsonrpc',
+    # 'jsonrpc',
     'registration_email',
     'memcache_admin',
     'django_mailbox',
