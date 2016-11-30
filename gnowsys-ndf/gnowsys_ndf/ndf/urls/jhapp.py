82b7271bc83c83bfdb7b4fe13db23e401043078d
saavrabh <saurabhbharswadkar7@gmail.com>
2016-11-30 17:58:45 +0530

    unzip file at given location

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/jhapp.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/jhapp.py
@@ -4,7 +4,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('gnowsys_ndf.ndf.views.jhapp',
                        url(r'^[/]$', 'jhapp', name='jhapp'),
-                       # url(r'^/uploadDoc/$', 'uploadDoc', name='uploadDoc'), #Direct ot html template                               
+                       url(r'^/uploadZapp/$', 'uploadZapp', name='uploadZapp'), 
+                       url(r'^/saveZapp/', 'saveZapp', name='saveZapp'),                               
                        
 )
 
