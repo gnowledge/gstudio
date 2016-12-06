c69cdfc3443841e89398af4e0988e585f73f759a
katkamrachana <katkam.rachana@gmail.com>
2016-12-06 16:57:31 +0530

    Set supports_repository_lookup return True to allow repo lookup

--- gnowsys-ndf/dlkit_gstudio/repository/managers.py
+++ gnowsys-ndf/dlkit_gstudio/repository/managers.py
@@ -263,7 +263,7 @@ class RepositoryProfile(osid_managers.OsidProfile, repository_managers.Repositor
         *compliance: mandatory -- This method must be implemented.*
 
         """
-        return False # Change to True when implemented.
+        return True # Change to True when implemented.
 
     def supports_repository_query(self):
         """Tests if repository query is supported.
