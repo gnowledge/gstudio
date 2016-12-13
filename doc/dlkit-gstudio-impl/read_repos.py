b6869df7935a86bcd0c4531310e15982cd9a7987
katkamrachana <katkam.rachana@gmail.com>
2016-12-13 17:15:00 +0530

    Test files for repo read/write

---
+++ doc/dlkit-gstudio-impl/read_repos.py
@@ -0,0 +1,33 @@
+from dlkit_runtime import RUNTIME, PROXY_SESSION
+from dlkit_runtime.proxy_example import TestRequest
+condition = PROXY_SESSION.get_proxy_condition()
+
+
+# dummy_request = TestRequest(username='administrator',
+#                             authenticated=True)
+
+from django.contrib.auth.models import User
+u = User.objects.get(pk=1)
+from django.test import RequestFactory
+
+rf = RequestFactory()
+
+req_obj = rf.get('/home')
+req_obj.user = u
+req_obj.user.id
+
+
+condition.set_http_request(req_obj)
+proxy = PROXY_SESSION.get_proxy(condition)
+
+
+#================================================== GROUP as REPOSITORY ==================
+repository_service_mgr = RUNTIME.get_service_manager('REPOSITORY', proxy=proxy)
+
+import ipdb; ipdb.set_trace()
+all_repos = repository_service_mgr.get_repositories()
+print "\n Total repositories: ", all_repos.len()
+
+repo_obj = all_repos.next()
+repo_obj.get_display_name().get_text()
+repo_obj.get_description().get_text()
