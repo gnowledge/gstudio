0eb5b41ff75eec9aedcfb8351dae0065988ab76c
saavrabh <saurabhbharswadkar7@gmail.com>
2017-01-05 12:17:18 +0530

    jhapp selection enabled in upload form

--- gnowsys-ndf/gnowsys_ndf/ndf/views/jhapp.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/jhapp.py
@@ -14,6 +14,7 @@ except ImportError:  # old pymongo
 from django.core.urlresolvers import reverse
 from gnowsys_ndf.ndf.models import node_collection
 from gnowsys_ndf.ndf.templatetags.ndf_tags import check_is_gstaff
+from gnowsys_ndf.settings import *
 
 GST_JSMOL = node_collection.one({"_type":"GSystemType","name":"Jsmol"})
 @login_required
@@ -35,7 +36,7 @@ def uploadZapp(request, group_id):
       group_id = ObjectId(group_id)
   except:
       group_name, group_id = get_group_name_id(group_id)
-  return render_to_response("ndf/uploadZapp.html",RequestContext(request, {"groupid":group_id, "group_id":group_id}))
+  return render_to_response("ndf/uploadZapp.html",RequestContext(request, {"groupid":group_id, "group_id":group_id,'gstudio_supported_jhapps':GSTUDIO_SUPPORTED_JHAPPS}))
 
 @login_required
 @get_execution_time
@@ -45,11 +46,13 @@ def saveZapp(request,group_id):
       group_id = ObjectId(group_id)
   except:
       group_name, group_id = get_group_name_id(group_id)
-
+  jhapp_type = request.POST.getlist("jhapp-type", "")
+  jhapp_type_trim = jhapp_type[0].replace(" ", "")
   group_obj = node_collection.one({'_id': ObjectId(group_id)})
   title = request.POST.get('context_name','')
   usrid = request.user.id
-  jsmol_gst  = node_collection.one({"_type":"GSystemType","name":"Jsmol" })
+  # jsmol_gst  = node_collection.one({"_type":"GSystemType","name":"Jsmol" })
+  sel_jhapp_gst  = node_collection.one({"_type":"GSystemType","name":unicode(jhapp_type_trim) })
   
   import zipfile
   from gnowsys_ndf.ndf.views.filehive import write_files
@@ -58,6 +61,9 @@ def saveZapp(request,group_id):
   fileobj_list = write_files(request, group_id)
   fileobj_id = fileobj_list[0]['_id']
   file_node = node_collection.one({'_id': ObjectId(fileobj_id) })
+  
+  if sel_jhapp_gst._id:
+    file_node.member_of = [ObjectId(sel_jhapp_gst._id)]
   
   uploaded_files = request.FILES.getlist('filehive', [])
   zip_path = file_node.if_file.original['relurl']
@@ -68,8 +74,7 @@ def saveZapp(request,group_id):
 
   with zipfile.ZipFile("/data/media/"+ file_node.if_file.original['relurl'], "r") as z:
     z.extractall(un_zip_split_path)
- 
-  # file_node.member_of = [ObjectId(jsmol_gst._id)]
+  
   relurl_path = str("/" + un_zip_path[0] + "/" + un_zip_path[1] + "/" +un_zip_path[2] + "/" + file_name[0] + "/" + "index" + ".html" )
   # print "_______________________________",relurl_path
   discussion_enable_at = node_collection.one({"_type": "AttributeType", "name": "discussion_enable"})
