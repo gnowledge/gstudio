20c9e3c1d76101d7584e313254c196d34034df7c
root <root@gnowgi.net>
2017-02-06 14:43:19 +0530

    setup dlkit fab command updated

--- gnowsys-ndf/fabfile.py
+++ gnowsys-ndf/fabfile.py
@@ -28,10 +28,10 @@ def purge_group():
 
 
 def setup_dlkit():
-    import os
-    dirspot = os.getcwd()
-    if "/home/docker/code/gstudio/gnowsys-ndf" == dirspot:
- 		if not os.path.isdir('./dlkit') and not os.path.isdir('./dlkit_runtime'):
+	import os
+	dirspot = os.getcwd()
+	if "/home/docker/code/gstudio/gnowsys-ndf" == dirspot:
+		if not os.path.isdir('./dlkit') and not os.path.isdir('./dlkit_runtime'):
 			local('git checkout dlkit')
 			local('git pull origin dlkit')
 			local('git clone https://bitbucket.org/cjshaw/dlkit_runtime.git')
@@ -40,9 +40,8 @@ def setup_dlkit():
 			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/dlkit')
 			local('git submodule update --init --recursive')
 			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/')
-			update_data()
-     	else:
-         	print "dlkit and dlkit_runtime are already exists."
+		else:
+			print "dlkit and dlkit_runtime are already exists."
 	else:
 		os.chdir('/home/docker/code/gstudio/gnowsys-ndf')
 		setup_dlkit()
