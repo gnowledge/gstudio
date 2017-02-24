2eb47daf956e1a7c1df51686ae219e75097e16f2
katkamrachana <katkam.rachana@gmail.com>
2017-02-24 18:21:00 +0530

    update kwargs and remove args from calling fill_gsystem_values()

--- gnowsys-ndf/gnowsys_ndf/ndf/views/asset.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/asset.py
@@ -83,6 +83,9 @@ def create_assetcontent(asset_id,
 		name = request.POST.get('name') if request else None
 	if not created_by:
 		created_by = request.user.id if request else None
+	kwargs.update({'name': name})
+	if content:
+		kwargs.update({'content': content})
 
 	group_name, group_id = get_group_name_id(group_name_or_id)
 	if group_id not in asset_obj['group_set']:
@@ -119,19 +122,17 @@ def create_assetcontent(asset_id,
 	else:
 		asset_content_obj = node_collection.collection.GSystem()
 		asset_content_obj.fill_gstystem_values(request=request,
-												name=name,
 												member_of=member_of_gst_id,
 												group_set=group_set,
 												created_by=created_by,
-												content=content,
 												uploaded_file=files[0],
 												unique_gs_per_file=True,
 												**kwargs)
 	print "\nasset_content_obj.name BEFORE: ", asset_content_obj.name
-
+	print "kwargs: ", kwargs
 	asset_content_obj.fill_node_values(**kwargs)
-	print "\nasset_content_obj.name: ", asset_content_obj.name
 	asset_content_obj.save(groupid=group_id)
+	print "\nasset_content_obj.name: ", asset_content_obj.name
 	asset_contents_list = [asset_content_obj._id]
 	rt_has_asset_content = node_collection.one({'_type': 'RelationType',
 		'name': 'has_assetcontent'})
