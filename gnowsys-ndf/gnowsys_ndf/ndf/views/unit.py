8234909b5d79cb1fefbcb017ff472b37627bf8d4
katkamrachana <katkam.rachana@gmail.com>
2017-03-03 16:52:10 +0530

    unit authoring flow changed.

--- gnowsys-ndf/gnowsys_ndf/ndf/views/unit.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/unit.py
@@ -7,10 +7,11 @@ try:
 except ImportError:  # old pymongo
     from pymongo.objectid import ObjectId
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 ''' -- imports from application folders/files -- '''
 from gnowsys_ndf.ndf.models import GSystemType, Group, Node  # GSystem, Triple
@@ -43,6 +44,7 @@ def unit_create_edit(request, group_id, unit_group_id=None):
 
     elif request.method == "POST":
         group_name = request.POST.get('name', '')
+        group_altnames = request.POST.get('altnames', '')
         unit_id_post = request.POST.get('_id', '')
         unit_group_id = unit_id_post if unit_id_post else unit_group_id
         unit_group_name, unit_group_id = Group.get_group_name_id(unit_group_id)
@@ -57,8 +59,13 @@ def unit_create_edit(request, group_id, unit_group_id=None):
                                         node_id=unit_group_id)
 
         # print result
-        return HttpResponse(int(result[0]))
-
+        # return HttpResponse(int(result[0]))
+        if not result[0]:
+            return HttpResponseRedirect(reverse('list_units', kwargs={'group_id': group_id}))
+        unit_node = result[1]
+        return HttpResponseRedirect(reverse('unit_detail', 
+            kwargs={'group_id': unit_node._id, 'unit_group_id': unit_node._id}))
+    
 
 @get_execution_time
 def unit_detail(request, group_id, unit_group_id):
@@ -107,10 +114,14 @@ def unit_detail(request, group_id, unit_group_id):
                         lesson_dict['activities'].append(activity_dict)
             unit_structure.append(lesson_dict)
 
-    template = "ndf/unit_structure.html"
+    # template = "ndf/unit_structure.html"
+    template = 'ndf/gevent_base.html'
+
     # print unit_structure
     req_context = RequestContext(request, {
+                                'title': 'unit_authoring',
                                 'group_id': parent_group_id,
+                                'groupid': parent_group_id,
                                 'unit_obj': unit_group_obj,
                                 'unit_structure': json.dumps(unit_structure)
                             })
