a29297dbf70679cbc53a33947e2cccebf42bbb02
Prachi <prachibhatia24@yahoo.com>
2017-03-08 20:27:36 +0530

    Subscript and superscript added in the editor

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckeditor-config.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckeditor-config.js
@@ -21,10 +21,13 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
+
+	
+
 	config.toolbar_GeneralToolbar =
 	[
 
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat','Subscript','Superscript' ] },
 		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-' ] },
 		{ name: 'insert', items : [ 'Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe','video'] },
 		{ name: 'styles', items : [ 'Styles','Format' ] },
@@ -33,10 +36,12 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'tools', items : [ 'addImage','addAudio','addVideo','addJhapp','Source','Maximize','-'] },
 
 	];
+	
+
 
 	config.toolbar_BasicToolbar =
 	[
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat','Subscript', 'Superscript' ] },
 		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-' ] },
 		{ name: 'links', items : [ 'Link','Unlink'] },
 		{ name: 'insert', items : [ 'Flash','Iframe' ] },
@@ -74,7 +79,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
