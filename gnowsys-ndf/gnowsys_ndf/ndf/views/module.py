aff968334f3eaa23fdfd83bbbada6994ecb28836
Kedar Aitawdekar <kedar2a@gmail.com>
2017-04-05 17:07:00 +0530

    overriden old modules views to have current actions on modules.

--- gnowsys-ndf/gnowsys_ndf/ndf/views/module.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/module.py
@@ -1,150 +1,83 @@
-from django.http import HttpResponseRedirect
-#from django.http import HttpResponse
-from django.shortcuts import render_to_response #render  uncomment when to use
-from django.template import RequestContext
-# from django.core.urlresolvers import reverse
-from django.contrib.auth.decorators import login_required
+''' -- imports from python libraries -- '''
+import json
 
+''' -- imports from installed packages -- '''
 try:
     from bson import ObjectId
 except ImportError:  # old pymongo
     from pymongo.objectid import ObjectId
 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
-from gnowsys_ndf.settings import GAPPS  # , MEDIA_ROOT
-from gnowsys_ndf.ndf.models import GSystemType, Node
-from gnowsys_ndf.ndf.models import node_collection, triple_collection
-from gnowsys_ndf.ndf.views.methods import get_execution_time
-GST_MODULE = node_collection.one({'_type': "GSystemType", 'name': GAPPS[8]})
-app = GST_MODULE
+''' -- imports from application folders/files -- '''
+from gnowsys_ndf.ndf.models import GSystemType, Group, Node, GSystem  #, Triple
+from gnowsys_ndf.ndf.models import node_collection
+
+from gnowsys_ndf.ndf.views.group import CreateGroup
+from gnowsys_ndf.ndf.views.methods import get_execution_time, staff_required, create_gattribute,get_language_tuple
+from gnowsys_ndf.ndf.views.ajax_views import get_collection
+
+gst_module_name, gst_module_id = GSystemType.get_gst_name_id('Module')
+
 
 @get_execution_time
-def module(request, group_id, module_id=None):
-    """
-    * Renders a list of all 'modules' available within the database.
-    """
-    # ins_objectid  = ObjectId()
-    # if ins_objectid.is_valid(group_id) is False :
-    #   group_ins = node_collection.find_one({'_type': "Group","name": group_id})
-    #   auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #   if group_ins:
-    #     group_id = str(group_ins._id)
-    #   else :
-    #     auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #     if auth :
-    #       group_id = str(auth._id)
-    # else :
-    #     pass
-    try:
-        group_id = ObjectId(group_id)
-    except:
-        group_name, group_id = get_group_name_id(group_id)
+def list_modules(request, group_id):
+    '''
+    listing of modules
+    '''
+    all_modules = GSystem.query_list(group_id, 'Module', request.user.id)
+    template = "ndf/explore_2017.html"
 
-    
-    if module_id is None:
-      module_ins = node_collection.find_one({'_type':"GSystemType", "name":"Module"})
-      if module_ins:
-        module_id = str(module_ins._id)
-    
-    if request.method == "POST":
-      # Module search view
-      title = GST_MODULE.name
-      
-      search_field = request.POST['search_field']
-      module_coll = node_collection.find({'member_of': {'$all': [ObjectId(GST_MODULE._id)]},
-                                         '$or': [{'name': {'$regex': search_field, '$options': 'i'}}, 
-                                                 {'tags': {'$regex':search_field, '$options': 'i'}}], 
-                                         'group_set': {'$all': [ObjectId(group_id)]}
-                                     }).sort('last_update', -1)
+    context_variable = {
+                        'title': 'Modules', 'doc_cur': all_modules,
+                        'group_id': group_id, 'groupid': group_id,
+                        'card': 'ndf/event_card.html', 'card_url_name': 'module_detail'
+                    }
 
-      # module_nodes_count = course_coll.count()
-
-      return render_to_response("ndf/module.html",
-                                {'title': title,
-                                 'appId':app._id,
-                                 'searching': True, 'query': search_field,
-                                 'module_coll': module_coll, 'groupid':group_id, 'group_id':group_id
-                                }, 
-                                context_instance=RequestContext(request)
-                                )
-
-    elif GST_MODULE._id == ObjectId(module_id):
-      # Module list view
-      title = GST_MODULE.name
-      module_coll = node_collection.find({'member_of': {'$all': [ObjectId(module_id)]}, 'group_set': {'$all': [ObjectId(group_id)]}})
-      template = "ndf/module.html"
-      variable = RequestContext(request, {'title': title, 'appId':app._id, 'module_coll': module_coll, 'group_id': group_id, 'groupid': group_id})
-      return render_to_response(template, variable)
-
-@get_execution_time
-def module_detail(request, group_id, _id):
-    # ins_objectid  = ObjectId()
-    # if ins_objectid.is_valid(group_id) is False :
-    #     group_ins = node_collection.find_one({'_type': "Group","name": group_id})
-    #     auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #     if group_ins:
-    #         group_id = str(group_ins._id)
-    #     else :
-    #         auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #         if auth :
-    #             group_id = str(auth._id)
-    # else :
-    #     pass
-    try:
-        group_id = ObjectId(group_id)
-    except:
-        group_name, group_id = get_group_name_id(group_id)
-
-    course_node = node_collection.one({"_id": ObjectId(_id)})
-    if course_node._type == "GSystemType":
-	return module(request, group_id, _id)
-    return render_to_response("ndf/module_detail.html",
-                                  { 'node': course_node,
-                                    'appId':app._id,
-                                    'groupid': group_id,
-                                    'group_id':group_id
-                                  },
-                                  context_instance = RequestContext(request)
-        )
+    return render_to_response(
+        template,
+        context_variable,
+        context_instance=RequestContext(request))
 
 
-    
 @login_required
-@get_execution_time    
-def delete_module(request, group_id, _id):
-    """This method will delete module object and its Attribute and Relation
-    """
-    # ins_objectid  = ObjectId()
-    # if ins_objectid.is_valid(group_id) is False :
-    #     group_ins = node_collection.find_one({'_type': "Group", "name": group_id})
-    #     auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #     if group_ins:
-    #         group_id = str(group_ins._id)
-    #     else :
-    #         auth = node_collection.one({'_type': 'Author', 'name': unicode(request.user.username) })
-    #         if auth :
-    #             group_id = str(auth._id)
-    # else :
-    #     pass
-    try:
-        group_id = ObjectId(group_id)
-    except:
-        group_name, group_id = get_group_name_id(group_id)
-        
-    pageurl = request.GET.get("next", "")
-    try:
-        node = node_collection.one({'_id': ObjectId(_id)})
-        if node:
-            attributes = triple_collection.find({'_type': 'GAttribute', 'subject': node._id})
-            relations = triple_collection.find({'_type': 'GRelation', 'subject': node._id})
-            if attributes.count() > 0:
-                for each in attributes:
-                    triple_collection.one({'_id': each['_id']}).delete()
-                    
-            if relations.count() > 0:
-                for each in relations:
-                    triple_collection.one({'_id': each['_id']}).delete()
-            node.delete()
-    except Exception as e:
-        print "Exception:", e
-    return HttpResponseRedirect(pageurl)
+@get_execution_time
+def module_create_edit(request, group_id, unit_group_id=None):
+    if request.method == "GET":
+
+        template = 'ndf/module_form.html'
+
+        req_context = RequestContext(request, {
+                                    'title': 'Module',
+                                    'group_id': group_id, 'groupid': group_id,
+                                    'post_url': reverse('node_create', kwargs={
+                                        'group_id': group_id,
+                                        'member_of': 'Module',
+                                        'detail_url_name': 'module_detail',
+                                        })
+                                })
+        return render_to_response(template, req_context)
+
+
+@get_execution_time
+def module_detail(request, group_id, node_id):
+    '''
+    detail of of selected module
+    '''
+    group_name, group_id = Group.get_group_name_id(group_id)
+
+    module_obj = Node.get_node_by_id(node_id)
+
+    template = 'ndf/module_detail.html'
+
+    req_context = RequestContext(request, {
+                                'title': 'Module',
+                                'group_id': group_id,
+                                'groupid': group_id,
+                                'node': module_obj,
+                            })
+    return render_to_response(template, req_context)
