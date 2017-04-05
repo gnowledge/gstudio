07504356364e43fa7e0f81bf1b3dd6f5866de094
Kedar Aitawdekar <kedar2a@gmail.com>
2017-04-05 17:05:38 +0530

    Added url for explore modules

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/explore.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/explore.py
@@ -8,5 +8,10 @@ urlpatterns = patterns('gnowsys_ndf.ndf.views.explore',
 						url(r'^courses/page-no=(?P<page_no>\d+)/$', 'explore_courses', name='courses_paged'),
 						url(r'^basecourses/page-no=(?P<page_no>\d+)/$', 'explore_basecourses', name='basecourses_paged'),
 						url(r'^groups/page-no=(?P<page_no>\d+)/$', 'explore_groups', name='groups_paged'),
-
  )
+
+
+urlpatterns += patterns('',
+						url(r'^(?P<group_id>[\w-]+)/modules$', 'gnowsys_ndf.ndf.views.module.list_modules', name='explore_modules'),
+						# url(r'^modules$', 'gnowsys_ndf.ndf.views.module.list_modules', {'group_id': None}, name='explore_modules'),
+)
