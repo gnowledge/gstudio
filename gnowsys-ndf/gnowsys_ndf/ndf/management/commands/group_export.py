5374b2be5275a1e8d5a0137ad313a7596d233f08
katkamrachana <katkam.rachana@gmail.com>
2017-04-07 17:25:13 +0530

    on export: add system config info and git active branch name in config file

--- gnowsys-ndf/gnowsys_ndf/ndf/management/commands/group_export.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/management/commands/group_export.py
@@ -15,7 +15,7 @@ from gnowsys_ndf.settings import GSTUDIO_DATA_ROOT, GSTUDIO_LOGS_DIR_PATH, MEDIA
 from schema_mapping import create_factory_schema_mapper
 from dump_users import create_users_dump
 from gnowsys_ndf.ndf.views.methods import get_group_name_id
-from gnowsys_ndf.ndf.templatetags.simple_filters import get_latest_git_hash
+from gnowsys_ndf.ndf.templatetags.simple_filters import get_latest_git_hash, get_active_branch_name
 
 # global variables declaration
 GROUP_CONTRIBUTORS = []
@@ -75,7 +75,9 @@ def create_configs_file(group_id):
         configs_file_out.write("\nRESTORE_USER_DATA=" + str(RESTORE_USER_DATA))
         configs_file_out.write("\nGSTUDIO_INSTITUTE_ID='" + str(GSTUDIO_INSTITUTE_ID) + "'")
         configs_file_out.write("\nGROUP_ID='" + str(group_id) + "'")
-        configs_file_out.write("\nGIT_COMMIT_HASH='" + str(get_latest_git_hash) + "'")
+        configs_file_out.write("\nGIT_COMMIT_HASH='" + str(get_latest_git_hash()) + "'")
+        configs_file_out.write("\nGIT_BRANCH_NAME='" + str(get_active_branch_name()) + "'")
+        configs_file_out.write('\nSYSTEM_DETAILS="' + str(os.uname()) + '"')
     return configs_file_path
 
 def write_md5_of_dump(group_dump_path, configs_file_path):
