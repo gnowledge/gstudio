29c18c1b5ed5e3d547a137f61c4164b218f2adfc
saurabh bharswadkar <saurabhbharswadkar7@gmail.com>
2017-04-15 21:21:40 +0530

    my desk added

--- gnowsys-ndf/gnowsys_ndf/ndf/views/userDashboard.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/userDashboard.py
@@ -43,6 +43,13 @@ gapp_mt = node_collection.one({'_type': "MetaType", 'name': META_TYPE[0]})
 GST_IMAGE = node_collection.one({'member_of': gapp_mt._id, 'name': GAPPS[3]})
 at_user_pref = node_collection.one({'_type': 'AttributeType', 'name': 'user_preference_off'})
 ins_objectid  = ObjectId()
+ce_gst = node_collection.one({'_type': "GSystemType", 'name': "CourseEventGroup"})
+announced_unit_gst = node_collection.one({'_type': "GSystemType", 'name': "announced_unit"})
+gst_acourse = node_collection.one({'_type': "GSystemType", 'name': "Announced Course"})
+gst_group = node_collection.one({'_type': "GSystemType", 'name': "Group"})
+group_id = node_collection.one({'_type': "Group", 'name': "home"})._id
+gst_module_name, gst_module_id = GSystemType.get_gst_name_id('Module')
+gst_base_unit_name, gst_base_unit_id = GSystemType.get_gst_name_id('base_unit')
 
 
 #######################################################################################################################################
@@ -689,6 +696,57 @@ def my_courses(request, group_id):
                 context_instance=RequestContext(request)
         )
 
+def my_desk(request, group_id):
+
+    if str(request.user) == 'AnonymousUser':
+        raise Http404("You don't have an authority for this page!")
+
+    try:
+        auth_obj = get_group_name_id(group_id, get_obj=True)
+        user_id = auth_obj.created_by
+
+    except:
+        user_id = eval(group_id)
+        auth_obj = node_collection.one({'_type': "Author", 'created_by': user_id})
+
+    auth_id = auth_obj._id
+    title = 'my desk'
+    modules_cur = node_collection.find({'member_of': gst_module_id  }).sort('last_update', -1)
+
+    my_course_objs = get_user_course_groups(user_id)
+    module_unit_ids = [val for each_module in modules_cur for val in each_module.collection_set ]
+
+    modules_cur.rewind()
+        # print my_course_objs
+    base_unit_cur = node_collection.find({'member_of': {'$in': [ce_gst._id, announced_unit_gst._id]},
+                                          'author_set': request.user.id,
+                                        }).sort('last_update', -1)
+    my_list_unit = []
+    for each in base_unit_cur:
+        my_list_unit.append(each._id)
+
+    base_unit_cur.rewind()
+    my_modules_cur = node_collection.find({'member_of': gst_module_id ,'collection_set':{'$in':my_list_unit } }).sort('last_update', -1)
+    
+    my_modules = []
+    for each in my_modules_cur:
+        my_modules.append(each._id)
+
+    my_units = node_collection.find({'member_of': {'$in': [ce_gst._id, announced_unit_gst._id]},
+                                          'author_set': request.user.id,
+                                          'prior_node': {'$nin': my_modules},
+                                        }).sort('last_update', -1)
+    my_modules_cur.rewind()
+    return render_to_response('ndf/lms_dashboard.html',
+                {
+                    'group_id': auth_id, 'groupid': auth_id,
+                    'node': auth_obj, 'title': title,
+                    'my_course_objs': my_course_objs,
+                    'units_cur':my_units,
+                    'modules_cur': my_modules_cur
+                },
+                context_instance=RequestContext(request)
+        )
 
 def my_groups(request, group_id,page_no=1):
 
