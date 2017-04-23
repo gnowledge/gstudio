34c5ac279c06a7fbd68b316b231ef7101941dbb1
saurabh bharswadkar <saurabhbharswadkar7@gmail.com>
2017-04-23 17:47:42 +0530

    motio game commented

--- gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckPlugins/addJhapp/plugin.js
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/ckPlugins/addJhapp/plugin.js
@@ -77,14 +77,14 @@ CKEDITOR.plugins.add('addJhapp',
                              CKEDITOR.instances[textAreaId].insertHtml(tjs_text_val);
                             $('#group_imgs_on_modal').foundation('reveal', 'close');
                           });
-
-                            $("#insert_earth_rotation").click(function(event){
-                            //ost_text_gallery = $('#paste-ost-gallery').val();
-                            //ost_text_file = $('#paste-ost-captions').val();
-                            ert_text_val = "<iframe src='/rotation/' style='height:100vh;width:100%'></iframe>"
-                            // split_word = ost_text_val.slice(0, 18) + "gallery=" + ost_text_gallery + "&amp;file=/ost/" + ost_text_file+"'"+ ost_text_val.slice(18);
-                            // alert(split_word)
-                             CKEDITOR.instances[textAreaId].insertHtml(ert_text_val);
+                            
+                            // $("#insert_earth_rotation").click(function(event){
+                            // //ost_text_gallery = $('#paste-ost-gallery').val();
+                            // //ost_text_file = $('#paste-ost-captions').val();
+                            // ert_text_val = "<iframe src='/rotation/' style='height:100vh;width:100%'></iframe>"
+                            // // split_word = ost_text_val.slice(0, 18) + "gallery=" + ost_text_gallery + "&amp;file=/ost/" + ost_text_file+"'"+ ost_text_val.slice(18);
+                            // // alert(split_word)
+                            //  CKEDITOR.instances[textAreaId].insertHtml(ert_text_val);
                             $('#group_imgs_on_modal').foundation('reveal', 'close');
                           });
 
