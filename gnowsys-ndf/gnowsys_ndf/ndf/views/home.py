f5d9682800918a1cb3f58c267b5f009322cf3cc3
saurabh bharswadkar <saurabhbharswadkar7@gmail.com>
2017-05-04 12:13:18 +0530

    name added in help page url

--- gnowsys-ndf/gnowsys_ndf/ndf/views/home.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/home.py
@@ -173,12 +173,13 @@ def landing_page(request):
 #             return "/home/"
 
 @get_execution_time
-def help_page_view(request,page_id):
-    page_obj = Node.get_node_by_id(page_id)
+def help_page_view(request,page_name):
+    # page_obj = Node.get_node_by_id(page_id)
+    page_obj = node_collection.one({"name":unicode(page_name)})
     return render_to_response(
                                         "ndf/help_page.html",
                                         {
-                                            "group_id": page_id,
+                                            "group_id": page_obj.pk,
                                             'title': 'Help Page',
                                             'page_obj':page_obj
                                         },
