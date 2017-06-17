7b32cc400c1eda58f6de30ac1f6ec903b59adc09
Kedar Aitawdekar <kedar2a@gmail.com>
2017-06-17 22:48:06 +0530

    Updated 'sync_users.py' script to add 'Teachers' for elements and 'Students' for others in agency_type field of Author instance

--- gnowsys-ndf/fabfile.py
+++ gnowsys-ndf/fabfile.py
@@ -52,21 +52,21 @@ def purge_group():
 	local('python manage.py purge_group')
 
 
-def setup_dlkit():
-	import os
-	dirspot = os.getcwd()
-	if "/home/docker/code/gstudio/gnowsys-ndf" == dirspot:
-		if not os.path.isdir('./dlkit') and not os.path.isdir('./dlkit_runtime'):
-			local('git checkout dlkit')
-			local('git pull origin dlkit')
-			local('git clone https://bitbucket.org/cjshaw/dlkit_runtime.git')
-			local('git clone https://bitbucket.org/cjshaw/dlkit-tests.git')
-			local('git clone https://bitbucket.org/cjshaw/dlkit.git')
-			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/dlkit')
-			local('git submodule update --init --recursive')
-			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/')
-		else:
-			print "dlkit and dlkit_runtime are already exists."
-	else:
-		os.chdir('/home/docker/code/gstudio/gnowsys-ndf')
-		setup_dlkit()
+# def setup_dlkit():
+# 	import os
+# 	dirspot = os.getcwd()
+# 	if "/home/docker/code/gstudio/gnowsys-ndf" == dirspot:
+# 		if not os.path.isdir('./dlkit') and not os.path.isdir('./dlkit_runtime'):
+# 			local('git checkout dlkit')
+# 			local('git pull origin dlkit')
+# 			local('git clone https://bitbucket.org/cjshaw/dlkit_runtime.git')
+# 			local('git clone https://bitbucket.org/cjshaw/dlkit-tests.git')
+# 			local('git clone https://bitbucket.org/cjshaw/dlkit.git')
+# 			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/dlkit')
+# 			local('git submodule update --init --recursive')
+# 			os.chdir('/home/docker/code/gstudio/gnowsys-ndf/')
+# 		else:
+# 			print "dlkit and dlkit_runtime are already exists."
+# 	else:
+# 		os.chdir('/home/docker/code/gstudio/gnowsys-ndf')
+# 		setup_dlkit()
