e890557027a7b2fc6e866646181e63c201b0e9f1
Kedar Aitawdekar <kedar2a@gmail.com>
2017-06-19 16:18:16 +0530

    moved django admin urls under gstudio_admin

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/gstudio_admin.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/gstudio_admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     (r'^data[\/]?', include('gnowsys_ndf.ndf.urls.adminDashboard')),
     (r'^designer[\/]?', include('gnowsys_ndf.ndf.urls.adminDesignerDashboard')),
+
     url(r'^query-doc/(?P<doc_id_or_name>[^/]+)?$', 'gnowsys_ndf.ndf.views.dev_utils.query_doc'),
+
+    # django's admin site url's
+    (r'^', include(admin.site.urls)),
 )
