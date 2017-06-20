23cc17ed5346db068c9226ceb4f1346d52f0d0d4
Kedar Aitawdekar <kedar2a@gmail.com>
2017-06-20 16:15:27 +0530

    updated query_doc function to take nbh input and render NBH of node

--- gnowsys-ndf/gnowsys_ndf/ndf/views/dev_utils.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/dev_utils.py
@@ -17,7 +17,7 @@ except ImportError:  # old pymongo
 ''' -- imports from application folders/files -- '''
 from gnowsys_ndf.ndf.models import node_collection, triple_collection, gridfs_collection, NodeJSONEncoder
 
-def query_doc(request, doc_id_or_name=None):
+def query_doc(request, doc_id_or_name=None, option=None):
 
 	if ObjectId.is_valid(doc_id_or_name):
 		doc_id_or_name = ObjectId(doc_id_or_name)
@@ -31,16 +31,18 @@ def query_doc(request, doc_id_or_name=None):
 
 	result = []
 	for each_doc in query_res:
+		if option in ['nbh', 'NBH', 'get_neighbourhood']:
+			each_doc.get_neighbourhood(each_doc.member_of)
 		result.append(json.dumps(each_doc, cls=NodeJSONEncoder, sort_keys=True))
 
 	return render_to_response('ndf/dev_query_doc.html',
-					        {'result': result},
+					        {'result': result, 'query': doc_id_or_name, 'count': query_res.count()},
 					        context_instance=RequestContext(request)
 						    )
 
 
 def render_test_template(request):
-	test_node = node_collection.one({"name":"home",'_type':"Group"})
+	test_node = node_collection.one({"name":"home", '_type':"Group"})
 	return render_to_response(
         'ndf/test_template.html',
         {"node":test_node},
