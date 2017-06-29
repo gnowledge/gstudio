6ecd8f4f8f1e8ec7662b53933a96385252d481da
root <root@gnowgi.net>
2017-06-29 16:31:00 +0530

    added form for search functionality

--- gnowsys-ndf/gnowsys_ndf/ndf/forms.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/forms.py
@@ -1,5 +1,5 @@
 import datetime
-
+import json
 from django import forms
 from django_mongokit.forms import DocumentForm
 from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
@@ -9,6 +9,40 @@ from models import GSystemType
 
 from registration.forms import RegistrationForm
 from passwords.fields import PasswordField
+
+CHOICES=[("all",'All'),("Author",'Users'),("image",'Images'),("video",'Video'),("text",'Text'),("audio","Audio")]
+GROUP_CHOICES=[]
+GROUP_CHOICES.append(("all","All"))
+group_map = {}
+attribute_map = {}
+ATTRIBUTE_CHOICES = [("--Select--","--Select--"),("educationaluse","Educational use"),("interactivitytype","Interactivity type"),("educationalsubject","Educational subject"),("educationallevel","Educational Level"),("source","Source"),("audience","Audience"),("educationalalignment","Educational alignment"),]
+
+secondlevel_choices = []
+
+with open("/home/docker/code/gstudio/gnowsys-ndf/gnowsys_ndf/ndf/mappings/groupmap.json", 'r') as gm:
+    group_map = json.load(gm)
+
+with open("/home/docker/code/gstudio/gnowsys-ndf/gnowsys_ndf/ndf/mappings/attribute_map.json") as am:
+    attribute_map = json.load(am)
+
+for i in ATTRIBUTE_CHOICES:
+    if i[0] != '--Select--':
+        l = []
+        for val in attribute_map[i[0]]:
+            tup = (val,val)
+            l.append(tup)
+        secondlevel_choices.append(l)
+
+for l in group_map.keys():
+    tup = (l, group_map[l])
+    tup = tuple(tup)
+    GROUP_CHOICES.append(tup)
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label = '', widget = forms.TextInput(attrs={'placeholder': 'Search for'}), error_messages = False)
+    group = forms.ChoiceField(label = "Group", widget = forms.Select, choices = GROUP_CHOICES)
+    select = forms.ChoiceField(label = "Filter", widget = forms.Select, choices = CHOICES)
+
 
 class NodeForm(DocumentForm):
 
