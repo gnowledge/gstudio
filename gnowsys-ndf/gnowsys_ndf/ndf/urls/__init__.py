ccf08d90de68ba9c895208d67c9c5ee29e8db697
dvjsm26 <dhananjayvs.m@gmail.com>
2017-07-12 21:27:13 +0530

    changed name of adv search form

--- gnowsys-ndf/gnowsys_ndf/ndf/urls/__init__.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/__init__.py
@@ -20,6 +20,8 @@ from gnowsys_ndf.ndf.views.methods import tag_info
 from gnowsys_ndf.ndf.views.custom_app_view import custom_app_view, custom_app_new_view
 from gnowsys_ndf.ndf.views import rpc_resources
 
+from gnowsys_ndf.ndf.views.esearch import get_search,get_advanced_search_form,advanced_search
+
 if GSTUDIO_SITE_NAME.lower() == 'clix':
     login_template = 'registration/login_clix.html'
 else:
@@ -46,6 +48,7 @@ urlpatterns = patterns('',
     # (r'^new/$', 'gnowsys_ndf.mobwrite.views.new'),
     # (r'^mobwrite/', 'gnowsys_ndf.mobwrite.views.mobwrite'),
     # --end of mobwrite
+    #url(r'^esearch//?',advanced_search,name='advanced_search')
 
     # url(r'^(?P<group_id>[^/]+)/mailclient[/]error[/](?P<error_obj>[\w-]+)$', 'gnowsys_ndf.ndf.views.mailclient.mailclient_error_display', name='mailclient_error_display'),
 
@@ -53,11 +56,11 @@ urlpatterns = patterns('',
     url(r'^welcome/?', landing_page, name="landing_page"),
     #url(r'^esearch/advanced/?', get_triples, name="get_triples"),
     url(r'^esearch/?', get_search, name="get_search"),
-    url(r'^advanced_form/?', get_advanced_search_form, name="advanced_search_form"),
+    url(r'^advanced_form/?', get_advanced_search_form, name="get_advanced_search_form"),
     url(r'^advanced_search/?', advanced_search, name="advanced_search"),
     url(r'^captcha/', include('captcha.urls')),
     (r'^', include('gnowsys_ndf.ndf.urls.captcha')),
-
+    #url(r'^esearch/get_mapping_json/(?P<json_type>[^/]+)/?$', '', name=''),
     # all main apps
     (r'^(?P<group_id>[^/]+)/mailclient', include('gnowsys_ndf.ndf.urls.mailclient')),
     (r'^(?P<group_id>[^/]+)/analytics', include('gnowsys_ndf.ndf.urls.analytics')),
