a84f0727785e62b0a0fc315fc97ef7075301b84f
root <root@gnowgi.net>
2017-08-10 16:00:20 +0530

    rating widget modiefied for comments

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/static/ndf/js/gstudio-functions.js
@@ -0,0 +1,15 @@
+
+function get_group_id()
+{
+	
+	 group_id = $(".group_id").val();
+	 return  group_id        
+}
+function is_user_authenticated()
+{
+	
+	 is_authenticated = $(".is_authenticated").val();
+	 return  is_authenticated        
+}
+
+
