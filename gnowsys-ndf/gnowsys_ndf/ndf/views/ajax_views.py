06a0334c7c13846ddd0209d11bdc0b1db93fcb1d
katkamrachana <katkam.rachana@gmail.com>
2017-08-30 22:34:23 +0530

    minor updates.

--- gnowsys-ndf/gnowsys_ndf/ndf/views/ajax_views.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/ajax_views.py
@@ -6631,7 +6631,7 @@ def get_group_resources(request, group_id, res_type="Page"):
 
     try:
         res_query = {'_type': 'GSystem', 'group_set': ObjectId(group_id)}
-            except_collection_set_of_id = request.GET.get('except_collection_set_of_id', None)
+        except_collection_set_of_id = request.GET.get('except_collection_set_of_id', None)
 
         except_collection_set_of_obj = Node.get_node_by_id(except_collection_set_of_id)
         if except_collection_set_of_obj:
