07be645a34467dd86acebbb44ad13006a2a6c9a3
kedar2a <kedar2a@gmail.com>
2017-09-02 20:44:00 +0530

    added initial url supporting view

--- gnowsys-ndf/gnowsys_ndf/ndf/views/api.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/api.py
@@ -1,5 +1,6 @@
 ''' -- imports from python libraries -- '''
 import json
+import bson
 from bson.json_util import dumps
 
 ''' -- imports from installed packages -- '''
@@ -27,15 +28,18 @@ def api_get_group_gst_nodes(request, group_name_or_id, gst_name_or_id, username_
     # GET: api/v1/<group_id>/<files>/<nroer_team>/
     # import ipdb; ipdb.set_trace()
     exception_occured = ''
+    oid_name_dict = {}
     try:
         group_id = ObjectId(group_name_or_id)
     except Exception as e:
         group_name, group_id = get_group_name_id(group_name_or_id)
+        oid_name_dict[group_id] = group_name
 
     try:
         gst_id = ObjectId(gst_name_or_id)
     except Exception as e:
         gst_name, gst_id = GSystemType.get_gst_name_id(gst_name_or_id)
+        oid_name_dict[gst_id] = gst_name
 
     # import ipdb; ipdb.set_trace()
     username_or_id_int = 0
@@ -48,36 +52,60 @@ def api_get_group_gst_nodes(request, group_name_or_id, gst_name_or_id, username_
     
     auth_obj = node_collection.one({'_type': u'Author', '$or': [{'name': unicode(username_or_id)}, {'created_by': username_or_id_int} ] })
     if auth_obj:
+        oid_name_dict[auth_obj._id] = auth_obj.name
         user_id = auth_obj.created_by
     else:
         return HttpResponse('Requested user does not exists.', content_type='text/plain')
 
-    gst_all_fields_dict = {i: 1 for i, j in GSystem.structure.iteritems()}
+    gsystem_structure_dict = GSystem.structure
+    gsystem_keys = gsystem_structure_dict.keys()
+
+    gst_all_fields_dict = {i: 1 for i in gsystem_keys}
     gst_api_fields_dict = { "_id": 1, "name": 1, "altnames": 1, "language": 1, "content": 1, "if_file": 1, "tags": 1, "location": 1, "created_by": 1, "modified_by": 1, "contributors": 1, "legal": 1, "rating": 1, "created_at": 1, "last_update": 1, "collection_set": 1, "post_node": 1, "prior_node": 1, "access_policy": 1, "status": 1, "group_set": 1, "member_of": 1, "type_of": 1,
     # "relation_set": 1, "attribute_set": 1, 
      }
 
+    sample_gs = GSystem()
+    attributes = sample_gs.get_possible_attributes([gst_id]) 
+
+    query_dict = {
+                    '_type': 'GSystem',
+                    'group_set': ObjectId(group_id),
+                    'member_of': ObjectId(gst_id),
+                    'created_by': user_id,
+                    'status': u'PUBLISHED',
+                    'access_policy': 'PUBLIC',
+                    # 'if_file.mime_type': {'$regex': 'video', '$options': 'i'}
+                }
+
     # GET parameters:
+    get_parameters_dict = request.GET.dict()
+
+    for key, val in get_parameters_dict.iteritems():
+        if ('gs_' in key):
+            stripped_key = key.lstrip('gs_').split('.')[0]
+            if stripped_key in gsystem_keys:
+                query_dict.update({key.lstrip('gs_'): {'$regex': val, '$options': 'i'}})
+
+            elif stripped_key in gst_attributes(gst_id):
+                query_dict.update({('attribute_set.' + stripped_key): {'$regex': val, '$options': 'i'}})
+
+            # print query_dict
+
+
     human = eval(request.GET.get('human', '0'))
 
     gst_fields = gst_api_fields_dict if human else gst_all_fields_dict
 
-    all_resources = node_collection.find({
-                            '_type': 'GSystem',
-                            'group_set': ObjectId(group_id),
-                            'member_of': ObjectId(gst_id),
-                            'created_by': user_id,
-                            'status': u'PUBLISHED',
-                            'access_policy': 'PUBLIC'
-                        }, gst_fields)
+    all_resources = node_collection.find(query_dict, gst_fields)
 
 
     if human:
         gst_fields = gst_api_fields_dict
-        user_fields = ['created_by', 'modified_by', 'contributors']
 
         # converting ids to human readable names:
         # Django User:
+        user_fields = ['created_by', 'modified_by', 'contributors']
         all_users = []
         for each_field in user_fields:
             all_users += all_resources.distinct(each_field)
@@ -86,20 +114,38 @@ def api_get_group_gst_nodes(request, group_name_or_id, gst_name_or_id, username_
         userid_name_dict_cur = node_collection.find({'_type': u'Author', 'created_by': {'$in': all_users}}, {'name': 1, 'created_by': 1, '_id': 0})
         userid_name_dict = {i['created_by']: i['name'] for i in userid_name_dict_cur}
 
-        sample_gs = GSystem()
-        attributes = sample_gs.get_possible_attributes([gst_id]) 
+
+        # Mongo ids
+        oid_fields = [ k for k, v in gsystem_structure_dict.iteritems() if v in [bson.objectid.ObjectId, [bson.objectid.ObjectId]] ]
+        all_oid_list = []
+        for each_field in oid_fields:
+            all_oid_list += all_resources.distinct(each_field)
+        all_oid_list = list(set(all_oid_list))
+
+        oid_name_dict_cur = node_collection.find({'_id': {'$in': all_oid_list}}, {'name': 1})
+        oid_name_dict = {i['_id']: i['name'] for i in oid_name_dict_cur}
+
+
 
         python_cur_list = []
         python_cur_list_append = python_cur_list.append
         for each_gst in all_resources:
+
             # attaching attributes:
-            for key, value in attributes.iteritems():
+            # NEEDS to optimize.
+            for key, value in each_gst.get_possible_attributes(each_gst.member_of).iteritems():
                 each_gst[key] = value['data_type']
                 each_gst[key] = value['object_value']
 
             # mapping user id to username.
             for each_field in user_fields:
                 each_gst[each_field] = [userid_name_dict[i] for i in each_gst[each_field]] if isinstance(each_gst[each_field], list) else userid_name_dict[each_gst[each_field]]
+
+
+            # mapping mongo _id to name.
+            for each_field in oid_fields:
+                each_gst[each_field] = [oid_name_dict[i] for i in each_gst[each_field]] if isinstance(each_gst[each_field], list) else oid_name_dict[each_gst[each_field]]
+
 
             python_cur_list_append(each_gst)
 
@@ -109,3 +155,14 @@ def api_get_group_gst_nodes(request, group_name_or_id, gst_name_or_id, username_
         json_result = dumps(all_resources)
 
     return HttpResponse(json_result, content_type='application/json')
+
+
+# helper methods:
+def gst_attributes(gst_name_or_id):
+
+    try:
+        gst_id = ObjectId(gst_name_or_id)
+    except Exception as e:
+        gst_name, gst_id = GSystemType.get_gst_name_id(gst_name_or_id)
+
+    return [at.name for at in node_collection.find({'_type': 'AttributeType', 'subject_type': gst_id})]
