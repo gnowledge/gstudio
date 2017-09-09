4afaf55290984788111ab57df11caac89a223f73
katkamrachana <katkam.rachana@gmail.com>
2017-09-09 20:19:58 +0530

    script updated for set_name_to_altnames_of_leafnodes.py

--- doc/deployer/set_name_to_altnames_of_leafnodes.py
+++ doc/deployer/set_name_to_altnames_of_leafnodes.py
@@ -5,8 +5,8 @@ if group_obj:
 	all_leaf_node_ids = dig_nodes_field(parent_node=n, member_of=['Page'], only_leaf_nodes=True)
 	all_leaf_node_cur = node_collection.find({'_id': {'$in': all_leaf_node_ids}})
 	for each_node in all_leaf_node_cur:
-		altnames_val = each_node.altnames 
-		each_node.name = altnames_val # Display name --> Unique name
-		each_node.altnames = each_node.name
+		name_val = each_node.name 
+		each_node.altnames = name_val # Unique name --> Display name 
+		each_node.save()
 else:
 	print "\nNo Group found!!"
