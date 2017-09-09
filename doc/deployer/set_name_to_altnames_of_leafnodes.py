74470457f6237f72ba8c2366ad09063f6c023035
katkamrachana <katkam.rachana@gmail.com>
2017-09-09 20:23:22 +0530

    script updated for set_name_to_altnames_of_leafnodes.py

--- doc/deployer/set_name_to_altnames_of_leafnodes.py
+++ doc/deployer/set_name_to_altnames_of_leafnodes.py
@@ -1,4 +1,6 @@
 from gnowsys_ndf.ndf.models import *
+from gnowsys_ndf.ndf.views.methods import dig_nodes_field
+
 group_id_str = raw_input("Enter Group_id: ")
 group_obj = node_collection.one({'_id': ObjectId(group_id_str)})
 if group_obj:
@@ -7,6 +9,7 @@ if group_obj:
 	print "\nLeaf nodes found: ", all_leaf_node_cur.count()
 	for each_node in all_leaf_node_cur:
 		name_val = each_node.name 
+		print "\n ", each_node.altnames , " --->", name_val
 		each_node.altnames = name_val # Unique name --> Display name 
 		each_node.save()
 else:
