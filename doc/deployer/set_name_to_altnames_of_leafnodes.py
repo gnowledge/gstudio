971b6e715474c69bf25d66092feb56930de238fc
katkamrachana <katkam.rachana@gmail.com>
2017-09-09 20:21:17 +0530

    script updated for set_name_to_altnames_of_leafnodes.py

--- doc/deployer/set_name_to_altnames_of_leafnodes.py
+++ doc/deployer/set_name_to_altnames_of_leafnodes.py
@@ -4,6 +4,7 @@ group_obj = node_collection.one({'_id': ObjectId(group_id_str)})
 if group_obj:
 	all_leaf_node_ids = dig_nodes_field(parent_node=n, member_of=['Page'], only_leaf_nodes=True)
 	all_leaf_node_cur = node_collection.find({'_id': {'$in': all_leaf_node_ids}})
+	print "\nLeaf nodes found: ", all_leaf_node_cur.count()
 	for each_node in all_leaf_node_cur:
 		name_val = each_node.name 
 		each_node.altnames = name_val # Unique name --> Display name 
