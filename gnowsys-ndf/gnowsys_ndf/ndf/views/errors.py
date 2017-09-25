b037f11c361247881ee155fb05ac4b33be177755
katkamrachana <katkam.rachana@gmail.com>
2017-09-25 17:37:37 +0530

    errors file added for future dvpt

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/errors.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+# To pass custom Error/Info/Warning messages on Error templates, pass in the context variables
+
+def handler404(request):
+    # print "\nrequest: ", request
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
