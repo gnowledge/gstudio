e9d7e61bf5576a7839dd42071681da9f7abe37e3
katkamrachana <katkam.rachana@gmail.com>
2017-09-26 13:02:11 +0530

    403handler added

--- gnowsys-ndf/gnowsys_ndf/ndf/views/errors.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/errors.py
@@ -15,3 +15,9 @@ def handler500(request):
                                   context_instance=RequestContext(request))
     response.status_code = 500
     return response
+
+def handler403(request):
+    response = render_to_response('403.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
