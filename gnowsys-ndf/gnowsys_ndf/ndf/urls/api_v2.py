a3ac0237877582022b111ef7c72c2ddb45da5feb
kedar2a <kedar2a@gmail.com>
2017-11-24 13:01:38 +0530

    added new urls and views files api_v2

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/urls/api_v2.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('gnowsys_ndf.ndf.views.api_v2',
+                        # GET: api/v2/<group>/<resource>/<user>/
+                        url(r'^/?$', 'api_get_gs_nodes'),
+                        url(r'^/(?P<field_name>[^/]+)/?$', 'api_get_field_values'),
+                        url(r'^/create/(?P<gst_name>[^/]+)/?$', 'api_create_gst'),
+                        # url(r'^/(?P<group_name_or_id>[^/]+)/(?P<gst_name_or_id>[^/]+)/?$', 'api_get_group_gst_nodes', name='(?P<unit_group_id>[\w-]+)'),
+                        # url(r'^/(?P<group_name_or_id>[^/]+)/(?P<gst_name_or_id>[^/]+)/(?P<username_or_id>[^/]+)/?$', 'api_get_group_gst_nodes', name='(?P<unit_group_id>[\w-]+)'),
+)
