2e51e819ec3e326c8ac07cf86f14a0799e2611ef
katkamrachana <katkam.rachana@gmail.com>
2017-11-24 19:15:11 +0530

    fetch buddies from session

--- gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/templatetags/ndf_tags.py
@@ -4182,8 +4182,9 @@ def load_quiz_player(request, group_id, node, hide_edit_opt=False):
 @register.assignment_tag
 def get_module_enrollment_status(request, module_obj):
     data_dict = {'full_enrolled': False}
-    if 'buddies_userid_list' in request.session:
-        user_obj_list = User.object.get(pk__in=request.session.buddies_userid_list)
+    buddies_ids = request.session.get('buddies_userid_list', [])
+    if buddies_ids:
+        user_obj_list = User.objects.get(pk__in=buddies_ids)
         for  userobj in user_obj_list:
             data_dict.update({userobj.pk : all(user_access_policy(groupid,userobj)=="allow" for groupid in module_obj.collection_set)})
         data_dict.update({'full_enrolled': all(data_dict.values())})
