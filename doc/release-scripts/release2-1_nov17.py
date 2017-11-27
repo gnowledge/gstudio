6e3da17c52cb6f1fc2e5025498923b206c4381ae
kedar2a <kedar2a@gmail.com>
2017-11-27 18:54:35 +0530

    created a doc/release-scripts folder. Added a new release script release2-1_nov17.py for release 2.1

---
+++ doc/release-scripts/release2-1_nov17.py
@@ -0,0 +1,51 @@
+import json
+
+from gnowsys_ndf.ndf.models import Node, Author, node_collection
+from gnowsys_ndf.settings import GSTUDIO_INSTITUTE_ID
+try:
+    from bson import ObjectId
+except ImportError:  # old pymongo
+    from pymongo.objectid import ObjectId
+
+author_user_map = {
+	"fields":{
+		"_id": "id",
+		"password": "password",
+		"name": "username",
+		"email": "email",
+		"created_at": "date_joined",
+		},
+	"attributes":{
+		"first_name": "first_name",
+		"last_name": "last_name"
+	}
+}
+
+username_existing_expected_map = {
+	"piegon": "pigeon",
+	"garpes": "grapes",
+	"beatle": "beetle"
+}
+
+
+def update_username(username_existing_expected_map={}, save_objects=True):
+
+	with open('../doc/release-scripts/user-tokens.json') as tokens:
+		user_tokens_map = json.load(tokens)
+
+		for existing, expected in username_existing_expected_map.iteritems():
+
+			check_in_username = user_tokens_map['seperator'] + existing + user_tokens_map['seperator'] + GSTUDIO_INSTITUTE_ID
+			auth_cur = node_collection.find({'_type': u'Author', 'name': {'$regex': unicode(check_in_username), '$options': 'i'} })
+			for each_auth in auth_cur:
+				each_auth.name = each_auth.name.replace(existing, expected)
+				each_auth.save()
+
+			user_cur = User.objects.filter(username__endswith=unicode(check_in_username))
+			for each_user in user_cur:
+				each_user.username = each_user.username.replace(existing, expected)
+				each_user.save()
+
+
+if __name__ == '__main__':
+	update_username(username_existing_expected_map)
