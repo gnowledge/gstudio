3f2db7521125fa8739d33c6c4d79c23fbc9d0def
kedar2a <kedar2a@gmail.com>
2017-11-28 11:05:52 +0530

    Updated release2.1 script for more verbose output

--- doc/release-scripts/release2-1_nov17.py
+++ doc/release-scripts/release2-1_nov17.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.admin import User
 from gnowsys_ndf.ndf.models import Node, Author, node_collection
 from gnowsys_ndf.settings import GSTUDIO_INSTITUTE_ID
 try:
@@ -42,15 +43,23 @@ def update_username(username_existing_expected_map={}):
 
 		check_in_username = user_tokens_map_seperator + existing + user_tokens_map_seperator + GSTUDIO_INSTITUTE_ID
 		auth_cur = node_collection.find({'_type': u'Author', 'name': {'$regex': unicode(check_in_username), '$options': 'i'} })
+		print "\nUpdating Author instances for: '%s' \n"%existing
 		for each_auth in auth_cur:
+			print "\nOLD: ", each_auth.name
 			each_auth.name = each_auth.name.replace(existing, expected)
 			each_auth.save()
+			print "NEW: ", each_auth.name
 
+		print "-------------------------------------------"
+		print "\nUpdating User instances for: '%s' \n"%existing
 		user_cur = User.objects.filter(username__endswith=unicode(check_in_username))
 		for each_user in user_cur:
+			print "\nOLD: ", each_user.username
 			each_user.username = each_user.username.replace(existing, expected)
+			print "NEW: ", each_user.username
 			each_user.save()
 
+		print "\n=========================================== \n"
 
 if __name__ == '__main__':
 	update_username(username_existing_expected_map)
