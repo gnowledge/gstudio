bfc56d4f7c45ab3cf792e1c4aa3e7f615157c31b
kedar2a <kedar2a@gmail.com>
2018-01-05 15:03:09 +0530

    Added 2 new methods in models.db_utils.py

--- gnowsys-ndf/gnowsys_ndf/ndf/models/db_utils.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/models/db_utils.py
@@ -19,5 +19,17 @@ def get_all_collection_child_names():
 	return sum([get_collection_child_names(i) for i in get_collection_names()], [])
 
 
+def get_collection_hierarchy():
+	# returns following JSON:
+	# {"Filehives": ["Filehive"], "Triples": ["GAttribute", "GRelation"], "Buddies": ["Buddy"], "Benchmarks": ["Benchmark"], "Nodes": ["MetaType", "GSystemType", "RelationType", "AttributeType", "GSystem", "Group", "ToReduceDocs", "Author"], "Counters": ["Counter"]}
+	return {k: get_collection_child_names(k) for k in get_collection_names()}
+
+
 def get_model_name(collection_obj):
 	return collection_obj._meta.model_name
+
+
+def get_model_structure(collection_name):
+	# collection_name: str
+	from gnowsys_ndf.ndf.models import *
+	return getattr(eval(collection_name), 'structure')
