a3490ea13e4251526bef8ce16d04440f421e8c9a
Saurabh Bharswadkar <saurabhbharswadkar7@gmail.com>
2018-02-12 16:55:27 +0530

    sitemap template added

--- gnowsys-ndf/gnowsys_ndf/ndf/views/sitemap.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/sitemap.py
@@ -1,9 +1,13 @@
 # from django.contrib.sitemaps import Sitemap
 from gnowsys_ndf.ndf.models import *
-
+from django.shortcuts import render_to_response  # , render  uncomment when to use
+from django.template import RequestContext
 import datetime
 
 def sitemap(request):
     print "******************* Inseide sitemap"
 
-    return 1
+    return render_to_response("ndf/sitemap.html",
+                                {"name":"name"},
+                                context_instance = RequestContext(request)
+    )
