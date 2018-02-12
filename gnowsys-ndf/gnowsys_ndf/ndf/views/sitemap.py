beead871d5a9fd818714b3ed33a464dea8294b66
Saurabh Bharswadkar <saurabhbharswadkar7@gmail.com>
2018-02-12 16:39:12 +0530

    sitemap initial urls and method

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/sitemap.py
@@ -0,0 +1,9 @@
+# from django.contrib.sitemaps import Sitemap
+from gnowsys_ndf.ndf.models import *
+
+import datetime
+
+def sitemap(request):
+    print "******************* Inseide sitemap"
+
+    return 1
