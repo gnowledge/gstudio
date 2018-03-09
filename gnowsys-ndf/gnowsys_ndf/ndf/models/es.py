bc981927976573539376bf826255557aa4e6188a
siddhu dhangar <siddhudhangar@gmail.com>
2018-03-09 18:23:08 +0530

    merge code of dev_utils in e-library

---
+++ gnowsys-ndf/gnowsys_ndf/ndf/models/es.py
@@ -0,0 +1,7 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import *
+from gnowsys_ndf.local_settings import GSTUDIO_ELASTIC_SEARCH
+
+es = Elasticsearch("http://elastic:changeme@gsearch:9200", timeout=100, retry_on_timeout=True)
+
+es_client = Search(using=es)
