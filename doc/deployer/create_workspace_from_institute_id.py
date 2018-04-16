10980aaf18eb0343dba92d59a9918c3855a059a1
kedar2a <kedar2a@gmail.com>
2018-04-16 18:46:40 +0530

    Updated error templates

--- doc/deployer/create_workspace_from_institute_id.py
+++ doc/deployer/create_workspace_from_institute_id.py
@@ -10,7 +10,7 @@ if GSTUDIO_INSTITUTE_ID and GSTUDIO_INSTITUTE_NAME:
 		group_obj = node_collection.collection.Group()
 		group_obj.name = unicode(GSTUDIO_INSTITUTE_ID.strip())
 		group_obj.altnames = unicode(workspace_name.strip())
-		group_obj.content = unicode("Welcome to CLIx workspace!\n Here all students and teachers in your school can collaboratively work to create interesting learning content.\nYou can create activity pages and lessons, write e-Notes, upload and share files such as images, audios, videos, Turtle projects, GeoGebra projects, PhET simulations, CLIx Apps. You can take help from other CLIx modules under Explore section.\n\n Unleash the fun of learning by creation :)\n")
+		group_obj.content = unicode("Welcome to CLIx workspace!<br/> Here all students and teachers in your school can collaboratively work to create interesting learning content.<br/>You can create activity pages and lessons, write e-Notes, upload and share files such as images, audios, videos, Turtle projects, GeoGebra projects, PhET simulations, CLIx Apps. You can take help from other CLIx modules under Explore section.<br/><br/> Unleash the fun of learning by creation :)<br/>")
 		group_obj.access_policy = u"PUBLIC"
 		group_obj.subscription_policy=u"OPEN"
 		group_obj.group_type = u"PUBLIC"
