70db55905b1612dba5add2e37da9833766a140a9
tirthakarmahesh211 <tirthakarmahesh211@gmail.com>
2018-04-21 10:35:30 +0530

    In filter Malayalam and aasamese lang added and Module name renamed as Name

--- gnowsys-ndf/gnowsys_ndf/settings.py
+++ gnowsys-ndf/gnowsys_ndf/settings.py
@@ -747,7 +747,7 @@ GSTUDIO_RESOURCES_AUDIENCE = ["Teachers", "Students", "Teacher educators"]
 
 GSTUDIO_RESOURCES_TEXT_COMPLEXITY = ["Easy", "Moderately Easy", "Intermediate", "Moderately Hard", "Hard"]
 
-GSTUDIO_RESOURCES_LANGUAGES = ["English", "Gujarati", "Hindi", "Manipuri", "Marathi", "Mizo", "Telugu"]
+GSTUDIO_RESOURCES_LANGUAGES = ["Assamese","English", "Gujarati", "Hindi", "Malayalam","Manipuri", "Marathi", "Mizo", "Telugu"]
 
 GSTUDIO_RESOURCES_AGE_RANGE = ["5-10", "11-20", "21-30", "31-40", "41 and above"]
 
