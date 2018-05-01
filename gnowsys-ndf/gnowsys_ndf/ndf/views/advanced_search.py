c8f80d60473165542e096cc389ac17fcd995f50a
siddhu dhangar <siddhudhangar@gmail.com>
2018-05-01 20:01:02 +0530

    In advanced_search.py file, new interactive_page gsystemtype added in mongo query to displayed interactive page also while searching

--- gnowsys-ndf/gnowsys_ndf/ndf/views/advanced_search.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/advanced_search.py
@@ -25,17 +25,21 @@ from gnowsys_ndf.ndf.gstudio_es.paginator import Paginator ,EmptyPage, PageNotAn
 from gnowsys_ndf.ndf.templatetags.ndf_tags import get_relation_value
 from gnowsys_ndf.settings import GSTUDIO_ELASTIC_SEARCH
 
-if GSTUDIO_ELASTIC_SEARCH:
+def search_detail(request,group_id,page_num=1):
+	if GSTUDIO_ELASTIC_SEARCH:
 
-	q = Q('match',name=dict(query='File',type='phrase'))
-	GST_FILE = Search(using=es, index="nodes",doc_type="gsystemtype").query(q)
-	GST_FILE1 = GST_FILE.execute()
+		q = Q('match',name=dict(query='File',type='phrase'))
+		GST_FILE = Search(using=es, index="nodes",doc_type="gsystemtype").query(q)
+		GST_FILE1 = GST_FILE.execute()
 
-	q = Q('match',name=dict(query='Page',type='phrase'))
-	GST_PAGE = Search(using=es, index="nodes",doc_type="gsystemtype").query(q)
-	GST_PAGE1 = GST_PAGE.execute()
+		q = Q('match',name=dict(query='Page',type='phrase'))
+		GST_PAGE = Search(using=es, index="nodes",doc_type="gsystemtype").query(q)
+		GST_PAGE1 = GST_PAGE.execute()
 
-	def search_detail(request,group_id,page_num=1):
+		q = Q('match',name=dict(query='interactive_page',type='phrase'))
+		GST_IPAGE = Search(using=es, index="nodes",doc_type="gsystemtype").query(q)
+		GST_IPAGE1 = GST_IPAGE.execute()
+
 		search_result = ''
 		try:
 			group_id = ObjectId(group_id)
@@ -51,7 +55,8 @@ if GSTUDIO_ELASTIC_SEARCH:
 		else:
 			selected_field = "content"
 
-		q = Q('bool', must=[Q('match', member_of=GST_FILE1.hits[0].id),Q('match',group_set=str(group_id)),Q('match',access_policy='public'),~Q('exists',field=selected_field)],
+		q = Q('bool', must=[Q('match',group_set=str(group_id)),Q('match',access_policy='public'),~Q('exists',field=selected_field)],
+			should=[Q('match', member_of=GST_FILE1.hits[0].id),Q('match', member_of=GST_IPAGE1.hits[0].id)],
 			must_not=[Q('match', member_of=GST_PAGE1.hits[0].id)])
 		search_result =Search(using=es, index="nodes",doc_type="gsystemtype,gsystem,metatype,relationtype,attribute_type,group,author").query(q)
 		search_result = search_result.exclude('terms', name=['thumbnail','jpg','png','svg'])
@@ -87,18 +92,18 @@ if GSTUDIO_ELASTIC_SEARCH:
 		paginator_search_result = None
 
 		if request.GET.get('field_list',None) == "attribute_type_set" or page_num > 1 :
-			temp = node_collection.find({'_type': 'GSystem','access_policy':'PUBLIC','member_of': { '$nin': [ObjectId(group_id_of_twist)] } ,'group_set':ObjectId('55ab34ff81fccb4f1d806025')})
+			temp = node_collection.find({'_type': 'GSystem','access_policy':'PUBLIC','member_of': { '$nin': [ObjectId(group_id_of_twist)] } ,'group_set':ObjectId(group_id)}).limit(1000)
 
 			lst = []
 			for each in temp:
 			    if each._id:
 			        rel_val = get_relation_value(ObjectId(each._id),"teaches")
 			        if not rel_val['grel_id']:
-			            lst.append(each._id)
+						lst.append(each._id)
 
-			search_result = node_collection.find({ '_id': {'$in': lst} })
+			search_result = node_collection.find({ '_id': {'$in': lst} }).limit(1000)
 			if_teaches = True
-			paginator_search_result = paginator.Paginator(search_result, page_num, 15)
+			paginator_search_result = paginator.Paginator(search_result, page_num, 30)
 
 		return render_to_response('ndf/asearch.html', {"page_info":paginator_search_result,"page_no":page_no,"has_next":has_next,'GSTUDIO_ELASTIC_SEARCH':GSTUDIO_ELASTIC_SEARCH,'advanced_search':"true",'groupid':group_id,'group_id':group_id,'title':"advanced_search","search_curr":search_result,'field_list':selected_field,'chk_advanced_search':chk_advanced_search,'if_teaches':if_teaches},
 					context_instance=RequestContext(request))
