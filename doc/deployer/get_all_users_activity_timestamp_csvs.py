e1cecbac6cf0ed0c4e6a95ae0c22c141b77da7c6
Saurabh Bharswadkar <saurabhbharswadkar7@gmail.com>
2018-05-17 16:20:52 +0530

    all_users query removed

--- doc/deployer/get_all_users_activity_timestamp_csvs.py
+++ doc/deployer/get_all_users_activity_timestamp_csvs.py
@@ -3,7 +3,6 @@ from gnowsys_ndf.ndf.models import *
 from django.contrib.auth.models import User
 from django.core.management import execute_from_command_line
 
-all_users = User.objects.all() 
 usernames_list = Author.get_author_usernames_list_from_user_id_list((counter_collection.find().distinct("user_id")))
 for each in usernames_list:
 	execute_from_command_line(["manage.py", "activity_timestamp",each])
