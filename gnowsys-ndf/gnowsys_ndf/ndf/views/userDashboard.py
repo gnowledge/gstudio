4d622616af32694291fe67c931217bfaf35b4dd3
siddhu dhangar <siddhudhangar@gmail.com>
2018-06-13 18:49:59 +0530

    In my-desk view,replaced mongo query with ES query and added pagination

--- gnowsys-ndf/gnowsys_ndf/ndf/views/userDashboard.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/userDashboard.py
@@ -38,6 +38,7 @@ from gnowsys_ndf.ndf.views.filehive import write_files
 from gnowsys_ndf.notification import models as notification
 from gnowsys_ndf.ndf.templatetags.ndf_tags import get_all_user_groups, get_user_course_groups
 from gnowsys_ndf.ndf.gstudio_es.es import *
+from gnowsys_ndf.ndf.gstudio_es.paginator import Paginator ,EmptyPage, PageNotAnInteger
 #######################################################################################################################################
 
 gapp_mt = node_collection.one({'_type': "MetaType", 'name': META_TYPE[0]})
@@ -737,6 +738,7 @@ def my_courses(request, group_id):
         )
 
 def my_desk(request, group_id,page_no=1):
+
     if str(request.user) == 'AnonymousUser':
         raise Http404("You don't have an authority for this page!")
 
@@ -774,11 +776,26 @@ def my_desk(request, group_id,page_no=1):
     #     my_modules.append(each._id)
 
     if GSTUDIO_ELASTIC_SEARCH and TESTING_VARIABLE_FOR_ES:
+
         my_units_page_cur = None
-        q = Q('bool', must=[Q('match', author_set=request.user.id),~Q('match',name="trash"),~Q('terms', name=GSTUDIO_DEFAULT_GROUPS_LIST)],
+        q = Q('bool', must=[Q('match', author_set=request.user.id),~Q('match', name='Trash'),~Q('terms', name=GSTUDIO_DEFAULT_GROUPS_LIST)],
         should=[Q('match',member_of=ce_gst.hits[0].id),Q('match',member_of=announced_unit_gst.hits[0].id),Q('match',member_of=gst_group.hits[0].id) ],
         minimum_should_match=1)
         my_units =Search(using=es, index="nodes",doc_type="gsystemtype,gsystem,metatype,relationtype,attribute_type,group,author").query(q)
+        # print my_units.count()
+
+        if int(page_no)==1:
+            my_units=my_units[0:21]
+        else:
+            temp=( int(page_no) - 1) * 21
+            my_units=my_units[temp:temp+21]
+        paginator = Paginator(my_units, 21)
+        try:
+            my_units_page_cur = paginator.page(page_no)
+        except PageNotAnInteger:
+            my_units_page_cur = paginator.page(1)
+        except EmptyPage:
+            my_units_page_cur = paginator.page(paginator.num_pages)
 
     else:
         my_units = node_collection.find(
@@ -799,6 +816,7 @@ def my_desk(request, group_id,page_no=1):
                     # 'my_course_objs': my_course_objs,
                     'units_cur':my_units,
                     'my_units_page_cur':my_units_page_cur,
+                    'page_info':my_units_page_cur
                     # 'modules_cur': my_modules_cur
                 },
                 context_instance=RequestContext(request)
