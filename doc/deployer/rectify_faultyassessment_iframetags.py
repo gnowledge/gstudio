bbb2bf7b07a5c762702406f72647077f27ebfe59
SheetalKashid <kashid.sheetal3@gmail.com>
2018-09-21 15:32:41 +0530

    extra whitespaces removed and uncommented the required code

--- doc/deployer/rectify_faultyassessment_iframetags.py
+++ doc/deployer/rectify_faultyassessment_iframetags.py
@@ -1,6 +1,4 @@
 # The Assessment script covers the following usecases : 
-
-
 #Usecase 1: Usecase which covers the correction of faulty iframe tags like the one with no proper ending iframe tags, no proper starting iframe tags, in the assessments.
 #Usecase 2: Usecase also covers the correction of multiple faulty iframe tags in the same content.
 #Usecase 3: Usecase which covers the conversion of special characters into websafe codec.  
@@ -11,11 +9,9 @@ from lxml import html
 from lxml.html import tostring
 import HTMLParser
 
-
 # regular expression to cover the starting iframe tag with no proper ending iframe tags.
 regx = re.compile("(<iframe[^>]*>(?!<\/iframe>))",re.IGNORECASE)                    
 
-#based on above regular expression, In ct10,total 46 culprit iframe tags were found
 all_culprit_iframes = node_collection.find({'_type':'GSystem', 'content':regx })
 print "Total Objects containing Faulty Iframe Tags: ", all_culprit_iframes.count()
 htmlparser = HTMLParser.HTMLParser()
@@ -32,7 +28,7 @@ for index, each in enumerate(all_culprit_iframes, start=1):
       # print content.encode("utf-8"), "\n" 
       # print "="*30
       each.content = content
-      #each.save()
+      each.save()
       
   
 
