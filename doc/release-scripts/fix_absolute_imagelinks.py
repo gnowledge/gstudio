6fde55c23781bcfd83f6fa2c2243f772e979cde0
SheetalKashid <kashid.sheetal3@gmail.com>
2019-01-21 18:17:25 +0530

    added logic to modify turtle_block_activities relative_path

--- doc/release-scripts/fix_absolute_imagelinks.py
+++ doc/release-scripts/fix_absolute_imagelinks.py
@@ -1,7 +1,11 @@
 '''
-Issue :The images in the activity pages are not showing up. The src of the image is given as absolute URL not that of relative
-
+***Rectified_Issues***
+Issue1 :The images in the activity pages are not showing up. The src of the image is given as absolute URL not that of relative
 Fix :  Finding the activities having the image src as the absolute URL and then changing the same to the relative one.
+
+-----------
+Issue2: Activity Turtle Blocks icon redirecting to clixplatform.tiss.edu/turtle/ instead of clixserver.tiss.edu/turtle/
+Fix: Finding the activities whose href source is absolute url: clixplatform.tiss.edu/turtle/ and then changing the same to the relative url: /turtle/.
 
 '''
 
@@ -10,6 +14,7 @@ import re
 from gnowsys_ndf.ndf.models import node_collection, Node
 from bs4 import BeautifulSoup  
 
+#Issue 1
 #To identify the image src having the absolute URL 
 regx = re.compile('"https:\/\/clixplatform.tiss.edu\/media',re.IGNORECASE)
 regx1 = re.compile('"http:\/\/clixplatform.tiss.edu\/media',re.IGNORECASE) 
@@ -42,4 +47,32 @@ for index,eachnd in enumerate(activitynds,start =1):
             eachnd.save()
             nodesave = False
     print "*"*30
+
+#Issue 2
+#Extracting all the activity pages containing href src as "clixplatform.tiss.edu/turtle"
+turtle_regx = re.compile('"https:\/\/clixplatform.tiss.edu\/turtle',re.IGNORECASE)
+turtle_nds = node_collection.find({'_type': 'GSystem','member_of':{'$in':[page_gst_id,trnsnd_gst_id]},'content': {'$in':[turtle_regx]},'collection_set': []})
+
+for indx,eachnode in enumerate(turtle_nds,start =1):
+    soup = BeautifulSoup(eachnode.content)
+    fndanchor = soup.find_all('a')
+    #print("previous content:",fndanchor)
+    if fndanchor:
+        #print indx ,"\t",":", eachnode._id,"\n"
+        for each in fndanchor:
+            if each.has_attr('href'):
+                hrefsrc=each["href"]
+                #print(hrefsrc)
+                innerflag = hrefsrc.startswith("https://clixplatform") or hrefsrc.startswith("http://clixplatform")
+                if innerflag:
+                    nodesave = True
+                    each["href"]="/turtle/"
+                    #print("modified href",each["href"])
+        if nodesave:
+            eachnode.content=soup
+            eachnode.content = eachnode.content.decode("utf-8")
+            print "Changing :",eachnode._id
+            eachnode.save()
+            nodesave = False
+
 
